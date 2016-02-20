3a137cec7659fb2ff5ff19569246e3bbeaed3be6
Adriaan Rol <adriaan.rol@gmail.com>
2016-02-20 13:49:21 +0100

    New functions to qubit object
    Basic seq pars
    Rabi measurement
    Spec and pulsed spec

--- instrument_drivers/meta_instrument/qubit_object.py
+++ instrument_drivers/meta_instrument/qubit_object.py
@@ -66,21 +66,23 @@ class Qubit(Instrument):
         self.msmt_suffix = '_' + name  # used to append to measuremnet labels
 
     def measure_T1(self):
+        # Note: I made all functions lowercase but for T1 it just looks too
+        # ridiculous
         raise NotImplementedError()
 
-    def measure_Rabi(self):
+    def measure_rabi(self):
         raise NotImplementedError()
 
-    def measure_Ramsey(self):
+    def measure_ramsey(self):
         raise NotImplementedError()
 
-    def measure_Echo(self):
+    def measure_echo(self):
         raise NotImplementedError()
 
-    def measure_AllXY(self):
+    def measure_allxy(self):
         raise NotImplementedError()
 
-    def measure_SSRO(self):
+    def measure_ssro(self):
         raise NotImplementedError()
 
     def measure_spectroscopy(self):
@@ -135,6 +137,20 @@ class Transmon(Qubit):
         self.add_parameter('f_res', label='resonator frequency', units='Hz',
                            get_cmd=self._get_f_res,
                            set_cmd=self._set_f_res)
+
+        # Sequence/pulse parameters
+        self.add_parameter('RO_pulse_delay', units='s',
+                           get_cmd=self._get_RO_pulse_delay,
+                           set_cmd=self._set_RO_pulse_delay)
+        self.add_parameter('RO_pulse_length', units='s',
+                           get_cmd=self._get_RO_pulse_length,
+                           set_cmd=self._set_RO_pulse_length)
+        self.add_parameter('RO_trigger_delay', units='s',
+                           get_cmd=self._get_RO_trigger_delay,
+                           set_cmd=self._set_RO_trigger_delay)
+        self.add_parameter('pulse_separation', units='s',
+                           get_cmd=self._get_pulse_separation,
+                           set_cmd=self._set_pulse_separation)
 
     def calculate_frequency(self, EC=None, EJ=None, assymetry=None,
                             dac_voltage=None, flux=None,
@@ -243,6 +259,30 @@ class Transmon(Qubit):
     def _get_f_res(self):
         return self._f_res
 
+    def _set_RO_pulse_delay(self, val):
+        self._RO_pulse_delay = val
+
+    def _get_RO_pulse_delay(self):
+        return self._RO_pulse_delay
+
+    def _set_RO_pulse_length(self, val):
+        self._RO_pulse_length = val
+
+    def _get_RO_pulse_length(self):
+        return self._RO_pulse_length
+
+    def _set_RO_trigger_delay(self, val):
+        self._RO_trigger_delay = val
+
+    def _get_RO_trigger_delay(self):
+        return self._RO_trigger_delay
+
+    def _set_pulse_separation(self, val):
+        self._pulse_separation = val
+
+    def _get_pulse_separation(self):
+        return self._pulse_separation
+
 
 class CBox_driven_transmon(Transmon):
     '''
@@ -253,8 +293,8 @@ class CBox_driven_transmon(Transmon):
     '''
     def __init__(self, name,
                  LO, cw_source, td_source,
-                 IVVI,
-                 CBox, heterodyne_source,
+                 IVVI, AWG, LutMan,
+                 CBox, heterodyne_instr,
                  MC):
         super().__init__(name)
         # MW-sources
@@ -262,35 +302,69 @@ class CBox_driven_transmon(Transmon):
         self.cw_source = cw_source
         self.td_source = td_source
         self.IVVI = IVVI
-        self.heterodyne_source = heterodyne_source
+        self.LutMan = LutMan
+        self.heterodyne_instr = heterodyne_instr
+        self.AWG = AWG
         self.CBox = CBox
         self.MC = MC
         self.add_parameter('mod_amp_cw', label='RO modulation ampl cw',
                            units='V',
                            get_cmd=self._get_mod_amp_cw,
                            set_cmd=self._set_mod_amp_cw)
+        self.add_parameter('mod_amp_td', label='RO modulation ampl td',
+                           units='V',
+                           get_cmd=self._get_mod_amp_td,
+                           set_cmd=self._set_mod_amp_td)
 
+        self.add_parameter('spec_pow', label='spectroscopy power',
+                           units='dBm',
+                           get_cmd=self._get_spec_pow,
+                           set_cmd=self._set_spec_pow)
+        self.add_parameter('spec_pow_pulsed',
+                           label='pulsed spectroscopy power',
+                           units='dBm',
+                           get_cmd=self._get_spec_pow_pulsed,
+                           set_cmd=self._set_spec_pow_pulsed)
+        self.add_parameter('td_source_pow',
+                           label='Time-domain power',
+                           units='dBm',
+                           get_cmd=self._get_td_source_pow,
+                           set_cmd=self._set_td_source_pow)
+
+        self.add_parameter('IF',
+                           label='inter-modulation frequency', units='Hz',
+                           get_cmd=self._get_IF,
+                           set_cmd=self._set_IF)
+        # Time-domain parameters
+        self.add_parameter('f_pulse_mod',
+                           label='pulse-modulation frequency', units='Hz',
+                           get_cmd=self._get_f_pulsemod,
+                           set_cmd=self._set_f_pulsemod)
+        self.add_parameter('awg_nr', label='CBox awg nr', units='#',
+                           get_cmd=self._get_awg_nr,
+                           set_cmd=self._set_awg_nr)
     def prepare_for_continuous_wave(self):
-        # TODO: Currently hardcoded here. This has to disappear
+
+        self.heterodyne_instr._disable_auto_seq_loading = False
         self.LO.on()
         self.td_source.off()
         self.cw_source.on()
-        self.CBox.set('nr_averages', 2**11)
-        self.heterodyne_source.set('mod_amp', self.mod_amp_cw.get())
-        self.heterodyne_source.set('IF', -20e6)
-
-        self.heterodyne_source.frequency.set(self.f_res.get())
-
+        self.heterodyne_instr.set('mod_amp', self.mod_amp_cw.get())
+        self.heterodyne_instr.set('IF', self.IF.get())
+        self.heterodyne_instr.frequency.set(self.f_res.get())
         self.cw_source.pulsemod_state.set('off')
-
+        self.cw_source.power.set(self.spec_pow.get())
 
     def prepare_for_timedomain(self):
-        # TODO: Currently hardcoded here. This has to disappear
         self.LO.on()
         self.cw_source.off()
         self.td_source.on()
-        self.AWG.set('ch3_amp', .13) #0.125)
-        self.AWG.set('ch4_amp', .13) #0.125)
+        # Set source to fs =f-f_mod such that pulses appear at f = fs+f_mod
+        self.td_source.frequency.set(self.f_qubit.get()
+                                     - self.f_pulse_mod.get())
+        self.td_source.power.set(self.td_source_pow.get())
+        self.AWG.set('ch3_amp', self.mod_amp_td.get())
+        self.AWG.set('ch4_amp', self.mod_amp_td.get())
 
     def find_resonator_frequency(self, use_min=False,
                                  update=True,
@@ -324,9 +398,9 @@ class CBox_driven_transmon(Transmon):
         if MC is None:
             MC = self.MC
         MC.set_sweep_function(pw.wrap_par_to_swf(
-                              self.heterodyne_source.frequency))
+                              self.heterodyne_instr.frequency))
         MC.set_sweep_points(freqs)
-        MC.set_detector_function(det.Heterodyne_probe(self.heterodyne_source))
+        MC.set_detector_function(det.Heterodyne_probe(self.heterodyne_instr))
         MC.run(name='Resonator_scan'+self.msmt_suffix)
         if analyze:
             ma.MeasurementAnalysis(auto=True, close_fig=close_fig)
@@ -336,26 +410,42 @@ class CBox_driven_transmon(Transmon):
         self.prepare_for_continuous_wave()
         if MC is None:
             MC = self.MC
-        if not pulsed:
-            MC.set_sweep_function(pw.wrap_par_to_swf(
-                                  self.cw_source.frequency))
-            MC.set_sweep_points(freqs)
-            MC.set_detector_function(
-                det.Heterodyne_probe(self.heterodyne_source))
-            MC.run(name='spectroscopy'+self.msmt_suffix)
-        elif pulsed:
-            if not ('Pulsed_spec' in self.AWG.setup_filename.get()):
-                st_seqs.Pulsed_spec_seq_RF_mod(
-                    IF=self.IF.get(),
-                    spec_pulse_length=16e-6, marker_interval=30e-6,
-                    RO_pulse_delay=300e-9)
-            self.cw_source.pulsemod_state.set('on')
+        if pulsed:
+            # Redirect to the pulsed spec function
+            return self.measure_pulsed_spectroscopy(freqs,
+                                                    MC, analyze, close_fig)
 
+        MC.set_sweep_function(pw.wrap_par_to_swf(
+                              self.cw_source.frequency))
+        MC.set_sweep_points(freqs)
+        MC.set_detector_function(
+            det.Heterodyne_probe(self.heterodyne_instr))
+        MC.run(name='spectroscopy'+self.msmt_suffix)
 
         if analyze:
             ma.MeasurementAnalysis(auto=True, close_fig=close_fig)
 
+    def measure_pulsed_spectroscopy(self, freqs, MC=None,
+                                    analyze=True, close_fig=False):
+        # This is a trick so I can reuse the heterodyne instr
+        # to do pulsed-spectroscopy
+        self.heterodyne_instr._disable_auto_seq_loading = True
+        if ('Pulsed_spec' not in self.AWG.setup_filename.get()):
+            st_seqs.Pulsed_spec_seq_RF_mod(
+                IF=self.IF.get(),
+                spec_pulse_length=16e-6, marker_interval=30e-6,
+                RO_pulse_delay=self.RO_pulse_delay.get())
+        self.cw_source.pulsemod_state.set('on')
+        self.cw_source.power.set(self.spec_pow_pulsed.get())
 
+        self.AWG.start()
+        self.heterodyne_instr.mod_amp.set(self.mod_amp_td.get())
+        MC.set_sweep_function(pw.wrap_par_to_swf(self.cw_source.frequency))
+        MC.set_sweep_points(freqs)
+        MC.set_detector_function(det.Heterodyne_probe(self.heterodyne_instr))
+        MC.run(name='pulsed-spec'+self.msmt_suffix)
+        if analyze:
+            ma.MeasurementAnalysis(auto=True, close_fig=close_fig)
 
     def measure_resonator_power(self, freqs, mod_amps,
                                 MC=None, analyze=True, close_fig=False):
@@ -367,11 +457,11 @@ class CBox_driven_transmon(Transmon):
         if MC is None:
             MC = self.MC
         MC.set_sweep_functions(
-            [pw.wrap_par_to_swf(self.heterodyne_source.frequency),
-             pw.wrap_par_to_swf(self.heterodyne_source.mod_amp)])
+            [pw.wrap_par_to_swf(self.heterodyne_instr.frequency),
+             pw.wrap_par_to_swf(self.heterodyne_instr.mod_amp)])
         MC.set_sweep_points(freqs)
         MC.set_sweep_points_2D(mod_amps)
-        MC.set_detector_function(det.Heterodyne_probe(self.heterodyne_source))
+        MC.set_detector_function(det.Heterodyne_probe(self.heterodyne_instr))
         MC.run(name='Resonator_power_scan'+self.msmt_suffix, mode='2D')
         if analyze:
             ma.MeasurementAnalysis(auto=True, TwoD=True, close_fig=close_fig)
@@ -382,13 +472,13 @@ class CBox_driven_transmon(Transmon):
         if MC is None:
             MC = self.MC
         MC.set_sweep_functions(
-            [pw.wrap_par_to_swf(self.heterodyne_source.frequency),
+            [pw.wrap_par_to_swf(self.heterodyne_instr.frequency),
              pw.wrap_par_to_swf(
                 self.IVVI['dac{}'.format(self.dac_channel.get())])
              ])
         MC.set_sweep_points(freqs)
         MC.set_sweep_points_2D(dac_voltages)
-        MC.set_detector_function(det.Heterodyne_probe(self.heterodyne_source))
+        MC.set_detector_function(det.Heterodyne_probe(self.heterodyne_instr))
         MC.run(name='Resonator_dac_scan'+self.msmt_suffix, mode='2D')
         if analyze:
             ma.MeasurementAnalysis(auto=True, TwoD=True, close_fig=close_fig)
@@ -396,7 +486,7 @@ class CBox_driven_transmon(Transmon):
     def measure_rabi(self, pulse_amps, n=1,
                      MC=None, analyze=True, close_fig=False,
                      verbose=False):
-        logging.warning('Not tested warning')
+        self.prepare_for_timedomain()
         st_seqs.CBox_multi_pulse_seq(
             IF=self.IF.get(), n_pulses=n,
             pulse_separation=self.pulse_separation.get(),
@@ -404,16 +494,19 @@ class CBox_driven_transmon(Transmon):
             RO_trigger_delay=self.RO_trigger_delay.get(),
             RO_pulse_length=self.RO_pulse_length.get(), verbose=verbose)
         self.AWG.start()
+
         cal_points = [0, 0]
         pulse_amps = cal_points + list(pulse_amps)
         self.CBox.set('AWG0_tape', [1, 1])
         self.CBox.set('AWG1_tape', [1, 1])
-        MC.set_sweep_function(pw.wrap_par_to_swf(self.LutMan.amp180))
-        MC.set_sweep_points(pulse_amps)
-        MC.set_detector_function(det.CBox_single_int_avg_with_LutReload(
-                                 self.CBox, self.LutMan, awg_nrs=[0, 1]))
-        MC.run('Rabi-flipping-{}'.format(n)+self.msmt_suffix)
-        ma.MeasurementAnalysis(auto=True, close_fig=close_fig)
+        self.MC.set_sweep_function(pw.wrap_par_to_swf(self.LutMan.amp180))
+        self.MC.set_sweep_points(pulse_amps)
+        self.MC.set_detector_function(det.CBox_single_int_avg_with_LutReload(
+                                      self.CBox, self.LutMan,
+                                      awg_nrs=[self.awg_nr.get()]))
+        self.MC.run('Rabi-n{}'.format(n)+self.msmt_suffix)
+        if analyze:
+            ma.MeasurementAnalysis(auto=True, close_fig=close_fig)
 
     ###########################
     # Parameter get set commands, should be removed in a later version.
@@ -423,3 +516,69 @@ class CBox_driven_transmon(Transmon):
 
     def _set_mod_amp_cw(self, val):
         self._mod_amp_cw = val
+
+    def _get_IF(self):
+        return self._IF
+
+    def _set_IF(self, val):
+        self._IF = val
+
+    def _get_mod_amp_cw(self):
+        return self._mod_amp_cw
+
+    def _set_mod_amp_cw(self, val):
+        self._mod_amp_cw = val
+
+    def _get_mod_amp_cw(self):
+        return self._mod_amp_cw
+
+    def _set_mod_amp_cw(self, val):
+        self._mod_amp_cw = val
+
+    def _get_mod_amp_cw(self):
+        return self._mod_amp_cw
+
+    def _set_mod_amp_cw(self, val):
+        self._mod_amp_cw = val
+
+    def _get_mod_amp_cw(self):
+        return self._mod_amp_cw
+
+    def _set_mod_amp_cw(self, val):
+        self._mod_amp_cw = val
+
+    def _get_mod_amp_td(self):
+        return self._mod_amp_td
+
+    def _set_mod_amp_td(self, val):
+        self._mod_amp_td = val
+
+    def _get_spec_pow(self):
+        return self._spec_pow
+
+    def _set_spec_pow(self, val):
+        self._spec_pow = val
+
+    def _get_spec_pow_pulsed(self):
+        return self._spec_pow_pulsed
+
+    def _set_spec_pow_pulsed(self, val):
+        self._spec_pow_pulsed = val
+
+    def _get_td_source_pow(self):
+        return self._td_source_pow
+
+    def _set_td_source_pow(self, val):
+        self._td_source_pow = val
+
+    def _get_f_pulsemod(self):
+        return self._f_pulsemod
+
+    def _set_f_pulsemod(self, val):
+        self._f_pulsemod = val
+
+    def _get_awg_nr(self):
+        return self._awg_nr
+
+    def _set_awg_nr(self, val):
+        self._awg_nr = val
