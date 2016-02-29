198ca126c27fbc25837b9789920adc9972f1411d
Adriaan <adriaan.rol@gmail.com>
2016-02-29 14:03:09 +0100

    Added a test instrument

---
+++ instrument_drivers/physical_instruments/dummy_instruments.py
@@ -0,0 +1,38 @@
+
+import numpy as np
+
+from qcodes.instrument.base import Instrument
+from qcodes.utils import validators as vals
+from qcodes.instrument.parameter import ManualParameter
+
+
+class DummyParHolder(Instrument):
+    '''
+    Holds dummy parameters which are get and set able aswell as provides
+    some basic functions that depends on these parameters for testing
+    purposes.
+
+    Located in physical instruments because it mimicks a instrument that
+    talks directly to the hardware.
+    '''
+
+    def __init__(self, name):
+        super().__init__(name)
+
+        # Instrument parameters
+        for parname in ['x', 'y', 'z']:
+            self.add_parameter(parname, units='a.u.',
+                               parameter_class=ManualParameter,
+                               vals=vals.Numbers(), initial_value=0)
+
+        self.add_parameter('noise', units='a.u.',
+                           label='white noise amplitude',
+                           parameter_class=ManualParameter,
+                           vals=vals.Numbers(), initial_value=0)
+
+        self.add_parameter('parabola', units='a.u.',
+                           get_cmd=self._measure_parabola)
+
+    def _measure_parabola(self):
+        return (self.x.get()**2 + self.y.get()**2 + self.z.get()**2 +
+                self.noise.get()*np.random.rand(1))
