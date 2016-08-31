43eec56e227e793eb0f5b6a8b34372e7cd37450a
elrama- <sagastizabal.ramiro@gmail.com>
2016-08-31 16:53:05 +0200

    Added install instructions for ZI UHFLI

---
+++ instrument_drivers/physical_instruments/ZI_UHFLI.py
@@ -0,0 +1,13 @@
+import zhinst.zishell as zis
+import zhinst.ziPython as zi
+import zhinst.utils as zi_utils
+
+"""
+This contains no driver yet! needs to be done.
+
+Installation instructions for Zurich Instrument Libraries.
+1. pip install zhinst (python3 module provided by ZI)
+    (this will put it in site-packages)
+2. manually past zishell.py in the zhinst directory
+3. add zishell.py to the zhinst __init__.py
+"""
