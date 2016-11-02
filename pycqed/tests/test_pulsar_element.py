7d65f2afa363361e24710d403fbf750158902246
Adriaan <adriaan.rol@gmail.com>
2016-11-02 18:09:17 +0100

    working fixpoint frequency problem

--- pycqed/tests/test_pulsar_element.py
+++ pycqed/tests/test_pulsar_element.py
@@ -6,7 +6,6 @@ import unittest
 from pycqed.measurement.waveform_control.pulsar import Pulsar
 from pycqed.measurement.waveform_control import element
 from pycqed.measurement.waveform_control.pulse import SquarePulse
-from pycqed.measurement.pulse_sequences import fluxing_sequences as fsqs
 
 
 class Test_Element(unittest.TestCase):
@@ -32,11 +31,6 @@ class Test_Element(unittest.TestCase):
                                           high=2.0, low=0, offset=0.,
                                           delay=0, active=True)
 
-        # We need to discuss where to store this stuff
-
-        fsqs.kernel_dir_path = 'testing/kernels/'
-        self.kernel_list = ['kernels_rabi/kernel_160208_220046_it1.txt']
-
     def test_basic_element(self):
         test_elt = element.Element('test_elt', pulsar=self.pulsar)
         test_elt.add(SquarePulse(name='dummy_square',
@@ -60,12 +54,34 @@ class Test_Element(unittest.TestCase):
         test_elt.add(SquarePulse(name='dummy_square',
                                  channel='ch1',
                                  amplitude=.3, length=20e-9),
-            refpulse='dummy_square', start=100e-9, refpoint='start')
+                     refpulse='dummy_square', start=100e-9, refpoint='start')
         min_samples = 960
         ch1_wf = test_elt.waveforms()[1]['ch1']
         self.assertEqual(len(ch1_wf), min_samples)
 
         expected_wf = np.zeros(960)
+        expected_wf[100:120] = .3
+        np.testing.assert_array_almost_equal(ch1_wf, expected_wf)
+
+    def test_fixpoint(self):
+        # Fixed point should shift both elements by 2 ns
+        test_elt = element.Element('test_elt', pulsar=self.pulsar)
+        refpulse = SquarePulse(name='dummy_square',
+                               channel='ch1',
+                               amplitude=.5, length=20e-9)
+        test_elt.add(refpulse,  name='dummy_square')
+
+        test_elt.add(SquarePulse(name='dummy_square',
+                                 channel='ch1',
+                                 amplitude=.3, length=20e-9),
+                     fixed_point_freq=200e6,
+                     refpulse='dummy_square', start=98e-9, refpoint='start')
+        min_samples = 960
+        ch1_wf = test_elt.waveforms()[1]['ch1']
+        self.assertEqual(len(ch1_wf), min_samples)
+
+        expected_wf = np.zeros(960)
+        expected_wf[2:22] = .5
         expected_wf[100:120] = .3
         np.testing.assert_array_almost_equal(ch1_wf, expected_wf)
 
