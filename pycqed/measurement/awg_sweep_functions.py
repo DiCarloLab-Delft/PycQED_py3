e817f90ada694248378ff55fe0bc4bd5ffa8d67f
elrama- <sagastizabala.ramiro@gmail.com>
2016-11-07 13:44:19 +0100

    freqxy and cphase seq part II

--- pycqed/measurement/awg_sweep_functions.py
+++ pycqed/measurement/awg_sweep_functions.py
@@ -146,6 +146,39 @@ class chevron_length(swf.Hard_Sweep):
                                     self.flux_pulse_pars,
                                     distortion_dict=self.dist_dict, return_seq=True)
 
+class chevron_cphase_length(swf.Hard_Sweep):
+    def __init__(self, length_vec, mw_pulse_pars,RO_pars,
+                 flux_pulse_pars, cphase_pulse_pars, dist_dict, AWG, upload=True, return_seq=False):
+        super().__init__()
+        self.length_vec = length_vec
+        self.mw_pulse_pars = mw_pulse_pars
+        self.RO_pars = RO_pars
+        self.flux_pulse_pars = flux_pulse_pars
+        self.dist_dict = dist_dict
+        self.upload = upload
+        self.name = 'Chevron'
+        self.parameter_name = 'Time'
+        self.unit = 's'
+        self.return_seq = return_seq
+        self.cphase_pulse_pars = cphase_pulse_pars
+        self.AWG = AWG
+
+    def prepare(self, **kw):
+        if self.upload:
+            fsqs.chevron_seq_cphase(self.length_vec,
+                                    self.mw_pulse_pars,
+                                    self.RO_pars,
+                                    self.flux_pulse_pars,
+                                    self.cphase_pulse_pars,
+                                    distortion_dict=self.dist_dict)
+
+    def pre_upload(self, **kw):
+        self.seq = fsqs.chevron_seq_cphase(self.length_vec,
+                                    self.mw_pulse_pars,
+                                    self.RO_pars,
+                                    self.flux_pulse_pars,
+                                    self.cphase_pulse_pars,
+                                    distortion_dict=self.dist_dict, return_seq=True)
 
 class repeat_swap(swf.Hard_Sweep):
     def __init__(self, rep_max, mw_pulse_pars, RO_pars,
@@ -429,6 +462,30 @@ class Motzoi_XY(swf.Hard_Sweep):
         sweep_pts = np.repeat(motzois, 2)
         self.sweep_points = np.append(sweep_pts,
                                    [motzois[-1]+(motzois[-1]-motzois[-2])]*4)
+
+    def prepare(self, **kw):
+        if self.upload:
+            sqs.Motzoi_XY(motzois=self.sweep_points,
+                          pulse_pars=self.pulse_pars,
+                          RO_pars=self.RO_pars)
+
+
+class Freq_XY(swf.Hard_Sweep):
+    def __init__(self, freqs, pulse_pars, RO_pars, upload=True):
+        '''
+        Measures 2 points per motzoi value specified in freqs and adds 4
+        calibration points to it.
+        '''
+        super().__init__()
+        self.pulse_pars = pulse_pars
+        self.RO_pars = RO_pars
+        self.upload = upload
+        self.name = 'Motzoi_XY'
+        self.parameter_name = 'motzoi'
+        self.unit = ' '
+        sweep_pts = np.repeat(freqs, 2)
+        self.sweep_points = np.append(sweep_pts,
+                                   [freqs[-1]+(freqs[-1]-freqs[-2])]*4)
 
     def prepare(self, **kw):
         if self.upload:
