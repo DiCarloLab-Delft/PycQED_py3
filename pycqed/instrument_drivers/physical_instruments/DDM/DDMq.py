c8adeb32be9caa4d52e884e9d8cb8b2696eb7e4b
NikitaVodyagin <nikita.vodyagin@topic.nl>
2017-01-23 14:16:28 +0100

    TVmode demo

--- pycqed/instrument_drivers/physical_instruments/DDM/DDMq.py
+++ pycqed/instrument_drivers/physical_instruments/DDM/DDMq.py
@@ -84,7 +84,109 @@ class DDMq(SCPIddm):
                                 set_cmd=sholdoff_cmd + ' {}',
                                 vals=vals.Numbers(0,127)
                               )
-            
+            '''
+            Weighted integral + Rotation matrix + TV mode parameters
+            '''
+            swinten_cmd= 'qutech:wint{}:enable'.format(ch_pair)
+            self.add_parameter('ch_pair{}_wint_enable'.format(ch_pair),
+                                label=('enable wighted integral chpair {} '.format(ch_pair)),
+                               #get_cmd=snsamp_cmd + '?',
+                                set_cmd=swinten_cmd
+                                #vals=vals.Numbers(1,4096)
+                              )
+            swintdis_cmd= 'qutech:wint{}:disable'.format(ch_pair)
+            self.add_parameter('ch_pair{}_wint_disable'.format(ch_pair),
+                                label=('disable wighted integral chpair {} '.format(ch_pair)),
+                               #get_cmd=snsamp_cmd + '?',
+                                set_cmd=swintdis_cmd,
+                                #vals=vals.Bool()            
+                              )
+            sintlength_cmd= 'qutech:wint{}:intlength'.format(ch_pair)
+            self.add_parameter('ch_pair{}_wint_intlength'.format(ch_pair),
+                                units='#',
+                                label=('the number of sample pairs that is used for one integration of chpair {} '.format(ch_pair)),
+                                get_cmd=sintlength_cmd + '?',
+                                set_cmd=sintlength_cmd + ' {}',
+                                vals=vals.Numbers(1,2048)
+                              )
+            swintstat_cmd= 'qutech:wint{}:status'.format(ch_pair)
+            self.add_parameter('ch_pair{}_wint_status'.format(ch_pair),
+                                units='#',
+                                label=('weighted integral status of chpair {} '.format(ch_pair)),
+                                get_cmd=swintstat_cmd + '?'
+                                #vals=vals.Numbers(1,2048)
+                              )
+            srotmat00_cmd= 'qutech:rotmat{}:rotmat00'.format(ch_pair)
+            self.add_parameter('ch_pair{}_rotmat_rotmat00'.format(ch_pair),
+                                units='#',
+                                label=('rotation matrix value 00 of chpair {} '.format(ch_pair)),
+                                get_cmd=srotmat00_cmd + '?',
+                                set_cmd=srotmat00_cmd + ' {}'
+                                #vals=vals.Numbers(1,2048)
+                              )
+            srotmat01_cmd= 'qutech:rotmat{}:rotmat01'.format(ch_pair)
+            self.add_parameter('ch_pair{}_rotmat_rotmat01'.format(ch_pair),
+                                units='#',
+                                label=('rotation matrix value 00 of chpair {} '.format(ch_pair)),
+                                get_cmd=srotmat01_cmd + '?',
+                                set_cmd=srotmat01_cmd + ' {}'
+                                #vals=vals.Numbers(1,2048)
+                              )
+            srotmat10_cmd= 'qutech:rotmat{}:rotmat10'.format(ch_pair)
+            self.add_parameter('ch_pair{}_rotmat_rotmat10'.format(ch_pair),
+                                units='#',
+                                label=('rotation matrix value 10 of chpair {} '.format(ch_pair)),
+                                get_cmd=srotmat10_cmd + '?',
+                                set_cmd=srotmat10_cmd + ' {}'
+                                #vals=vals.Numbers(1,2048)
+                              )
+            srotmat11_cmd= 'qutech:rotmat{}:rotmat11'.format(ch_pair)
+            self.add_parameter('ch_pair{}_rotmat_rotmat11'.format(ch_pair),
+                                units='#',
+                                label=('rotation matrix value 11 of chpair {} '.format(ch_pair)),
+                                get_cmd=srotmat11_cmd + '?',
+                                set_cmd=srotmat11_cmd + ' {}'
+                                #vals=vals.Numbers(1,2048)
+                              )
+            '''
+            TV mode parameters
+            '''
+            sintavg_cmd= 'qutech:tvmode{}:naverages'.format(ch_pair)
+            self.add_parameter('ch_pair{}_tvmode_naverages'.format(ch_pair),
+                                units='#',
+                                label=('the number of integration averages of chpair {} '.format(ch_pair)),
+                                get_cmd=sintavg_cmd + '?',
+                                set_cmd=sintavg_cmd + ' {}',
+                                vals=vals.Numbers(1,1024)
+                              )
+            stvseg_cmd= 'qutech:tvmode{}:nsegments'.format(ch_pair)
+            self.add_parameter('ch_pair{}_tvmode_nsegments'.format(ch_pair),
+                                units='#',
+                                label=('the number of TV segments of chpair {} '.format(ch_pair)),
+                                get_cmd=stvseg_cmd + '?',
+                                set_cmd=stvseg_cmd + ' {}',
+                                vals=vals.Numbers(1,256)
+                              )
+            stven_cmd= 'qutech:tvmode{}:enable'.format(ch_pair)
+            self.add_parameter('ch_pair{}_tvmode_enable'.format(ch_pair),
+                                label=('enable tv mode chpair {} '.format(ch_pair)),
+                               #get_cmd=snsamp_cmd + '?',
+                                set_cmd=stven_cmd
+                                #vals=vals.Numbers(1,4096)
+                              )
+            stvdis_cmd= 'qutech:tvmode{}:disable'.format(ch_pair)
+            self.add_parameter('ch_pair{}_tvmode_disable'.format(ch_pair),
+                                label=('disable tv mode chpair {} '.format(ch_pair)),
+                               #get_cmd=snsamp_cmd + '?',
+                                set_cmd=stvdis_cmd,
+                                #vals=vals.Bool()            
+                              )
+            self.add_parameter('ch_pair{}_tvmode_data'.format(ch_pair),
+                                label=('get TV data ch {} '.format(ch_pair)),
+                                get_cmd=self._gen_ch_get_func(self.getTVdata,ch_pair)
+                       
+                                #vals=vals.Numbers(-128,127)
+                              )
         for i in range(self.device_descriptor.numChannels):
             ch=i+1
             sgetdata_cmd= 'qutech:inputavg{}:data'.format(ch)
@@ -94,7 +196,14 @@ class DDMq(SCPIddm):
                        
                                 vals=vals.Numbers(-128,127)
                               )
-
+            srotres_cmd= 'qutech:wintrot{}:result'.format(ch)
+            self.add_parameter('ch{}_wintrot_result'.format(ch),
+                                units='#',
+                                label=('rotated integration result of ch {} '.format(ch)),
+                                get_cmd=srotres_cmd + '?'
+                                #vals=vals.Numbers(1,2048)
+                              )
+            
     def getInputAverage(self,ch):
         
         self.write('qutech:inputavg{:d}:data? '.format(ch))
@@ -103,7 +212,27 @@ class DDMq(SCPIddm):
         print ('inputavgLen in bytes {:d}'.format(len(binBlock)))
         inputavg = np.frombuffer(binBlock,dtype=np.float32)
         return inputavg
+    def getTVdata(self,ch_pair):
+        
+        self.write('qutech:tvmode{:d}:data? '.format(ch_pair))
+        binBlock = self.binBlockRead()
+        #print(binBlock)
+        print ('TV data in bytes {:d}'.format(len(binBlock)))
+        tvmodedata = np.frombuffer(binBlock,dtype=np.float32)
+        return tvmodedata
+    def sendWeightData(self, ch, weight):
+        # generate the binblock
+        if 1:   # high performance
+            arr = np.asarray(weight, dtype=np.float32)
+            binBlock = arr.tobytes()
+        else:   # more generic
+            binBlock = b''
+            for i in range(len(weight)):
+                binBlock = binBlock + struct.pack('<f', weight[i])
 
+        # write binblock
+        hdr = 'qutech:wint{}:data'.format(ch)
+        self.binBlockWrite(binBlock, hdr)
     def _gen_ch_get_func(self, fun, ch):
         def get_func():
             return fun(ch)
