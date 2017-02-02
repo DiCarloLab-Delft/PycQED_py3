cbc43cf12c324d70b91cce23c572032296bfa3ce
Adriaan <adriaan.rol@gmail.com>
2017-02-02 23:34:36 +0100

    And now I stop fixing tomo test

--- pycqed/tests/test_tomo.py
+++ pycqed/tests/test_tomo.py
@@ -29,6 +29,26 @@ try:
                                     target_bell=0,
                                     MLE=False)
 
+    class Test_tomo_helpers(unittest.TestCase):
+
+        def test_bell_fids(self):
+            pass
+
+        def test_bell_paulis(self):
+
+            bell_0 = [1] + [0]*3 + [0]*3 + [-1, 0, 0, 0, 1, 0, 0, 0, 1]
+            bell_1 = [1] + [0]*3 + [0]*3 + [1, 0, 0, 0, -1, 0, 0, 0, 1]
+            bell_2 = [1] + [0]*3 + [0]*3 + [-1, 0, 0, 0, -1, 0, 0, 0, -1]
+            bell_3 = [1] + [0]*3 + [0]*3 + [1, 0, 0, 0, 1, 0, 0, 0, -1]
+            expected_bells = [bell_0, bell_1, bell_2, bell_3]
+            # Test if the definition or order has not changed
+            for bell_idx in range(4):
+                bell_paulis = tomo.get_bell_pauli_exp(
+                    bell_idx, theta_q0=0, theta_q1=0)
+                np.testing.assert_array_equal(
+                    expected_bells[bell_idx], bell_paulis)
+
+
 except ImportError as e:
     if str(e).find('qutip') >= 0:
         class Test_tomo_analysis_skipped(unittest.TestCase):
@@ -39,25 +59,6 @@ except ImportError as e:
     else:
         raise
 #
-
-class Test_tomo_helpers(unittest.TestCase):
-
-    def test_bell_fids(self):
-        pass
-
-    def test_bell_paulis(self):
-
-        bell_0 = [1] + [0]*3 + [0]*3 + [-1, 0, 0, 0, 1, 0, 0, 0, 1]
-        bell_1 = [1] + [0]*3 + [0]*3 + [1, 0, 0, 0, -1, 0, 0, 0, 1]
-        bell_2 = [1] + [0]*3 + [0]*3 + [-1, 0, 0, 0, -1, 0, 0, 0, -1]
-        bell_3 = [1] + [0]*3 + [0]*3 + [1, 0, 0, 0, 1, 0, 0, 0, -1]
-        expected_bells = [bell_0, bell_1, bell_2, bell_3]
-        # Test if the definition or order has not changed
-        for bell_idx in range(4):
-            bell_paulis = tomo.get_bell_pauli_exp(
-                bell_idx, theta_q0=0, theta_q1=0)
-            np.testing.assert_array_equal(
-                expected_bells[bell_idx], bell_paulis)
 
 
 
