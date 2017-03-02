282cb4f0bb08d8d5cb7835fe5db3972c86b56c29
elrama- <adriaan.rol@gmail.com>
2017-03-02 19:50:13 +0100

    Updates on flux control
    Relates to #128
    Example of setting up the flux control
    Update to the flux control instrument

--- pycqed/instrument_drivers/meta_instrument/Flux_Control.py
+++ pycqed/instrument_drivers/meta_instrument/Flux_Control.py
@@ -6,7 +6,7 @@ import qcodes as qc
 from qcodes.instrument.base import Instrument
 from qcodes.utils import validators as vals
 from qcodes.instrument.parameter import ManualParameter
-
+from pycqed.instrument_drivers.pq_parameters import InstrumentParameter
 # from pycqed.analysis.analysis_toolbox import calculate_transmon_transitions
 # from pycqed.analysis import analysis_toolbox as a_tools
 # from pycqed.measurement import detector_functions as det
@@ -15,45 +15,42 @@ from qcodes.instrument.parameter import ManualParameter
 
 
 class Flux_Control(Instrument):
-
-    '''
-
-    Flux Control object
-
-    '''
-
     def __init__(self, name, num_channels, IVVI=None, **kw):
         super().__init__(name, **kw)
 
-        self.IVVI = IVVI
-
-        # Add parameters
+        self.add_parameter('IVVI',
+                           initial_value=IVVI,
+                           parameter_class=InstrumentParameter)
+        trnsf_mat_docst = 'Converts dac voltages to virtual flux'
+        # Ramiro will expand this to include the proper equations
         self.add_parameter('transfer_matrix',
                            label='Transfer Matrix',
-                           set_cmd=self.do_set_transfer_matrix,
-                           get_cmd=self.do_get_transfer_matrix,
-                           vals=vals.Anything())
+                           docstring=trnsf_mat_docst,
+                           parameter_class=ManualParameter,
+                           vals=vals.Arrays())
+        self._inv_transfer_matrix = None
         self.add_parameter('inv_transfer_matrix',
-                           label='Transfer Matrix',
-                           set_cmd=self.do_set_inv_transfer_matrix,
-                           get_cmd=self.do_get_inv_transfer_matrix,
-                           vals=vals.Anything())
-        self.add_parameter('flux_offsets', unit='Phi_0',
-                           label='Flux offsets',
-                           set_cmd=self.do_set_flux_offsets,
-                           get_cmd=self.do_get_flux_offsets,
-                           vals=vals.Anything())
-        self.add_parameter('flux_vector', unit='Phi_0',
-                           label='Linear transformation coefficients',
-                           set_cmd=self.do_set_flux_vector,
-                           get_cmd=self.do_get_flux_vector,
-                           vals=vals.Anything())
+                           label='Inverse transfer Matrix',
+                           docstring=('Returns the inverse of the transfer'
+                                      ' matrix, unless explictly specified'),
+                           set_cmd=self._do_set_inv_transfer_matrix,
+                           get_cmd=self._do_get_inv_transfer_matrix,
+                           vals=vals.Arrays())
+        self.add_parameter('dac_offsets', unit='mV',
+                           label='Dac offsets',
+                           docstring=('Offsets in mV corresponding to setting'
+                                      ' all qubits to the'
+                                      ' sweetspot. N.B. the order here is the'
+                                      ' same as the flux vector.'),
+                           parameter_class=ManualParameter,
+                           vals=vals.Arrays())
+
         self.add_parameter('dac_mapping',
                            label='Linear transformation coefficients',
-                           set_cmd=self.do_set_dac_mapping,
-                           get_cmd=self.do_get_dac_mapping,
-                           vals=vals.Anything())
+                           parameter_class=ManualParameter,
+                           vals=vals.Arrays())
 
+        self._flux_vector = np.zeros(num_channels)
         for i in range(0, num_channels):
             self.add_parameter(
                 'flux{}'.format(i),
@@ -61,57 +58,44 @@ class Flux_Control(Instrument):
                 unit=r'$\Phi_0$',
                 get_cmd=self._gen_ch_get_func(self._get_flux, i),
                 set_cmd=self._gen_ch_set_func(self._set_flux, i),
-                vals=vals.Numbers(-10, 10.))
+                vals=vals.Numbers())
 
     def _set_flux(self, id_flux, val):
-        current_flux = self.flux_vector()
+        current_flux = self._flux_vector
         new_flux = current_flux
         new_flux[id_flux] = val
-        self.flux_vector(new_flux)
+        self._do_set_flux_vector(new_flux)
 
     def _get_flux(self, id_flux):
-        val = self.flux_vector()
+        val = self._flux_vector
         return val[id_flux]
 
-    def do_set_transfer_matrix(self, matrix):
-        self._transfer_matrix = matrix
-
-    def do_get_transfer_matrix(self):
-        return self._transfer_matrix
-
-    def do_set_inv_transfer_matrix(self, matrix):
+    def _do_set_inv_transfer_matrix(self, matrix):
         self._inv_transfer_matrix = matrix
 
-    def do_get_inv_transfer_matrix(self):
-        return self._inv_transfer_matrix
+    def _do_get_inv_transfer_matrix(self):
+        if self._inv_transfer_matrix is None:
+            return np.linalg.inv(self.transfer_matrix())
+        else:
+            return self._inv_transfer_matrix
 
-    def do_set_flux_offsets(self, vector):
-        self._flux_offsets = vector
-
-    def do_get_flux_offsets(self):
-        return self._flux_offsets
-
-    def do_set_flux_vector(self, vector):
-        currents = np.dot(self._inv_transfer_matrix,
-                          (vector-self._flux_offsets))
-        for i in range(len(self._dac_mapping)):
-            self.IVVI._set_dac(self._dac_mapping[i], currents[i])
+    def _do_set_flux_vector(self, vector):
+        IVVI = self.IVVI.get_instr()
+        currents = np.dot(self.inv_transfer_matrix(),
+                          vector) + self.dac_offsets()
+        for i in range(len(self.dac_mapping())):
+            IVVI._set_dac(self.dac_mapping()[i], currents[i])
         return currents
 
-    def do_get_flux_vector(self):
-        currents = np.zeros(len(self._dac_mapping))
-        for i in range(len(self._dac_mapping)):
-            currents[i] = self.IVVI._get_dac(self._dac_mapping[i])
-        flux_vector = np.dot(
-            self._transfer_matrix, currents) + self._flux_offsets
-        return flux_vector
+    def _do_get_flux_vector(self):
+        IVVI = self.IVVI.get_instr()
+        currents = np.zeros(len(self.dac_mapping()))
+        for i in range(len(self.dac_mapping())):
+            currents[i] = IVVI._get_dac(self.dac_mapping()[i])
+        self._flux_vector = np.dot(
+            self._transfer_matrix, currents - self.dac_offsets())
 
-    def do_set_dac_mapping(self, vector):
-        self._dac_mapping = vector
-
-    def do_get_dac_mapping(self):
-        return self._dac_mapping
-
+        return self._flux_vector
 
     def _gen_ch_set_func(self, fun, ch):
         def set_func(val):
