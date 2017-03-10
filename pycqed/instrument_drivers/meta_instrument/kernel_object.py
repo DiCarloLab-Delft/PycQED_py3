8c0f4f09a3a11fe2f44f44b10f40e13784bf4a9e
elrama- <adriaan.rol@gmail.com>
2017-03-10 09:35:59 +0100

    made kernel object work with moved version of config param

--- pycqed/instrument_drivers/meta_instrument/kernel_object.py
+++ pycqed/instrument_drivers/meta_instrument/kernel_object.py
@@ -11,7 +11,7 @@ from pycqed.measurement.kernel_functions import kernel_generic, htilde_bounce, \
     heaviside, kernel_generic2
 
 import pycqed.measurement.kernel_functions as kf
-
+from pycqed.instrument_drivers.pq_parameters import ConfigParameter
 
 class Distortion(Instrument):
 
@@ -239,50 +239,3 @@ class Distortion(Instrument):
 
         return self._precalculated_kernel
 
-
-class ConfigParameter(ManualParameter):
-    # TODO: move this to qcodes as a pull request
-
-    """
-    Define one parameter that reflects a manual configuration setting.
-
-    Args:
-        name (string): the local name of this parameter
-
-        instrument (Optional[Instrument]): the instrument this applies to,
-            if any.
-
-        initial_value (Optional[string]): starting value, the
-            only invalid value allowed, and None is only allowed as an initial
-            value, it cannot be set later
-
-        **kwargs: Passed to Parameter parent class
-    """
-
-    def __init__(self, name, instrument=None, initial_value=None, **kwargs):
-        super().__init__(name=name, **kwargs)
-        self._instrument = instrument
-        # if the instrument does not have _config_changed attribute creates it
-        if not hasattr(self._instrument, '_config_changed'):
-            self._instrument._config_changed = True
-        self._meta_attrs.extend(['instrument', 'initial_value'])
-        logging.warning('use the version of this parameter in pq_parameters')
-        if initial_value is not None:
-            self.validate(initial_value)
-            self._save_val(initial_value)
-
-    def set(self, value):
-        """
-        Validate and saves value.
-        If the value is different from the latest value it sets the
-        Args:
-            value (any): value to validate and save
-        """
-        self.validate(value)
-        if value != self._latest()['value']:
-            self._instrument._config_changed = True
-        self._save_val(value)
-
-    def get(self):
-        """ Return latest value"""
-        return self._latest()['value']
