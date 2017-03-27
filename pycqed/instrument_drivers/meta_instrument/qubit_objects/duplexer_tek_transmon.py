4ef07bcd601550aca812f7602bc96af6b571f7de
elrama- <adriaan.rol@gmail.com>
2017-03-27 15:36:18 +0200

    Updates to duplexer transmon object

--- pycqed/instrument_drivers/meta_instrument/qubit_objects/duplexer_tek_transmon.py
+++ pycqed/instrument_drivers/meta_instrument/qubit_objects/duplexer_tek_transmon.py
@@ -2,30 +2,21 @@ from .Tektronix_driven_transmon import Tektronix_driven_transmon
 from math import gcd
 from qcodes.utils import validators as vals
 from qcodes.instrument.parameter import ManualParameter
+from pycqed.instrument_drivers.pq_parameters import InstrumentParameter
 
 
 class Duplexer_tek_transmon(Tektronix_driven_transmon):
-    shared_kwargs = ['LO', 'cw_source', 'td_source', 'IVVI', 'AWG', 'CBox',
-                     'Mux',
-                     'heterodyne_instr', 'rf_RO_source', 'MC']
 
-    def __init__(self, name,
-                 LO, cw_source, td_source,
-                 IVVI, AWG, CBox, Mux,
-                 heterodyne_instr, MC, rf_RO_source=None, **kw):
+    def __init__(self, name, **kw):
         """
         Basic Duplexer controlled transmon.
         This splits up the I and Q qaudratures over two channels on the
         duplexer. It does not support gated pulses yet but this should be
         implemntable
         """
+        super().__init__(name, **kw)
 
-        super().__init__(name, LO=LO, cw_source=cw_source, td_source=td_source,
-                         IVVI=IVVI, AWG=AWG, CBox=CBox,
-                         heterodyne_instr=heterodyne_instr,
-                         MC=MC, rf_RO_source=rf_RO_source, **kw)
-
-        self.Mux = Mux
+        self.add_parameter('VSM', parameter_class=InstrumentParameter)
         # Delete any unused parameters from the parent class
         del self.parameters['phi_skew']
         del self.parameters['alpha']
@@ -106,42 +97,44 @@ class Duplexer_tek_transmon(Tektronix_driven_transmon):
         self.td_source.frequency.set(self.f_qubit.get()
                                      - self.f_pulse_mod.get())
         # setting Mux parameters
-        self.Mux.set(self.Mux_G_ch()+'_switch', 'ON')
-        self.Mux.set(self.Mux_D_ch()+'_switch', 'ON')
-        self.Mux.set(self.Mux_G_ch()+'_attenuation', self.Mux_G_att())
-        self.Mux.set(self.Mux_D_ch()+'_attenuation', self.Mux_D_att())
-        self.Mux.set(self.Mux_G_ch()+'_phase', self.Mux_G_phase())
-        self.Mux.set(self.Mux_D_ch()+'_phase', self.Mux_D_phase())
+        self.VSM.get_instr().set(self.Mux_G_ch()+'_switch', 'ON')
+        self.VSM.get_instr().set(self.Mux_D_ch()+'_switch', 'ON')
+        self.VSM.get_instr().set(
+            self.Mux_G_ch()+'_attenuation', self.Mux_G_att())
+        self.VSM.get_instr().set(
+            self.Mux_D_ch()+'_attenuation', self.Mux_D_att())
+        self.VSM.get_instr().set(self.Mux_G_ch()+'_phase', self.Mux_G_phase())
+        self.VSM.get_instr().set(self.Mux_D_ch()+'_phase', self.Mux_D_phase())
 
         # Use resonator freq unless explicitly specified
         if self.f_RO.get() is None:
             f_RO = self.f_res.get()
         else:
             f_RO = self.f_RO.get()
-        self.LO.frequency.set(f_RO - self.f_RO_mod.get())
-        self.td_source.power.set(self.td_source_pow.get())
+        self.LO.get_instr().frequency.set(f_RO - self.f_RO_mod.get())
+        self.td_source.get_instr().power.set(self.td_source_pow.get())
         self.get_pulse_pars()
 
-        self.AWG.set(self.pulse_GI_channel.get()+'_offset',
-                     self.pulse_GI_offset.get())
-        self.AWG.set(self.pulse_GQ_channel.get()+'_offset',
-                     self.pulse_GQ_offset.get())
+        self.AWG.get_instr().set(self.pulse_GI_channel.get()+'_offset',
+                                 self.pulse_GI_offset.get())
+        self.AWG.get_instr().set(self.pulse_GQ_channel.get()+'_offset',
+                                 self.pulse_GQ_offset.get())
 
-        self.AWG.set(self.pulse_DI_channel.get()+'_offset',
-                     self.pulse_DI_offset.get())
-        self.AWG.set(self.pulse_DQ_channel.get()+'_offset',
-                     self.pulse_DQ_offset.get())
+        self.AWG.get_instr().set(self.pulse_DI_channel.get()+'_offset',
+                                 self.pulse_DI_offset.get())
+        self.AWG.get_instr().set(self.pulse_DQ_channel.get()+'_offset',
+                                 self.pulse_DQ_offset.get())
 
         if self.RO_pulse_type.get() is 'MW_IQmod_pulse':
-            self.AWG.set(self.RO_I_channel.get()+'_offset',
-                         self.RO_I_offset.get())
-            self.AWG.set(self.RO_Q_channel.get()+'_offset',
-                         self.RO_Q_offset.get())
+            self.AWG.get_instr().set(self.RO_I_channel.get()+'_offset',
+                                     self.RO_I_offset.get())
+            self.AWG.get_instr().set(self.RO_Q_channel.get()+'_offset',
+                                     self.RO_Q_offset.get())
         elif self.RO_pulse_type.get() is 'Gated_MW_RO_pulse':
-            self.rf_RO_source.on()
-            self.rf_RO_source.pulsemod_state.set('on')
-            self.rf_RO_source.frequency.set(self.f_RO.get())
-            self.rf_RO_source.power.set(self.RO_pulse_power.get())
+            self.RF_RO_source.get_instr().on()
+            self.RF_RO_source.get_instr().pulsemod_state.set('on')
+            self.RF_RO_source.get_instr().frequency.set(self.f_RO.get())
+            self.RF_RO_source.get_instr().power.set(self.RO_pulse_power.get())
 
     def get_pulse_pars(self):
         self.pulse_pars = {
@@ -171,8 +164,6 @@ class Duplexer_tek_transmon(Tektronix_driven_transmon):
             'length': self.RO_pulse_length.get(),
             'pulse_delay': self.RO_pulse_delay.get(),
             'mod_frequency': self.f_RO_mod.get(),
-            'fixed_point_frequency': gcd(int(self.f_RO_mod()),
-                                         int(self.f_JPA_pump_mod())),
             'acq_marker_delay': self.RO_acq_marker_delay.get(),
             'acq_marker_channel': self.RO_acq_marker_channel.get(),
             'phase': 0,
