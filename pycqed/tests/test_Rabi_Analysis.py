d4a191208c1771c394752d0a944a22733205cf72
fluthi <f.luthi@tudelft.nl>
2017-04-13 13:17:39 +0200

    Update Rabi Analysis test

--- pycqed/tests/test_Rabi_Analysis.py
+++ pycqed/tests/test_Rabi_Analysis.py
@@ -5,14 +5,14 @@ import os
 from pycqed.analysis import measurement_analysis as ma
 from pycqed.analysis import analysis_toolbox as a_tools
 
-class Test_qubitspec_analysis(unittest.TestCase):
+class Test_Rabi_analysis(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
         self.datadir = os.path.join(pq.__path__[0], 'tests', 'test_data')
         ma.a_tools.datadir = self.datadir
-        self.rabis = [self.rabis.append(ma.Rabi_Analysis(timestamp='20170412_185618')),
-            self.rabis.append(ma.Rabi_Analysis(timestamp='20170412_183928'))]
+        self.rabis = [ma.Rabi_Analysis(timestamp='20170412_185618'),
+            ma.Rabi_Analysis(timestamp='20170412_183928')]
 
 
 
@@ -22,9 +22,9 @@ class Test_qubitspec_analysis(unittest.TestCase):
         for ii in range(len(self.rabis)):
             rabi_an = self.rabis[ii]
             for tt in range(2):
-                rabi_freq = rabi_an.fit_res[tt].values['frequency']
-                freq_low = 0.69
-                freq_high = 0.8
+                rabi_amp = rabi_an.fit_res[tt].values['period']/2.
+                amp_low = 0.69
+                amp_high = 0.8
 
-                self.assertGreaterEqual(rabi_freq, freq_low)
-                self.assertGreaterEqual(freq_high, rabi_freq)
+                self.assertGreaterEqual(rabi_amp, amp_low)
+                self.assertGreaterEqual(amp_high, rabi_amp)
