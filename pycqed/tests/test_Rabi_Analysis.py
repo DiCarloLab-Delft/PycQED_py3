5473fc3e2b2d5baaea59539d235abbcfc9c27b92
fluthi <f.luthi@tudelft.nl>
2017-04-13 13:52:59 +0200

    More to the Rabi test
    Issue #185

--- pycqed/tests/test_Rabi_Analysis.py
+++ pycqed/tests/test_Rabi_Analysis.py
@@ -12,7 +12,8 @@ class Test_Rabi_analysis(unittest.TestCase):
         self.datadir = os.path.join(pq.__path__[0], 'tests', 'test_data')
         ma.a_tools.datadir = self.datadir
         self.rabis = [ma.Rabi_Analysis(timestamp='20170412_185618'),
-            ma.Rabi_Analysis(timestamp='20170412_183928')]
+            ma.Rabi_Analysis(timestamp='20170412_183928'),
+            ma.Rabi_Analysis(timestamp='20170413_134244')]
 
 
 
@@ -23,7 +24,7 @@ class Test_Rabi_analysis(unittest.TestCase):
             rabi_an = self.rabis[ii]
             for tt in range(2):
                 rabi_amp = rabi_an.fit_res[tt].values['period']/2.
-                amp_low = 0.69
+                amp_low = 0.63
                 amp_high = 0.8
 
                 self.assertGreaterEqual(rabi_amp, amp_low)
