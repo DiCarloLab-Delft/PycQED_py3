6ff836d6cfcd1a63ec07c073c4dd8b95244f84db
Livio Ciorciaro <livioc@ethz.ch>
2017-06-02 19:47:48 +0200

    Bugfixes for scope driver

--- pycqed/instrument_drivers/physical_instruments/RTO1024.py
+++ pycqed/instrument_drivers/physical_instruments/RTO1024.py
@@ -1,18 +1,19 @@
 import qcodes as qcodes
 import numpy as np
 from time import sleep
-from qcodes.selfument import visa
+from qcodes.instrument import visa
 from qcodes.utils import validators as vals
 from qcodes.instrument.parameter import ManualParameter
 
-class RTO1024_scope(VisaInstrument):
+class RTO1024_scope(visa.VisaInstrument):
     '''
     Instrument driver for the Rohde-Schwarz RTO1024 oscilloscope.
     Currently only able to measure signal on Channel 1.
     '''
     def __init__(self, name, address=None, timeout=5, terminator='',
                  **kwargs):
-        super().__init__(name, address, timeout, terminator, **kwargs)
+        super().__init__(name=name, address=address, timeout=timeout,
+                         terminator=terminator, **kwargs)
 
         self.add_parameter('num_averages',
                            docstring='Number of averages for measuring '
@@ -28,7 +29,7 @@ class RTO1024_scope(VisaInstrument):
                            parameter_class=ManualParameter)
         self.add_parameter('trigger_ch',
                            docstring='Trigger channel',
-                           vals=vals.ints(1, 4),
+                           vals=vals.Ints(1, 4),
                            initial_value=2,
                            parameter_class=ManualParameter)
 
@@ -43,27 +44,28 @@ class RTO1024_scope(VisaInstrument):
                                 to trigger (in seconds)
             acq_rate   (int): acquisition rate
         '''
+        # NOTE: I think these settings don't work at the moment.
         opc = self.visa_handle.ask('STOP;*OPC?')
         self.visa_handle.write('TRIGger1:SOURce CHAN{}'
-                               .format(self.trigger_ch))
-        self.visa_handle.write('ACQuire:SRATe 10000000000')
+                               .format(self.trigger_ch()))
+        self.visa_handle.write('ACQuire:SRATe {}'.format(acq_rate))
         self.visa_handle.write('EXPort:WAVeform:FASTexport ON')
         self.visa_handle.write('EXPort:WAVeform:INCXvalues ON')
         self.visa_handle.write('CHANnel1:WAVeform1:STATe 1')
-        self.visa_handle.write('EXPort:WAVeform:STARt %s'%str(start))
-        self.visa_handle.write('EXPort:WAVeform:STOP %s'%str(stop))
-        sampling_rate = float(self.visa_handle.ask('ACQuire:POINts:ARATe?'))*1e-9
+        self.visa_handle.write('EXPort:WAVeform:STARt {}'.format(t_start))
+        self.visa_handle.write('EXPort:WAVeform:STOP {}'.format(t_stop))
+        self.sampling_rate = float(self.visa_handle.ask('ACQuire:POINts:ARATe?'))*1e-9
 
-    def measure_trace():
+    def measure_trace(self):
         '''
         Measures trace num_averages times.
         Returns x_values, y_values as numpy arrays.
         '''
         # defines the acquisition no. for run single mode
-        self.visa_handle.write('ACQuire:COUNt %s'%str(self.num_averages))
+        self.visa_handle.write('ACQuire:COUNt %s'%str(self.num_averages()))
         self.visa_handle.write('RUNSingle')
         # Wait until measurement finishes before extracting data.
-        sleep(num_averages * self.meas_interval + 1)
+        sleep(self.num_averages() * self.trigger_interval() + 1)
         self.visa_handle.write('EXPort:WAVeform:SOURce C1W1')
         self.visa_handle.write('CHANnel1:ARIThmetics AVERage')
         ret_str = self.visa_handle.ask('CHANNEL1:WAVEFORM1:DATA?')
@@ -71,5 +73,4 @@ class RTO1024_scope(VisaInstrument):
         array = np.double(array)
         x_values = array[::2]
         y_values = array[1::2]
-        print(array)
         return x_values, y_values
