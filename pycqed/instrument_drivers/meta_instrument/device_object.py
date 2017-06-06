4668524cc2c160678409540c8212e4c8c804c7f6
Adriaan <adriaan.rol@gmail.com>
2017-06-06 21:00:31 +0200

    Integration of multiplexed RO in the device object

--- pycqed/instrument_drivers/meta_instrument/device_object.py
+++ pycqed/instrument_drivers/meta_instrument/device_object.py
@@ -2,7 +2,8 @@ from qcodes.instrument.base import Instrument
 from qcodes.utils import validators as vals
 from qcodes.instrument.parameter import ManualParameter
 from pycqed.instrument_drivers.pq_parameters import InstrumentParameter
-
+from pycqed.analysis import  multiplexed_RO_analysis as mra
+from pycqed.measurement.waveform_control_CC import multi_qubit_module_CC as mqmc
 
 class DeviceObject(Instrument):
 
@@ -16,6 +17,7 @@ class DeviceObject(Instrument):
 
         self.add_parameter('acquisition_instrument',
                            parameter_class=InstrumentParameter)
+
         sc_docstr = (
             'Instrument responsible for controlling the waveform sequences. '
             'This is currently either a tek5014 AWG or a CBox.')
@@ -105,3 +107,84 @@ class DeviceObject(Instrument):
             q.get_operation_dict(operation_dict)
         operation_dict['sequencer_config'] = self.sequencer_config()
         return operation_dict
+
+    def calibrate_mux_RO(self):
+        raise NotImplementedError
+
+
+class TwoQubitDevice(DeviceObject):
+
+    def __init__(self, name, **kw):
+        super().__init__(name, **kw)
+        self.add_parameter('RO_LutManMan',
+                           docstring='Used for generating multiplexed RO pulses',
+                           parameter_class=InstrumentParameter)
+
+        self.add_parameter('RO_LO_freq', unit='Hz',
+            docstring=('Frequency of the common LO for all RO pulses.'),
+            parameter_class=ManualParameter)
+
+    def calibrate_mux_RO(self, calibrate_optimal_weights=True):
+
+        RO_LMM = self.RO_LutManMan.get_instr()
+        UHFQC = self.acquisition_instrument.get_instr()
+
+        q0 = self.find_instrument(self.qubits()[0])
+        q1 = self.find_instrument(self.qubits()[1])
+
+
+        # Set the modulation frequencies so that the LO's match
+        q0.f_RO_mod(q0.f_RO() - self.RO_LO_freq())
+        q1.f_RO_mod(q1.f_RO() - self.RO_LO_freq())
+
+        # Update parameters
+        q0.RO_pulse_type('IQmod_multiplexed_UHFQC')
+        q1.RO_pulse_type('IQmod_multiplexed_UHFQC')
+
+        q1.prepare_for_timedomain()
+        q0.prepare_for_timedomain()
+        RO_LMM.acquisition_delay(q0.RO_acq_marker_delay())
+
+        # Generate multiplexed pulse
+        multiplexed_wave = [[q0.RO_LutMan(), 'M_square'],
+                            [q1.RO_LutMan(), 'M_square']]
+        RO_LMM.generate_multiplexed_pulse(multiplexed_wave)
+        RO_LMM.load_pulse_onto_AWG_lookuptable('Multiplexed_pulse')
+
+        if calibrate_optimal_weights:
+            q0.calibrate_optimal_weights(analyze=True, verify=False)
+            q1.calibrate_optimal_weights(analyze=True, verify=False)
+
+        UHFQC.quex_trans_offset_weightfunction_0(0)
+        UHFQC.quex_trans_offset_weightfunction_1(0)
+        UHFQC.upload_transformation_matrix([[1, 0], [0, 1]])
+
+        mqmc.measure_two_qubit_ssro(self, q0.name, q1.name, no_scaling=True,
+                                   result_logging_mode='lin_trans')
+
+        res_dict = mra.two_qubit_ssro_fidelity(
+            label='{}_{}'.format(q0.name, q1.name),
+            qubit_labels=[q0.name, q1.name])
+        V_offset_cor = res_dict['V_offset_cor']
+        UHFQC.quex_trans_offset_weightfunction_0(V_offset_cor[0])
+        UHFQC.quex_trans_offset_weightfunction_1(V_offset_cor[1])
+
+        # Does not work because axes are not normalized
+        UHFQC.upload_transformation_matrix(res_dict['mu_matrix_inv'])
+
+        mqmc.measure_two_qubit_ssro(self, q0.name, q1.name, no_scaling=True,
+                                   result_logging_mode='lin_trans')
+
+        return  mra.two_qubit_ssro_fidelity(
+            label='{}_{}'.format(q0.name, q1.name),
+            qubit_labels=[q0.name, q1.name])
+
+    def check_mux_RO(self):
+        q0_name = self.qubits()[0]
+        q1_name = self.qubits()[1]
+        mqmc.measure_two_qubit_ssro(self, q0_name, q1_name, no_scaling=True,
+                                   result_logging_mode='lin_trans')
+
+        return mra.two_qubit_ssro_fidelity(
+            label='{}_{}'.format(q0_name, q1_name),
+            qubit_labels=[q0_name, q1_name])
