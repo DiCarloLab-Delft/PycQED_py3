6d8452871706e9447b694cbaadc64614c2f8c9aa
Stan <s.t.vannieuwamerongen@tudelft.nl>
2017-06-26 09:42:22 +0200

    The receive url function now accepts only true urls

--- pycqed/measurement/demonstrator_helper/helper_functions.py
+++ pycqed/measurement/demonstrator_helper/helper_functions.py
@@ -22,7 +22,7 @@ defualt_simulate_options = {
     "iterations": 1
 }
 defualt_execute_options = {}
-server_host = "http://localhost:3000/"
+
 
 def execute_AllXY(options={}):
     MC = measurement_control.MeasurementControl(
@@ -39,6 +39,7 @@ def execute_AllXY(options={}):
     MC.close()
     return return_value
 
+
 def execute_qasm_file(file_url, options={}):
     # file_url="uploads/asset/file/65/f27d92be-8505-43dc-af7d-4c395c70aaf9.qasm"
     file_path = _get_file_from_url(file_url)
@@ -49,9 +50,9 @@ def simulate_qasm_file(file_url, options={}):
     file_path = _get_file_from_url(file_url)
 
     # Connect to the qx simulator
-    
+
     MC = measurement_control.MeasurementControl(
-            'MC', live_plot_enabled=False, verbose=True)
+        'MC', live_plot_enabled=False, verbose=True)
     st = station.Station()
     MC.station = st
     st.add_component(MC)
@@ -60,7 +61,7 @@ def simulate_qasm_file(file_url, options={}):
     time.sleep(1)
     qxc.create_qubits(5)
     try:
-        
+
         qx_sweep = QX_Hard_Sweep(qxc, file_path)
         qx_detector = QX_Hard_Detector(qxc, [file_path], num_avg=10000)
         sweep_points = range(len(qx_detector.randomizations[0]))
@@ -72,10 +73,10 @@ def simulate_qasm_file(file_url, options={}):
         MC.set_sweep_function(qx_sweep)
         MC.set_sweep_points(sweep_points)
         dat = MC.run("run QASM")
-        return [_MC_result_to_chart_dict(dat)]
+        return _MC_result_to_chart_dict(dat)
     except:
         raise
-        
+
         return []
     finally:
         MC.close()
@@ -87,13 +88,14 @@ def _get_file_from_url(file_url):
     file_name = file_url.split("/")[-1]
     base_path = os.path.dirname(os.path.abspath(__file__))+"\\QASM_files\\"
     file_path = base_path + file_name
-
+    print(file_url)
     # download file from server
-    urllib.request.urlretrieve(server_host+file_url, file_path)
-    return file_path;
+    urllib.request.urlretrieve(file_url, file_path)
+    return file_path
+
 
 def _get_qasm_sweep_points(file_path):
-    counter = 0;
+    counter = 0
     with open(file_path) as f:
         line = f.readline()
         while(line):
@@ -103,14 +105,15 @@ def _get_qasm_sweep_points(file_path):
 
     return range(counter)
 
+
 def _MC_result_to_chart_dict(result):
     for i in result:
         if(isinstance(result[i], np.ndarray)):
             result[i] = result[i].tolist()
-    return {
+    return [{
         "data-type": "chart",
         "data": result
-    }
+    }]
 
 # operation_dict # Exists in global namespace for you
 # CBox = qc.station['CBox']
