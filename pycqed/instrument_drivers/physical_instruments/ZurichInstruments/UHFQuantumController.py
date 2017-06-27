fix: DSB demodulation scaling with UHFLI

pycqed/instrument_drivers/physical_instruments/ZurichInstruments/UHFQuantumController.py
@@ -631,9 +631,9 @@ class UHFQC(Instrument):
             weight_function_Q))
         eval('self.quex_wint_weights_{}_imag(np.array(cosI))'.format(
             weight_function_Q))
-        eval('self.quex_rot_{}_real(1.0)'.format(weight_function_I))
+        eval('self.quex_rot_{}_real(2.0)'.format(weight_function_I))
         eval('self.quex_rot_{}_imag(0.0)'.format(weight_function_I))
-        eval('self.quex_rot_{}_real(1.0)'.format(weight_function_Q))
+        eval('self.quex_rot_{}_real(2.0)'.format(weight_function_Q))
         eval('self.quex_rot_{}_imag(0.0)'.format(weight_function_Q))
 
 