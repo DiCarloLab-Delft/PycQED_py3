c26cee397cf3c6c94082d499e611b9734f3ee458
Livio Ciorciaro <livioc@ethz.ch>
2017-07-06 16:56:10 +0200

    Grover Tomo debugging sequences

--- pycqed/measurement/waveform_control_CC/QWG_fluxing_seqs.py
+++ pycqed/measurement/waveform_control_CC/QWG_fluxing_seqs.py
@@ -433,6 +433,151 @@ def grover_tomo_seq(q0_name, q1_name, omega, RO_target='all',
     return qasm_file
 
 
+def grover_test_seq(q0_name, q1_name, RO_target='all',
+                    precompiled_flux=True, cal_points: bool=True):
+    '''
+    Writes the QASM sequence for Grover's algorithm on two qubits.
+    Sequence:
+        q0: G0 -       - mY90 -    - mY90  - RO
+                 CZ_ij          CZ
+        q1: G1 -       - mY90 -    - mY90  - RO
+    whit all combinations of (ij) = omega.
+    G0 and G1 are Y90 or Y90, depending on the (ij).
+
+    Args:
+        q0_name, q1_name (string):
+                Names of the qubits to which the sequence is applied.
+        RO_target (string):
+                Readout target. Can be a qubit name or 'all'.
+        precompiled_flux (bool):
+                Determies if the full waveform for the flux pulses is
+                precompiled, thus only needing one trigger at the start,
+                or if every flux pulse should be triggered individually.
+        cal_points (bool):
+                Whether to add calibration points.
+
+    Returns:
+        qasm_file: a reference to the new QASM file object.
+    '''
+    if not precompiled_flux:
+        raise NotImplementedError('Currently only precompiled flux pulses '
+                                  'are supported.')
+
+    filename = join(base_qasm_path, 'Grover_seq.qasm')
+    qasm_file = mopen(filename, mode='w')
+    qasm_file.writelines('qubit {} \n'.format(q0_name))
+    qasm_file.writelines('qubit {} \n'.format(q1_name))
+
+    if RO_target == 'all':
+        RO_line = 'RO {} | RO {}\n'.format(q0_name, q1_name)
+    else:
+        RO_line = 'RO {} \n'.format(RO_target)
+
+    for G1 in ['mY90', 'Y90']:
+        for G0 in ['mY90', 'Y90']:
+            qasm_file.writelines('\ninit_all\n')
+            qasm_file.writelines('{} {} | {} {}\n'.format(G0, q0_name,
+                                                          G1, q1_name))
+            qasm_file.writelines('grover_CZ {} {}\n'.format(q0_name, q1_name))
+            qasm_file.writelines('mY90 {} | mY90 {}\n'.format(q0_name,
+                                                              q1_name))
+            # qasm_file.writelines('cz {} {}\n'.format(q0_name, q1_name))
+            # qasm_file.writelines('mY90 {} | mY90 {}\n'.format(q0_name,
+            #                                                   q1_name))
+
+            qasm_file.writelines(RO_line)
+
+    # Add calibration points
+    if cal_points:
+        cal_pulses = []
+        for seq in cal_points_2Q:
+            cal_pulses += [[seq[0].format(q0_name), seq[1].format(q1_name),
+                            RO_line]]
+
+        for seq in cal_pulses:
+            qasm_file.writelines('\ninit_all\n')
+            for p in seq:
+                qasm_file.writelines(p)
+
+    qasm_file.close()
+    return qasm_file
+
+
+def grover_test_tomo_seq(q0_name, q1_name, omega, RO_target='all',
+                         precompiled_flux=True):
+    '''
+    Test sequence to debug Grover's algorithm.
+    '''
+    if not precompiled_flux:
+        raise NotImplementedError('Currently only precompiled flux pulses '
+                                  'are supported.')
+
+    tomo_pulses = ['I ', 'X180 ', 'Y90 ', 'mY90 ', 'X90 ', 'mX90 ']
+    tomo_list_q0 = []
+    tomo_list_q1 = []
+    for tp in tomo_pulses:
+        tomo_list_q0 += [tp + q0_name]
+        tomo_list_q1 += [tp + q1_name]
+
+    if omega == 0:
+        G0 = 'mY90'
+        G1 = 'mY90'
+    elif omega == 1:
+        G0 = 'Y90'
+        G1 = 'mY90'
+    elif omega == 2:
+        G0 = 'mY90'
+        G1 = 'Y90'
+    elif omega == 3:
+        G0 = 'Y90'
+        G1 = 'Y90'
+    else:
+        raise ValueError('omega must be in [0, 3]')
+
+    if RO_target == 'all':
+        RO_line = 'RO {} | RO {}\n'.format(q0_name, q1_name)
+    else:
+        RO_line = 'RO {} \n'.format(RO_target)
+
+    filename = join(base_qasm_path, 'Grover_tomo_seq.qasm')
+    qasm_file = mopen(filename, mode='w')
+    qasm_file.writelines('qubit {} \n'.format(q0_name))
+    qasm_file.writelines('qubit {} \n'.format(q1_name))
+
+    for p_q1 in tomo_list_q1:
+        for p_q0 in tomo_list_q0:
+            qasm_file.writelines('\ninit_all\n')
+            qasm_file.writelines('{} {} | {} {}\n'.format(G0, q0_name,
+                                                          G1, q1_name))
+            qasm_file.writelines('grover_CZ {} {}\n'.format(q0_name, q1_name))
+            qasm_file.writelines('mY90 {} | mY90 {}\n'.format(q0_name,
+                                                              q1_name))
+            # qasm_file.writelines('cz {} {}\n'.format(q0_name, q1_name))
+            # qasm_file.writelines('mY90 {} | mY90 {}\n'.format(q0_name,
+            #                                                   q1_name))
+
+            qasm_file.writelines('{} | {}\n'.format(p_q1, p_q0))
+            qasm_file.writelines(RO_line)
+
+    # Add calibration pulses
+    cal_pulses = []
+    # every calibration point is repeated 7 times. This is copied from the
+    # script for Tektronix driven qubits. I do not know if this repetition
+    # is important or even necessary here.
+    for seq in cal_points_2Q:
+        cal_pulses += [[seq[0].format(q0_name), seq[1].format(q1_name),
+                        RO_line]] * 7
+
+    for seq in cal_pulses:
+        qasm_file.writelines('\ninit_all\n')
+        for p in seq:
+            qasm_file.writelines(p)
+
+    qasm_file.close()
+    return qasm_file
+
+
+
 def purity_CZ_seq(q0, q1, RO_target='all'):
     """
     Creates the |00> + |11> Bell state and does a partial tomography in
