6215665a4c42f34664851260e79b1c1e44c23796
Livio Ciorciaro <livioc@ethz.ch>
2017-08-28 14:29:39 +0200

    Updated example to be slightly more instructive (and have less typos).

--- examples/pulse_scheme_example.py
+++ examples/pulse_scheme_example.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import pycqed.utilities.pulse_scheme as pls
+import numpy as np
 
-# Simple example fo h
+# Simple example of how to use the pulse_scheme module
 
 cm = 1 / 2.54  # inch to cm conversion
 
@@ -11,7 +12,7 @@ fig, ax = pls.new_pulse_fig((7*cm, 3*cm))
 p1 = pls.mwPulse(ax, 0, width=1.5, label='$X_{\\pi/2}$')
 p2 = pls.ramZPulse(ax, p1, width=2.5, sep=1.5)
 p3 = pls.separator(ax, p2)
-p4 = pls.mwPulse(ax, p3, width=1.5, label='$X_{\\pi/2}$')
+p4 = pls.mwPulse(ax, p3, width=1.5, phase=np.pi/2, label='$Y_{\\pi/2}$')
 
 # Add some arrows and labeling
 pls.interval(ax, p1, p1 + 1.5, height=1.7, label='$T_\\mathsf{p}$')
