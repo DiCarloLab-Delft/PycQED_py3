78d08797f57ae4e4a803dd184b4e0b9f13c549a0
Adriaan <adriaan.rol@gmail.com>
2017-09-21 13:19:13 +0200

    Added virtual MW_source

--- pycqed/instrument_drivers/meta_instrument/qubit_objects/CCL_Transmon.py
+++ pycqed/instrument_drivers/meta_instrument/qubit_objects/CCL_Transmon.py
@@ -19,7 +19,7 @@ import pygsti
 
 from .qubit_object import Transmon
 from qcodes.utils import validators as vals
-from qcodes.instrument.parameter import ManualParameter
+from qcodes.instrument.parameter import ManualParameter, InstrumentRefParameter
 from pycqed.measurement.waveform_control_CC import waveform as wf
 from pycqed.analysis import measurement_analysis as ma
 from pycqed.analysis_v2 import measurement_analysis as ma2
@@ -40,7 +40,6 @@ from pycqed.measurement.waveform_control_CC import QWG_fluxing_seqs as qwfs
 from pycqed.measurement.waveform_control_CC.instruction_lib import convert_to_clocks
 
 from pycqed.measurement import detector_functions as det
-from pycqed.instrument_drivers.pq_parameters import InstrumentParameter
 import pycqed.measurement.gate_set_tomography.gate_set_tomography_CC as gstCC
 
 
@@ -65,7 +64,43 @@ class CCLight_Transmon(Transmon):
         pass
 
     def add_instrument_ref_parameters(self):
-        pass
+        # MW sources
+        self.add_parameter('LO', parameter_class=InstrumentRefParameter)
+        self.add_parameter('cw_source', parameter_class=InstrumentRefParameter)
+        self.add_parameter('td_source', parameter_class=InstrumentRefParameter)
+
+        # Control electronics
+        # self.add_parameter('AWG', parameter_class=InstrumentRefParameter)
+        self.add_parameter(
+            'CC', label='Central Controller',
+            docstring=('Device responsible for controlling the experiment'
+                       ' using eQASM generated using OpenQL, in the near'
+                       ' future will be the CC_Light.'),
+            parameter_class=InstrumentRefParameter)
+        self.add_parameter('acquisition_instrument',
+                           parameter_class=InstrumentRefParameter)
+        self.add_parameter('VSM', label='Vector Switch Matrix',
+                           parameter_class=InstrumentRefParameter)
+
+        self.add_parameter('MC', label='MeasurementControl',
+                           parameter_class=InstrumentRefParameter)
+        self.add_parameter('SH', label='SignalHound',
+                           parameter_class=InstrumentRefParameter)
+
+        # LutMan's
+        self.add_parameter('LutMan_MW',
+                           docstring='Lookuptable manager  for '
+                           'microwave control pulses.',
+                           parameter_class=InstrumentRefParameter)
+        self.add_parameter('LutMan_RO',
+                           docstring='Lookuptable manager responsible for '
+                           'microwave readout pulses.',
+                           parameter_class=InstrumentRefParameter)
+        self.add_parameter('LutMan_Flux',
+                           docstring='Lookuptable manager responsible for '
+                                     'flux pulses.',
+                           initial_value=None,
+                           parameter_class=InstrumentRefParameter)
 
     def prepare_for_continuous_wave(self):
         pass
--- pycqed/tests/test_qubit_objects.py
+++ pycqed/tests/test_qubit_objects.py
@@ -1,16 +1,29 @@
 import unittest
 import numpy as np
 import pycqed.instrument_drivers.virtual_instruments.virtual_AWG8 as v8
+import pycqed.instrument_drivers.virtual_instruments.virtual_MW_source as vmw
 from pycqed.instrument_drivers.meta_instrument.LutMans import mw_lutman as mwl
 from pycqed.measurement.waveform_control_CC import waveform as wf
 import pycqed.instrument_drivers.meta_instrument.qubit_objects.CCL_Transmon as ct
+from pycqed.measurement import measurement_control
+from qcodes import station
 
 
 class Test_Qubit_Object(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
+        self.station = station.Station()
         self.CCL_qubit = ct.CCLight_Transmon('CCL_qubit')
+
+        self.MW1 = vmw.VirtualMWsource('MW1')
+        self.MW2 = vmw.VirtualMWsource('MW2')
+        self.MW3 = vmw.VirtualMWsource('MW3')
+
+        self.MC = measurement_control.MeasurementControl(
+            'MC', live_plot_enabled=False, verbose=False)
+        self.MC.station = self.station
+        self.station.add_component(self.MC)
 
         self.AWG = v8.VirtualAWG8('DummyAWG8')
         self.AWG8_VSM_MW_LutMan = mwl.AWG8_VSM_MW_LutMan('MW_LutMan_VSM')
@@ -21,6 +34,12 @@ class Test_Qubit_Object(unittest.TestCase):
         self.AWG8_VSM_MW_LutMan.channel_DQ(4)
         self.AWG8_VSM_MW_LutMan.Q_modulation(100e6)
         self.AWG8_VSM_MW_LutMan.sampling_rate(2.4e9)
+
+        # Assign instruments
+        self.CCL_qubit.LutMan_MW(self.AWG8_VSM_MW_LutMan.name)
+        self.CCL_qubit.LO(self.MW1.name)
+        self.CCL_qubit.cw_source(self.MW2.name)
+        self.CCL_qubit.td_source(self.MW3.name)
 
     def test_prepare_for_timedomain(self):
         self.CCL_qubit.prepare_for_timedomain()
@@ -33,3 +52,13 @@ class Test_Qubit_Object(unittest.TestCase):
 
     def test_prepare_readout(self):
         self.CCL_qubit.prepare_readout()
+
+    @classmethod
+    def tearDownClass(self):
+        for inststr in list(self.CCL_qubit._all_instruments):
+            try:
+                inst = self.CCL_qubit.find_instrument(inststr)
+                inst.close()
+            except KeyError:
+                pass
+
