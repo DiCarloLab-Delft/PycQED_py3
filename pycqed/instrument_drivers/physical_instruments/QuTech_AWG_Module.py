b7b362f06d23d0b255c341932290d99cf782fd4f
nyxus <gercoversloot@gmail.com>
2017-09-29 10:19:56 +0200

    Added QWG trigger_logic_input parameter

--- pycqed/instrument_drivers/physical_instruments/QuTech_AWG_Module.py
+++ pycqed/instrument_drivers/physical_instruments/QuTech_AWG_Module.py
@@ -15,6 +15,7 @@ from .SCPI import SCPI
 import numpy as np
 import struct
 from qcodes import validators as vals
+# from qcodes import parser as pars
 
 
 from qcodes.instrument.parameter import StandardParameter
@@ -179,6 +180,22 @@ class QuTech_AWG_Module(SCPI):
         self.add_parameter('Wlist',
                            label='Waveform list',
                            get_cmd=self._getWlist)
+
+        # Trigger parameters
+        doc_trgs_log_inp = 'Reads the current input values on the all the trigger ' \
+                    +'inputs.\nReturn:\n    uint32 where trigger 1 (T1) ' \
+                    +'is on the Least significant bit (LSB), T2 on the second  ' \
+                    +'bit after LSB, etc.\n\n For example, if only T3 is ' \
+                    +'connected to a high signal, the return value is: ' \
+                    +'4 (0b0000100)\n\n Note: To convert the return value ' \
+                    +'to a readable ' \
+                    +'binary output use: `print(\"{0:#010b}\".format(qwg.' \
+                    +'triggers_logic_input()))`'
+        self.add_parameter('triggers_logic_input',
+                           label='Read triggers input value',
+                           get_cmd='QUTEch:TRIGgers:LOGIcinput?',
+                           get_parser=np.uint32,
+                           docstring=doc_trgs_log_inp)
 
         # This command is added manually
         # self.add_function('deleteWaveform'
