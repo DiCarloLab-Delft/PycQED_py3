83a641fa1f2cdde1ff4edbcf5ff7a01f2e419f0c
Stefania Balasiu <balasiu@phys.ethz.ch>
2017-10-02 14:10:17 +0200

    Fix travis error. Checks for Qubit_Spectroscopy_Analysis in test_spectroscopy_analysis.py are now in GHz/MHz not Hz.

--- pycqed/tests/test_spectroscopy_analysis.py
+++ pycqed/tests/test_spectroscopy_analysis.py
@@ -14,16 +14,16 @@ class Test_Qubit_spectroscopy_analysis(unittest.TestCase):
     def test_Qubit_spectroscopy_analysis_one_peak(self):
 
         a = ma.Qubit_Spectroscopy_Analysis(timestamp='20170929_175516')
-        self.assertAlmostEqual(a.fit_res.values['f0'], 6.111810985426058E9, places=2)
-        self.assertAlmostEqual(a.fit_res.values['kappa'], 332986.1894, places=2)
+        self.assertAlmostEqual(a.fit_res.values['f0']/1e9, 6.11181, places=2)
+        self.assertAlmostEqual(a.fit_res.values['kappa']/1e6, 0.332, places=2)
 
     def test_Qubit_spectroscopy_analysis_two_peaks(self):
 
         a = ma.Qubit_Spectroscopy_Analysis(timestamp='20170929_144754', analyze_ef=True)
-        self.assertAlmostEqual(a.fit_res.values['f0'], 6.110897089343999E9, places=2)
-        self.assertAlmostEqual(a.fit_res.values['kappa'], 1.664737312747661E7, places=2)
-        self.assertAlmostEqual(a.fit_res.values['f0_gf_over_2'], 5.998399149274546E9, places=2)
-        self.assertAlmostEqual(a.fit_res.values['kappa_gf_over_2'], 1927578.1062894785, places=2)
+        self.assertAlmostEqual(a.fit_res.values['f0']/1e9, 6.11089, places=2)
+        self.assertAlmostEqual(a.fit_res.values['kappa']/1e6, 16.647, places=2)
+        self.assertAlmostEqual(a.fit_res.values['f0_gf_over_2']/1e9, 5.998, places=2)
+        self.assertAlmostEqual(a.fit_res.values['kappa_gf_over_2']/1e6, 1.9275, places=2)
 
 class Test_Homodyne_analysis(unittest.TestCase):
 
