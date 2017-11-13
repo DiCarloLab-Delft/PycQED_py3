0dd189e27425cfb9534a6975249bbc7ac4957add
MalaySingh <malaysingh121993@gmail.com>
2017-11-13 15:46:29 +0100

    Test working!

--- pycqed/tests/test_tomo_v2.py
+++ pycqed/tests/test_tomo_v2.py
@@ -23,16 +23,17 @@ class Test_tomography_execute(unittest.TestCase):
       tomo_object = tomography_execute.TomographyExecute(timestamp='20161124_162604')
       # Get the dm for 00 cardinal state  
       # returned rho is a quantum object
-      rho = (tomo_object.get_density_matrix()).full()
+      rho_tomo = (tomo_object.get_density_matrix()).full()
       # get a rho target corresponding to the 00 state
       rho_target = (qt.ket2dm(qt.basis(4, 0))).full()
       #get it's fidelity to a 00 state
       #This is not the correct fidelity 
       #The correct method shhould be pauli labels
       benchmark_fidelity = np.real_if_close(np.dot(rho_tomo.flatten(),rho_target.flatten()))
-      
-      
       self.assertAlmostEqual(benchmark_fidelity, 0.9077158, places=7)
+
+    
+
 
 
     
