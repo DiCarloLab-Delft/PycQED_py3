Takes out the code in MeasurementControl where the snapshot is saved in the HDF file: saves a lot of disk space! Fixes RO fixed point calculation to take 11 significant digits instead of 9.
Latest changes to simultaneous RB (and other stuff).

pycqed/measurement/waveform_control/element.py
@@ -398,7 +398,7 @@ class Element:
 
 
 def calculate_time_correction(t0, fixed_point=1e-6):
-    return np.round((fixed_point-t0) % fixed_point, decimals=9)
+    return np.round((fixed_point-t0) % fixed_point, decimals=11)
 
 
 def is_divisible_by_clock(value, clock=1e9):