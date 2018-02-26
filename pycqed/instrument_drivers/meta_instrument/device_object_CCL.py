c94929bf91a80c482c2118546f5cdab56cf38725
AdriaanRol <adriaan.rol@gmail.com>
2018-02-26 17:16:56 +0100

    Flux stuff

--- pycqed/instrument_drivers/meta_instrument/device_object_CCL.py
+++ pycqed/instrument_drivers/meta_instrument/device_object_CCL.py
@@ -136,18 +136,20 @@ class DeviceCCL(Instrument):
         # prepares by loading the awg_hack_program
         q0 = self.qubits()[0]
         fl_lutman = self.find_instrument(q0).instr_LutMan_Flux.get_instr()
-        fl_lutman.load_waveforms_onto_awg_lookuptable()
+        # fl_lutman.load_waveforms_onto_awg_lookuptable()
+        fl_lutman.load_waveforms_onto_AWG_lookuptable()
         awg = fl_lutman.AWG.get_instr()
+        awg.upload_codeword_program(awgs=[0])
 
-        awg_hack_program_cz = """
-        while (1) {
-          waitDIOTrigger();
-          playWave("dev8005_wave_ch1_cw001", "dev8005_wave_ch2_cw001");
-        }
-        """
+        # awg_hack_program_cz = """
+        # while (1) {
+        #   waitDIOTrigger();
+        #   playWave("dev8005_wave_ch1_cw001", "dev8005_wave_ch2_cw001");
+        # }
+        # """
+        # awg.configure_awg_from_string(0, awg_hack_program_cz)
+        # awg.configure_codeword_protocol()
 
-        awg.configure_awg_from_string(0, awg_hack_program_cz)
-        awg.configure_codeword_protocol()
         awg.start()
 
     def _prep_ro_setup_qubits(self):
@@ -501,7 +503,9 @@ class DeviceCCL(Instrument):
     def measure_conditional_oscillation(self, q0: str, q1: str,
                                         prepare_for_timedomain=True, MC=None,
                                         wait_time_ns: int=0,
-                                        verbose=True):
+                                        label='',
+                                        flux_codeword='fl_cw_01',
+                                        verbose=True, disable_metadata=False):
         """
         Measures the "conventional cost function" for the CZ gate that
         is a conditional oscillation.
@@ -520,6 +524,7 @@ class DeviceCCL(Instrument):
         p = mqo.conditional_oscillation_seq(q0idx, q1idx,
                                             platf_cfg=self.cfg_openql_platform_fn(),
                                    angles=angles, wait_time=wait_time_ns,
+                                   flux_codeword=flux_codeword,
                                    CZ_disabled=False)
         s = swf.OpenQL_Sweep(openql_program=p,
                              CCL=self.instr_CC.get_instr(),
@@ -527,7 +532,8 @@ class DeviceCCL(Instrument):
         MC.set_sweep_function(s)
         MC.set_sweep_points(p.sweep_points)
         MC.set_detector_function(self.get_correlation_detector())
-        MC.run('conditional_oscillation{}'.format(self.msmt_suffix))
+        MC.run('conditional_oscillation{}'.format(self.msmt_suffix, label),
+               disable_snapshot_metadata=disable_metadata)
 
         a = ma2.Conditional_Oscillation_Analysis(
             options_dict={'ch_idx_osc': self.qubits().index(q0),
