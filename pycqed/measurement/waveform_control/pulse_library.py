40171ad03dea613b454c67051aff404e4b7b37d6
Ants Remm <remma@phys.ethz.ch>
2018-03-12 18:17:14 +0100

    add BufferedSquarePulse pulse type

--- pycqed/measurement/waveform_control/pulse_library.py
+++ pycqed/measurement/waveform_control/pulse_library.py
@@ -343,6 +343,39 @@ class SquareFluxPulse(Pulse):
             round((self.length-self.square_pulse_length)*1e9)))
         return np.concatenate([sq_pulse, buff_pulse])
 
+class BufferedSquarePulse(Pulse):
+    def __init__(self, channel=None, channels=None,
+                 name='buffered square pulse', **kw):
+        Pulse.__init__(self, name)
+        if channel is None and channels is None:
+            raise ValueError('Must specify either channel or channels')
+        elif channels is None:
+            self.channels.append(channel)
+        else:
+            self.channels = channels
+        self.amplitude = kw.pop('amplitude', 0)
+
+        self.pulse_length = kw.pop('pulse_length', 0)
+        self.buffer_length_start = kw.pop('buffer_length_start', 0)
+        self.buffer_length_end = kw.pop('buffer_length_end', 0)
+        self.length = self.pulse_length + self.buffer_length_start + \
+                      self.buffer_length_end
+
+    def __call__(self, **kw):
+        self.amplitude = kw.pop('amplitude', self.amplitude)
+        self.pulse_length = kw.pop('pulse_length', 0)
+        self.buffer_length_start = kw.pop('buffer_length_start', 0)
+        self.buffer_length_end = kw.pop('buffer_length_end', 0)
+        self.length = self.pulse_length + self.buffer_length_start + \
+                      self.buffer_length_end
+        self.channels = kw.pop('channels', self.channels)
+        self.channels.append(self.channel)
+        return self
+
+    def chan_wf(self, chan, tvals):
+        return np.ones_like(tvals)*self.amplitude * \
+               (tvals >= tvals[0] + self.buffer_length_start) * \
+               (tvals < tvals[0] + self.buffer_length_start + self.pulse_length)
 
 class MartinisFluxPulse(Pulse):
 
