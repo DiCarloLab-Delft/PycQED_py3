914f0106326ba18b6980cbf58871c9207cb97bda
elrama- <sagastizabal.ramiro@gmail.com>
2018-04-10 14:08:27 +0200

    Staircase test for QWG

--- examples/QWG_examples/QWG_staircase_test.py
+++ examples/QWG_examples/QWG_staircase_test.py
@@ -28,6 +28,7 @@ import pycqed as pq
 from importlib import reload
 from pycqed.instrument_drivers.physical_instruments.ZurichInstruments import UHFQuantumController as ZI_UHFQC
 from pycqed.instrument_drivers.meta_instrument.LutMans.ro_lutman import UHFQC_RO_LutMan
+from pycqed.instrument_drivers.meta_instrument.LutMans.mw_lutman import get_redundant_codewords
 
 from pycqed.instrument_drivers.physical_instruments import QuTech_AWG_Module as qwg
 from pycqed.instrument_drivers.physical_instruments import QuTech_CCL
@@ -39,7 +40,7 @@ cs_filepath = os.path.join(pq.__path__[0], 'measurement', 'openql_experiments',
 
 CCL.control_store(cs_filepath)
 
-QWG = qwg.QuTech_AWG_Module('QWG_MW', address='192.168.0.190', port=5025, numCodewords=128)
+QWG = qwg.QuTech_AWG_Module('QWG_MW', address='192.168.0.190', port=5025, numCodewords=256)
 
 
 ##########################################
@@ -73,13 +74,45 @@ import numpy as np
 waveform_type = 'square'
 # waveform_type = 'cos'
 
+#######################
+# CHANNELS 1,2 FOR s0 #
+# CHANNELS 3,4 FOR s2 #
+#######################
+
 if waveform_type == 'square':
     for ch in range(8):
-        for i in range(128):
-            QWG.set('wave_ch{}_cw{:03}'.format(ch+1, i), (np.ones(50)*i/32))
+        for codeword_int in range(256):
+            # FOR s0
+            redundant_cw_list = get_redundant_codewords(codeword_int,
+                                                        bit_width=4,
+                                                        bit_shift=4)
+            # update all of them
+            wave_I = np.ones(50)*codeword_int/256
+            wave_Q = -np.ones(50)*codeword_int/256
+            for redundant_cw_idx in redundant_cw_list:
+                redundant_cw_I = 'wave_ch1_cw{:03}'.format(redundant_cw_idx)
+                QWG.get_instr().set(redundant_cw_I, wave_I)
+                redundant_cw_Q = 'wave_ch2_cw{:03}'.format(redundant_cw_idx)
+                QWG.get_instr().set(redundant_cw_Q, wave_Q)
+
+            # FOR s1
+            redundant_cw_list = get_redundant_codewords(codeword_int,
+                                                        bit_width=4,
+                                                        bit_shift=0)
+            # update all of them
+            wave_I = np.ones(50)*codeword_int/256
+            wave_Q = -np.ones(50)*codeword_int/256
+            for redundant_cw_idx in redundant_cw_list:
+                redundant_cw_I = 'wave_ch3_cw{:03}'.format(redundant_cw_idx)
+                QWG.get_instr().set(redundant_cw_I, wave_I)
+                redundant_cw_Q = 'wave_ch4_cw{:03}'.format(redundant_cw_idx)
+                QWG.get_instr().set(redundant_cw_Q, wave_Q)
+
+
+            QWG.set('wave_ch{}_cw{:03}'.format(ch+1, i), (np.ones(50)*i/256))
 elif waveform_type == 'cos':
     for ch in range(8):
-        for i in range(128):
+        for i in range(256):
             QWG.set('wave_ch{}_cw{:03}'.format(ch+1, i),
                      (np.cos(np.arange(50)/2)*i/32))
 else:
