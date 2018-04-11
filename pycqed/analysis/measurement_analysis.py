e5b2a55a630dc01e54847a39bc7eb4593b579040
Niels Bultink <nielsbultink@gmail.com>
2018-04-11 11:18:13 +0200

    improving the fitting of the echo, no more failed fits

--- pycqed/analysis/measurement_analysis.py
+++ pycqed/analysis/measurement_analysis.py
@@ -2146,14 +2146,25 @@ class Echo_analysis(TD_Analysis):
 
         self.add_analysis_datagroup_to_file()
         # Instantiating here ensures models have no memory of constraints
-        model = lmfit.Model(fit_mods.ExpDecayFunc)
-        model.guess = fit_mods.exp_dec_guess
+        fit_mods.ExpDecayModel.set_param_hint('amplitude',
+                                              value=-0.5,
+                                              min=-2,
+                                              max=2)
+        fit_mods.ExpDecayModel.set_param_hint('tau',
+                                              value=self.sweep_points[1]*50,
+                                              min=self.sweep_points[1],
+                                              max=self.sweep_points[-1]*1000)
+        fit_mods.ExpDecayModel.set_param_hint('offset',
+                                              value=0,
+                                              vary=True)
+        fit_mods.ExpDecayModel.set_param_hint('n',
+                                              value=1,
+                                              vary=False)
+        self.params = fit_mods.ExpDecayModel.make_params()
 
-        params = model.guess(model, data=self.corr_data[:-self.NoCalPoints],
-                             t=self.sweep_points[:-self.NoCalPoints])
-        self.fit_res = model.fit(data=self.corr_data[:-self.NoCalPoints],
-                                 t=self.sweep_points[:-self.NoCalPoints],
-                                 params=params)
+        self.fit_res = fit_mods.ExpDecayModel.fit(data=self.normalized_data_points,
+                                     t=self.sweep_points[:-self.NoCalPoints],
+                                     params=self.params)
         self.save_fitted_parameters(fit_res=self.fit_res,
                                     var_name='corr_data')
 
