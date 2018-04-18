763f3e706e9328c02f0fa4ca1d50d3b601a55ce4
rvollmer <rvollmer@aypac.de>
2018-04-18 09:54:54 +0200

    Fix error in plot_color2D method: handle lists of different-lengths lists in the y-axis

--- pycqed/analysis_v2/base_analysis.py
+++ pycqed/analysis_v2/base_analysis.py
@@ -954,8 +954,8 @@ class BaseDataAnalysis(object):
         else:
             plot_xvals_step = (abs(np.max(plot_xvals) - np.min(plot_xvals)) /
                                len(plot_xvals))
-            plot_yvals_step = (abs(np.max(plot_yvals) - np.min(plot_yvals)) /
-                               len(plot_yvals))
+            plot_yvals_step = [(abs(np.max(x) - np.min(x)) /
+                               len(x)) for x in plot_yvals]
             # plot_yvals_step = plot_yvals[1]-plot_yvals[0]
 
         if plot_zrange is not None:
