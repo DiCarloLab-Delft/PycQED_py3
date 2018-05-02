a33b091cc114c3069edd5908f24ae18178b52c02
Niels Bultink <nielsbultink@gmail.com>
2018-05-02 11:27:41 +0200

    updates UHFQC driver: replacing all eval statements with self.get() and self.set(), setting the output channels to 50 ohm by default

--- pycqed/instrument_drivers/physical_instruments/ZurichInstruments/UHFQuantumController.py
+++ pycqed/instrument_drivers/physical_instruments/ZurichInstruments/UHFQuantumController.py
@@ -231,9 +231,9 @@ class UHFQC(Instrument):
             self.awgs_0_dio_valid_index(16)
             self.awgs_0_dio_valid_polarity(2) # high polarity
 
-
-
-
+        # setting the output channels to 50 ohm
+        self.sigouts_0_imp50(True)
+        self.sigouts_1_imp50(True)
 
         # We probably need to adjust some delays here...
         #self.awgs_0_dio_delay_index(31)
@@ -242,16 +242,16 @@ class UHFQC(Instrument):
         # No rotation on the output of the weighted integration unit, i.e. take
         # real part of result
         for i in range(0, 4):
-            eval('self.quex_rot_{0}_real(1.0)'.format(i))
-            eval('self.quex_rot_{0}_imag(0.0)'.format(i))
+            self.set('quex_rot_{0}_real'.format(i), 0.0)
+            self.set('quex_rot_{0}_imag'.format(i), 0.0)
 
         # No cross-coupling in the matrix multiplication (identity matrix)
         for i in range(0, 4):
             for j in range(0, 4):
                 if i == j:
-                    eval('self.quex_trans_{0}_col_{1}_real(1)'.format(i, j))
+                    self.set('quex_trans_{0}_col_{1}_real'.format(i, j),1)
                 else:
-                    eval('self.quex_trans_{0}_col_{1}_real(0)'.format(i, j))
+                    self.set('quex_trans_{0}_col_{1}_real'.format(i, j),0)
 
         # Configure the result logger to not do any averaging
         self.quex_rl_length(pow(2, LOG2_AVG_CNT)-1)
@@ -988,24 +988,19 @@ setTrigger(0);"""
         coswave = RO_amp*np.cos(2*np.pi*array*f_RO_mod/f_sampling)
         Iwave = (coswave+sinwave)/np.sqrt(2)
         Qwave = (coswave-sinwave)/np.sqrt(2)
-        # Iwave, Qwave = PG.mod_pulse(np.ones(samples), np.zeros(samples), f=f_RO_mod, phase=0, sampling_rate=f_sampling)
         self.awg_sequence_acquisition_and_pulse(
             Iwave, Qwave, acquisition_delay, dig_trigger=dig_trigger)
 
     def upload_transformation_matrix(self, matrix):
         for i in range(np.shape(matrix)[0]):  # looping over the rows
             for j in range(np.shape(matrix)[1]):  # looping over the colums
-                eval(
-                    'self.quex_trans_{}_col_{}_real(matrix[{}][{}])'.format(j, i, i, j))
+                self.set('quex_trans_{}_col_{}_real'.format(j, i), matrix[i][j])
 
     def download_transformation_matrix(self, nr_rows=4, nr_cols=4):
         matrix = np.zeros([nr_rows, nr_cols])
         for i in range(np.shape(matrix)[0]):  # looping over the rows
             for j in range(np.shape(matrix)[1]):  # looping over the colums
-                matrix[i][j] = (
-                    eval('self.quex_trans_{}_col_{}_real()'.format(j, i)))
-                # print(value)
-                # matrix[i,j]=value
+                matrix[i][j] = self.get('quex_trans_{}_col_{}_real'.format(j, i))
         return matrix
 
 
