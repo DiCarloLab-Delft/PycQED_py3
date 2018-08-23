47581b6ce60cf9b078a24523bbc4765961044ebf
Adriaan <adriaan.rol@gmail.com>
2018-08-23 13:37:57 +0200

    made RB analysis support missing f-state cal pts and made robust against missing uncertainty

--- pycqed/tests/analysis_v2/test_randomized_benchmarking_analysis.py
+++ pycqed/tests/analysis_v2/test_randomized_benchmarking_analysis.py
@@ -26,4 +26,16 @@ class Test_RBAnalysis(unittest.TestCase):
         F = rb_pars['F'].value
         self.assertAlmostEqual(F, 0.997895, places=4)
 
+    def test_single_qubit_RB_analysis_missing_f_cal(self):
+        a = ma.RandomizedBenchmarking_SingleQubit_Analysis(
+            t_start='20180815_150417',
+            classification_method='rates', rates_ch_idx=0,
+            ignore_f_cal_pts=True)
+
+        rb_pars = a.fit_res['rb_decay'].params
+        eps = rb_pars['eps'].value
+        self.assertAlmostEqual(eps, 0.00236731, places=4)
+
+
+
 
