f42bc664bc1126ac2f6022d49af6d5a659cede39
Adriaan <adriaan.rol@gmail.com>
2018-08-24 17:33:12 +0200

    fixed failing test

--- pycqed/tests/test_linear_filter_distortions.py
+++ pycqed/tests/test_linear_filter_distortions.py
@@ -1,13 +1,16 @@
 import unittest
 import numpy as np
 import pycqed.instrument_drivers.meta_instrument.lfilt_kernel_object as lko
-
+import pycqed.instrument_drivers.virtual_instruments.virtual_AWG8 as v8
 
 class Test_LinDistortionKernelObject(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
         self.k0 = lko.LinDistortionKernel('k0')
+        self.AWG = v8.VirtualAWG8('DummyAWG8')
+        self.k0.instr_AWG(self.AWG.name)
+        self.k0.cfg_awg_channel(1)
 
     def setUp(self):
         self.k0.cfg_sampling_rate(2.4e9)
@@ -15,6 +18,7 @@ class Test_LinDistortionKernelObject(unittest.TestCase):
             {'model': 'high-pass', 'params': {'tau': 4.071755778296734e-05}})
         self.k0.filter_model_01({'model': 'exponential',
                                  'params': {'amp': 4.2035373039155806, 'tau': 5.9134605614601521e-06}})
+
 
     def test_print_overview(self):
         # only test that it doesn't raise errors
@@ -46,3 +50,6 @@ class Test_LinDistortionKernelObject(unittest.TestCase):
     @classmethod
     def tearDownClass(self):
         self.k0.close()
+        self.AWG.close()
+
+
