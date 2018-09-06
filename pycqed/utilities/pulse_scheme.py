e445497b755eb52c7515a32f1b278004b858abee
Adriaan <adriaan.rol@gmail.com>
2018-09-06 10:35:53 +0200

    Added some extra options to the pulse schem functions

--- pycqed/utilities/pulse_scheme.py
+++ pycqed/utilities/pulse_scheme.py
@@ -28,7 +28,7 @@ def new_pulse_subplot(fig, *args, **kwargs):
     return ax
 
 
-def mwPulse(ax, pos, width=1.5, amp=1, label=None, phase=0, labelHeight=1.3,
+def mwPulse(ax, pos, y_offs=0, width=1.5, amp=1, label=None, phase=0, labelHeight=1.3,
             color='C0', modulation='normal', **plot_kws):
     '''
     Draw a microwave pulse: Gaussian envelope with modulation.
@@ -44,9 +44,9 @@ def mwPulse(ax, pos, width=1.5, amp=1, label=None, phase=0, labelHeight=1.3,
     else:
         raise ValueError()
 
-    ax.plot(x, envPos, '--', color=color, **plot_kws)
-    ax.plot(x, envNeg, '--', color=color, **plot_kws)
-    ax.plot(x, mod, '-', color=color, **plot_kws)
+    ax.plot(x, envPos+y_offs, '--', color=color, **plot_kws)
+    ax.plot(x, envNeg+y_offs, '--', color=color, **plot_kws)
+    ax.plot(x, mod+y_offs, '-', color=color, **plot_kws)
 
     if label is not None:
         ax.text(pos + width / 2, labelHeight, label,
@@ -55,7 +55,7 @@ def mwPulse(ax, pos, width=1.5, amp=1, label=None, phase=0, labelHeight=1.3,
     return pos + width
 
 
-def fluxPulse(ax, pos, width=2.5, s=.1, amp=1.5, label=None, labelHeight=1.7,
+def fluxPulse(ax, pos, y_offs=0, width=2.5, s=.1, amp=1.5, label=None, labelHeight=1.7,
               color='C1', **plot_kws):
     '''
     Draw a smooth flux pulse, where the rising and falling edges are given by
@@ -66,8 +66,8 @@ def fluxPulse(ax, pos, width=2.5, s=.1, amp=1.5, label=None, labelHeight=1.7,
     y = amp / ((np.exp(-(x - (pos + 5.5 * s)) / s) + 1) *
                (np.exp((x - (pos + width - 5.5 * s)) / s) + 1))
 
-    ax.fill_between(x, y, color=color, alpha=0.3)
-    ax.plot(x, y, color=color, **plot_kws)
+    ax.fill_between(x, y+y_offs, color=color, alpha=0.3)
+    ax.plot(x, y+y_offs, color=color, **plot_kws)
 
     if label is not None:
         ax.text(pos + width / 2, labelHeight, label,
@@ -76,7 +76,7 @@ def fluxPulse(ax, pos, width=2.5, s=.1, amp=1.5, label=None, labelHeight=1.7,
     return pos + width
 
 
-def ramZPulse(ax, pos, width=2.5, s=0.1, amp=1.5, sep=1.5, color='C1'):
+def ramZPulse(ax, pos, y_offs=0, width=2.5, s=0.1, amp=1.5, sep=1.5, color='C1'):
     '''
     Draw a Ram-Z flux pulse, i.e. only part of the pulse is shaded, to indicate
     cutting off the pulse at some time.
@@ -88,24 +88,21 @@ def ramZPulse(ax, pos, width=2.5, s=0.1, amp=1.5, sep=1.5, color='C1'):
                (np.exp((xFull - (pos + width - 5.5 * s)) / s) + 1))
     yLeft = y[:len(xLeft)]
 
-    ax.fill_between(xLeft, yLeft, alpha=0.3, color=color, linewidth=0.0)
-    ax.plot(xFull, y, color=color)
+    ax.fill_between(xLeft, yLeft+y_offs, alpha=0.3, color=color, linewidth=0.0)
+    ax.plot(xFull, y+y_offs, color=color)
 
     return pos + width
 
 
-def modZPulse(ax, pos, width=2.5, s=0.1, amp=1.5, sep=1.5, color='C1'):
+def modZPulse(ax, pos, y_offs=0, width=2.5, s=0.1, amp=1.5, sep=1.5, color='C1'):
     '''
     Draw a modulated Z pulse.
     '''
 
-
     return pos + width
 
 
-
-
-def interval(ax, start, stop, height=1.5, label=None, labelHeight=None,
+def interval(ax, start, stop, y_offs = 0, height=1.5, label=None, labelHeight=None,
              vlines=True, color='k', arrowstyle='<|-|>', **plot_kws):
     '''
     Draw an arrow to indicate an interval.
@@ -114,20 +111,21 @@ def interval(ax, start, stop, height=1.5, label=None, labelHeight=None,
         labelHeight = height + 0.2
 
     arrow = matplotlib.patches.FancyArrowPatch(
-        posA=(start, height), posB=(stop, height), arrowstyle=arrowstyle,
+        posA=(start, height+y_offs), posB=(stop, height+y_offs), arrowstyle=arrowstyle,
         color=color, mutation_scale=7, **plot_kws)
     ax.add_patch(arrow)
 
     if vlines:
-        ax.plot([start, start], [0, height], '--', color=color, **plot_kws)
-        ax.plot([stop, stop], [0, height], '--', color=color, **plot_kws)
+        ax.plot([start, start], [0+y_offs, height+y_offs], '--', color=color, **plot_kws)
+        ax.plot([stop, stop], [0+y_offs, height+y_offs], '--', color=color, **plot_kws)
 
     if label is not None:
-        ax.text((start + stop) / 2, labelHeight, label, color=color,
+        ax.text((start + stop) / 2, labelHeight+y_offs, label, color=color,
                 horizontalalignment='center')
 
+
 def interval_vertical(ax, start, stop, position, label=None, labelHeight=None,
-                      color='k', arrowstyle='<|-|>', labeloffset:float = 0,
+                      color='k', arrowstyle='<|-|>', labeloffset: float = 0,
                       horizontalalignment='center'):
     '''
     Draw an arrow to indicate an interval.
@@ -145,21 +143,21 @@ def interval_vertical(ax, start, stop, position, label=None, labelHeight=None,
                 horizontalalignment=horizontalalignment)
 
 
-def meter(ax, x0, y0, w=1.1, h=.8, color='black', fillcolor=None):
+def meter(ax, x0, y0, y_offs=0,  w=1.1, h=.8, color='black', fillcolor=None):
     """
     Draws a measurement meter on the specified position.
     """
-    if fillcolor==None:
-        fill=False
+    if fillcolor == None:
+        fill = False
     else:
         fill = True
     p1 = matplotlib.patches.Rectangle(
-        (x0-w/2, y0-h/2), w, h, facecolor=fillcolor, edgecolor=color,
+        (x0-w/2, y0-h/2+y_offs), w, h, facecolor=fillcolor, edgecolor=color,
         fill=fill, zorder=5)
     ax.add_patch(p1)
     p0 = matplotlib.patches.Wedge(
-        (x0,y0-h/4), .35, theta1=40, theta2=180-40, color=color, lw=2,
-        width =.01, zorder=5)
+        (x0, y0-h/4+y_offs), .35, theta1=40, theta2=180-40, color=color, lw=2,
+        width=.01, zorder=5)
     ax.add_patch(p0)
-    ax.arrow(x0, y0-h/4, dx=.5*np.cos(np.deg2rad(70)),
+    ax.arrow(x0, y0-h/4+y_offs, dx=.5*np.cos(np.deg2rad(70)),
              dy=.5*np.sin(np.deg2rad(60)), width=.03, color=color, zorder=5)
