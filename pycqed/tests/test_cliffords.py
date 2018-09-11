0cb2e18b1b6b94e9fdb0c69616ad119b1863de4a
Viacheslav Ostroukh <V.Ostroukh@tudelft.nl>
2018-09-11 17:48:22 +0200

    convert test_clifford to pytest

--- pycqed/tests/test_cliffords.py
+++ pycqed/tests/test_cliffords.py
@@ -1,62 +1,59 @@
-import numpy as np
-from unittest import expectedFailure
-from unittest import TestCase
 from zlib import crc32
 
-from pycqed.measurement.randomized_benchmarking.clifford_group import(
-    clifford_lookuptable, clifford_group_single_qubit,
-    X,Y,Z, H, S, S2, CZ)
+import numpy as np
+import pytest
+from numpy.testing import assert_almost_equal, assert_array_equal
 
-import pycqed.measurement.randomized_benchmarking.randomized_benchmarking \
-    as rb
-
+import pycqed.measurement.randomized_benchmarking.randomized_benchmarking as rb
+import pycqed.measurement.randomized_benchmarking.two_qubit_clifford_group as\
+    tqc
 from pycqed.measurement.randomized_benchmarking.clifford_decompositions \
-    import(gate_decomposition, epstein_fixed_length_decomposition)
-
-from pycqed.measurement.randomized_benchmarking import \
-    two_qubit_clifford_group as tqc
-from pycqed.measurement.randomized_benchmarking.generate_clifford_hash_tables import construct_clifford_lookuptable
-
+    import (gate_decomposition, epstein_fixed_length_decomposition)
+from pycqed.measurement.randomized_benchmarking.clifford_group \
+    import (clifford_lookuptable, clifford_group_single_qubit)
+from pycqed.measurement.randomized_benchmarking.generate_clifford_hash_tables \
+    import construct_clifford_lookuptable
 
 np.random.seed(0)
 test_indices_2Q = np.random.randint(0, high=11520, size=50)
 # To test all elements of the 2 qubit clifford group use:
 # test_indices_2Q = np.arange(11520)
 
-class TestLookuptable(TestCase):
+
+class TestLookupTable:
     def test_unique_mapping(self):
         for row in clifford_lookuptable:
-            self.assertFalse(len(row) > len(set(row)))
+            assert len(row) <= len(set(row))
 
     def test_sum_of_rows(self):
         expected_sum = np.sum(range(len(clifford_group_single_qubit)))
         for row in clifford_lookuptable:
-            self.assertEqual(np.sum(row), expected_sum)
+            assert np.sum(row) == expected_sum
 
     def test_element_index_in_group(self):
         for row in clifford_lookuptable:
             for el in row:
-                self.assertTrue(el < len(clifford_group_single_qubit))
+                assert el < len(clifford_group_single_qubit)
 
 
-class TestCalculateNetClifford(TestCase):
+class TestCalculateNetClifford:
     def test_identity_does_nothing(self):
         id_seq = np.zeros(5)
         net_cl = rb.calculate_net_clifford(id_seq)
-        self.assertEqual(net_cl, 0)
+        assert net_cl == 0
 
         for i in range(len(clifford_group_single_qubit)):
             id_seq[3] = i
             net_cl = rb.calculate_net_clifford(id_seq)
-            self.assertEqual(net_cl, i)
+            assert net_cl == i
 
     def test_pauli_squared_is_ID(self):
         for cl in [0, 3, 6, 9, 12]:  # 12 is Hadamard
             net_cl = rb.calculate_net_clifford([cl, cl])
-            self.assertEqual(net_cl, 0)
+            assert net_cl == 0
 
 
-class TestRecoveryClifford(TestCase):
+class TestRecoveryClifford:
     def testInversionRandomSequence(self):
         random_cliffords = np.random.randint(0, len(clifford_group_single_qubit), 100)
         net_cl = rb.calculate_net_clifford(random_cliffords)
@@ -68,49 +65,49 @@ class TestRecoveryClifford(TestCase):
             comb_net_cl_simple = rb.calculate_net_clifford([net_cl, rec_cliff])
             comb_net_cl = rb.calculate_net_clifford(comb_seq)
 
-            self.assertEqual(comb_net_cl, des_cl)
-            self.assertEqual(comb_net_cl_simple, des_cl)
+            assert comb_net_cl == des_cl
+            assert comb_net_cl_simple == des_cl
 
 
-class TestRB_sequence(TestCase):
+class TestRBSequence:
     def test_net_cliff(self):
         for i in range(len(clifford_group_single_qubit)):
             rb_seq = rb.randomized_benchmarking_sequence(500, desired_net_cl=i)
             net_cliff = rb.calculate_net_clifford(rb_seq)
-            self.assertEqual(net_cliff, i)
+            assert net_cliff == i
 
     def test_seed_reproduces(self):
         rb_seq_a = rb.randomized_benchmarking_sequence(500, seed=5)
         rb_seq_b = rb.randomized_benchmarking_sequence(500, seed=None)
         rb_seq_c = rb.randomized_benchmarking_sequence(500, seed=5)
         rb_seq_d = rb.randomized_benchmarking_sequence(500, seed=None)
-        self.assertTrue((rb_seq_a == rb_seq_c).all())
-        self.assertTrue((rb_seq_a != rb_seq_b).any)
-        self.assertTrue((rb_seq_c != rb_seq_b).any)
-        self.assertTrue((rb_seq_b != rb_seq_d).any)
+        assert (rb_seq_a == rb_seq_c).all()
+        assert (rb_seq_a != rb_seq_b).any()
+        assert (rb_seq_c != rb_seq_b).any()
+        assert (rb_seq_b != rb_seq_d).any()
 
 
-class TestGateDecomposition(TestCase):
+class TestGateDecomposition:
     def test_unique_elements(self):
         for gate in gate_decomposition:
-            self.assertEqual(gate_decomposition.count(gate), 1)
+            assert gate_decomposition.count(gate) == 1
 
     def test_average_number_of_gates_epst_efficient(self):
         from itertools import chain
         avg_nr_gates = len(list(chain(*gate_decomposition)))/24
-        self.assertEqual(avg_nr_gates, 1.875)
+        assert_almost_equal(avg_nr_gates, 1.875)
 
     def test_average_number_of_gates_epst_fixed_length(self):
         from itertools import chain
         avg_nr_gates = len(list(chain(*epstein_fixed_length_decomposition)))/24
-        self.assertEqual(avg_nr_gates, 3)
+        assert_almost_equal(avg_nr_gates, 3)
 
 
 ######################################################################
 # Two qubit clifford group below
 ######################################################################
 
-class TestHashedLookuptables(TestCase):
+class TestHashedLookuptables:
     def test_single_qubit_hashtable_constructed(self):
         hash_table = construct_clifford_lookuptable(tqc.SingleQubitClifford,
                                                     np.arange(24))
@@ -118,7 +115,7 @@ class TestHashedLookuptables(TestCase):
             Cl = tqc.SingleQubitClifford(i)
             target_hash = crc32(Cl.pauli_transfer_matrix.round().astype(int))
             table_idx = hash_table.index(target_hash)
-            self.assertEqual(table_idx, i)
+            assert table_idx == i
 
     def test_single_qubit_hashtable_file(self):
         hash_table = tqc.get_single_qubit_clifford_hash_table()
@@ -127,7 +124,7 @@ class TestHashedLookuptables(TestCase):
             Cl = tqc.SingleQubitClifford(i)
             target_hash = crc32(Cl.pauli_transfer_matrix.round().astype(int))
             table_idx = hash_table.index(target_hash)
-            self.assertEqual(table_idx, i)
+            assert table_idx == i
 
     def test_two_qubit_hashtable_constructed(self):
         hash_table = construct_clifford_lookuptable(tqc.TwoQubitClifford,
@@ -136,7 +133,7 @@ class TestHashedLookuptables(TestCase):
             Cl = tqc.TwoQubitClifford(i)
             target_hash = crc32(Cl.pauli_transfer_matrix.round().astype(int))
             table_idx = hash_table.index(target_hash)
-            self.assertEqual(table_idx, i)
+            assert table_idx == i
 
     def test_two_qubit_hashtable_file(self):
         hash_table = tqc.get_two_qubit_clifford_hash_table()
@@ -144,25 +141,26 @@ class TestHashedLookuptables(TestCase):
             Cl = tqc.TwoQubitClifford(i)
             target_hash = crc32(Cl.pauli_transfer_matrix.round().astype(int))
             table_idx = hash_table.index(target_hash)
-            self.assertEqual(table_idx, i)
+            assert table_idx == i
 
     def test_get_clifford_id(self):
         for i in range(24):
             Cl = tqc.SingleQubitClifford(i)
             idx = tqc.get_clifford_id(Cl.pauli_transfer_matrix)
-            self.assertEqual(idx, Cl.idx)
+            assert idx == Cl.idx
 
         for i in test_indices_2Q:
             Cl = tqc.TwoQubitClifford(i)
             idx = tqc.get_clifford_id(Cl.pauli_transfer_matrix)
-            self.assertEqual(idx, Cl.idx)
+            assert idx == Cl.idx
 
-class Test_CliffordGroupProperties(TestCase):
+
+class Test_CliffordGroupProperties:
 
     def test_single_qubit_group(self):
         hash_table = tqc.get_single_qubit_clifford_hash_table()
-        self.assertEqual(len(hash_table), 24)
-        self.assertEqual(len(np.unique(hash_table)), 24)
+        assert len(hash_table) == 24
+        assert len(np.unique(hash_table)) == 24
 
     # Testing the subgroups of the Clifford group
     def test_single_qubit_like_PTM(self):
@@ -171,9 +169,9 @@ class Test_CliffordGroupProperties(TestCase):
             clifford = tqc.single_qubit_like_PTM(idx)
             hash_val = crc32(clifford.round().astype(int))
             hash_table.append(hash_val)
-        self.assertEqual(len(hash_table), 24**2)
-        self.assertEqual(len(np.unique(hash_table)), 24**2)
-        with self.assertRaises(AssertionError):
+        assert len(hash_table) == 24**2
+        assert len(np.unique(hash_table)) == 24**2
+        with pytest.raises(AssertionError):
             clifford = tqc.single_qubit_like_PTM(24**2+1)
 
     def test_CNOT_like_PTM(self):
@@ -182,9 +180,9 @@ class Test_CliffordGroupProperties(TestCase):
             clifford = tqc.CNOT_like_PTM(idx)
             hash_val = crc32(clifford.round().astype(int))
             hash_table.append(hash_val)
-        self.assertEqual(len(hash_table), 5184)
-        self.assertEqual(len(np.unique(hash_table)), 5184)
-        with self.assertRaises(AssertionError):
+        assert len(hash_table) == 5184
+        assert len(np.unique(hash_table)) == 5184
+        with pytest.raises(AssertionError):
             clifford = tqc.CNOT_like_PTM(5184**2+1)
 
     def test_iSWAP_like_PTM(self):
@@ -193,9 +191,9 @@ class Test_CliffordGroupProperties(TestCase):
             clifford = tqc.iSWAP_like_PTM(idx)
             hash_val = crc32(clifford.round().astype(int))
             hash_table.append(hash_val)
-        self.assertEqual(len(hash_table), 5184)
-        self.assertEqual(len(np.unique(hash_table)), 5184)
-        with self.assertRaises(AssertionError):
+        assert len(hash_table) == 5184
+        assert len(np.unique(hash_table)) == 5184
+        with pytest.raises(AssertionError):
             clifford = tqc.iSWAP_like_PTM(5184+1)
 
     def test_SWAP_like_PTM(self):
@@ -204,33 +202,33 @@ class Test_CliffordGroupProperties(TestCase):
             clifford = tqc.SWAP_like_PTM(idx)
             hash_val = crc32(clifford.round().astype(int))
             hash_table.append(hash_val)
-        self.assertEqual(len(hash_table), 24**2)
-        self.assertEqual(len(np.unique(hash_table)), 24**2)
-        with self.assertRaises(AssertionError):
+        assert len(hash_table) == 24**2
+        assert len(np.unique(hash_table)) == 24**2
+        with pytest.raises(AssertionError):
             clifford = tqc.SWAP_like_PTM(24**2+1)
 
     def test_two_qubit_group(self):
         hash_table = tqc.get_two_qubit_clifford_hash_table()
-        self.assertEqual(len(hash_table), 11520)
-        self.assertEqual(len(np.unique(hash_table)), 11520)
+        assert len(hash_table) == 11520
+        assert len(np.unique(hash_table)) == 11520
 
 
 
 
-class TestCliffordCalculus(TestCase):
+class TestCliffordCalculus:
 
     def test_products(self):
         Cl_3 = tqc.SingleQubitClifford(3)
         Cl_3*Cl_3
-        self.assertEqual(Cl_3.idx, 3) # Pauli X
-        self.assertEqual((Cl_3*Cl_3).idx, 0) # The identity
+        assert Cl_3.idx == 3  # Pauli X
+        assert (Cl_3*Cl_3).idx == 0  # The identity
 
         Cl_3 = tqc.TwoQubitClifford(3)
-        self.assertEqual(Cl_3.idx, 3) # Pauli X on q0
-        self.assertEqual((Cl_3*Cl_3).idx, 0) # The identity
+        assert Cl_3.idx == 3 # Pauli X on q0
+        assert (Cl_3*Cl_3).idx == 0 # The identity
         product_hash = crc32((Cl_3*Cl_3).pauli_transfer_matrix.round().astype(int))
         target_hash = crc32(tqc.TwoQubitClifford(0).pauli_transfer_matrix.round().astype(int))
-        self.assertEqual(product_hash, target_hash)
+        assert product_hash == target_hash
 
     def test_product_order(self):
         """
@@ -251,36 +249,36 @@ class TestCliffordCalculus(TestCase):
         for i in range(24):
             Cl = tqc.SingleQubitClifford(i)
             Cl_inv = Cl.get_inverse()
-            self.assertEqual((Cl_inv*Cl).idx, 0)
+            assert (Cl_inv*Cl).idx == 0
 
     def test_inverse_two_qubit_clifford(self):
         for i in test_indices_2Q:
             Cl = tqc.TwoQubitClifford(i)
             Cl_inv = Cl.get_inverse()
-            self.assertEqual((Cl_inv*Cl).idx, 0)
+            assert (Cl_inv*Cl).idx == 0
 
-class TestCliffordGateDecomposition(TestCase):
+
+class TestCliffordGateDecomposition:
     def test_single_qubit_gate_decomposition(self):
         for i in range(24):
             CL = tqc.SingleQubitClifford(i)
             gate_dec = CL.gate_decomposition
-            self.assertIsInstance(gate_dec, list)
+            assert isinstance(gate_dec, list)
             for g in gate_dec:
-                self.assertIsInstance(g[0], str)
-                self.assertEqual(g[1], 'q0')
+                assert isinstance(g[0], str)
+                assert g[1] == 'q0'
 
     def test_two_qubit_gate_decomposition(self):
         for idx in (test_indices_2Q):
             CL = tqc.TwoQubitClifford(idx)
             gate_dec = CL.gate_decomposition
-            print(idx, gate_dec)
-            self.assertIsInstance(gate_dec, list)
+            assert isinstance(gate_dec, list)
             for g in gate_dec:
-                self.assertIsInstance(g[0], str)
+                assert isinstance(g[0], str)
                 if g[0] == 'CZ':
-                    self.assertEqual(g[1], ['q0', 'q1'])
+                    assert g[1] == ['q0', 'q1']
                 else:
-                    self.assertIn(g[1], ['q0', 'q1'])
+                    assert g[1] in ['q0', 'q1']
 
     def test_gate_decomposition_unique_single_qubit(self):
         hash_table = []
@@ -288,8 +286,8 @@ class TestCliffordGateDecomposition(TestCase):
             CL = tqc.SingleQubitClifford(i)
             gate_dec = CL.gate_decomposition
             hash_table.append(crc32(bytes(str(gate_dec), 'utf-8')))
-        self.assertEqual(len(hash_table),24)
-        self.assertEqual(len(np.unique(hash_table)),24)
+        assert len(hash_table) == 24
+        assert len(np.unique(hash_table)) == 24
 
     def test_gate_decomposition_unique_two_qubit(self):
         hash_table = []
@@ -297,19 +295,12 @@ class TestCliffordGateDecomposition(TestCase):
             CL = tqc.TwoQubitClifford(i)
             gate_dec = CL.gate_decomposition
             hash_table.append(crc32(bytes(str(gate_dec), 'utf-8')))
-        self.assertEqual(len(hash_table), 11520)
-        self.assertEqual(len(np.unique(hash_table)), 11520)
+        assert len(hash_table) == 11520
+        assert len(np.unique(hash_table)) == 11520
 
 
-
-
-class TestCliffordClassRBSeqs(TestCase):
-    """
-
-    """
+class TestCliffordClassRBSeqs:
     def test_single_qubit_randomized_benchmarking_sequence(self):
-        """
-        """
         seeds = [0, 100, 200, 300, 400]
         net_cliffs = np.arange(len(seeds))
         for seed, net_cl in zip(seeds, net_cliffs):
@@ -317,7 +308,7 @@ class TestCliffordClassRBSeqs(TestCase):
                 n_cl=20, desired_net_cl=0,  number_of_qubits=1, seed=0)
             cliffords = rb.randomized_benchmarking_sequence_old(
                 n_cl=20, desired_net_cl=0, seed=0)
-            np.testing.assert_array_equal(cliffords_single_qubit_class, cliffords)
+            assert_array_equal(cliffords_single_qubit_class, cliffords)
 
     def test_interleaved_randomized_benchmarking_sequence_1Q(self):
         seeds = [0, 100, 200, 300, 400]
@@ -332,8 +323,7 @@ class TestCliffordClassRBSeqs(TestCase):
             new_cliff = np.empty(cliffords.size*2-1, dtype=int)
             new_cliff[0::2] = cliffords
             new_cliff[1::2] = 0
-            np.testing.assert_array_equal(intl_cliffords,
-                                          new_cliff)
+            assert_array_equal(intl_cliffords, new_cliff)
 
 
     def test_interleaved_randomized_benchmarking_sequence_2Q(self):
