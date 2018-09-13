d5b8aeb82e4b2b7459d348eeb8bcf0f75b9fcf5d
FKMalino <oficjalny.krwiopijca@gmail.com>
2018-09-13 12:03:09 +0200

    Import fine timing analysis when loading  ma2

--- pycqed/analysis_v2/measurement_analysis.py
+++ pycqed/analysis_v2/measurement_analysis.py
@@ -59,7 +59,7 @@ from pycqed.analysis_v2.cryo_spectrumanalyzer_analysis import Cryospec_Analysis
 from pycqed.analysis_v2.distortions_analysis import Scope_Trace_analysis
 
 from pycqed.analysis_v2.optimization_analysis import OptimizationAnalysis
-from pycqed.analysis_v2.timing_cal_analysis import Timing_Cal_Flux_Coarse
+from pycqed.analysis_v2.timing_cal_analysis import Timing_Cal_Flux_Coarse, Timing_Cal_Flux_Fine
 
 from pycqed.analysis_v2.coherence_analysis import CoherenceTimesAnalysis, CoherenceTimesAnalysisSingle
 from pycqed.analysis_v2.spectroscopy_analysis import Spectroscopy, ResonatorSpectroscopy, VNA_analysis, complex_spectroscopy
