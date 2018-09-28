70777c05cfaaf24ab4b98d63d558ae875381e670
FKMalino <oficjalny.krwiopijca@gmail.com>
2018-09-28 17:32:10 +0200

    QI workers

--- pycqed/measurement/demonstrator_helper/execute_helpers_worker.py
+++ pycqed/measurement/demonstrator_helper/execute_helpers_worker.py
@@ -5,7 +5,10 @@ import pycqed as pq
 from qcodes.instrument.base import Instrument
 import qcodes as qc
 
-from QI_production_workers.CCLightWorker import CCLightWorker
+import pycqed.measurement.openql_experiments.generate_qi_cfg as gcfg_qi
+
+
+from QIProductionWorkers.CCLightWorker import CCLightWorker
 
 try:
     MC_demo = measurement_control.MeasurementControl(
@@ -22,7 +25,17 @@ except KeyError:
     MC_demo = Instrument.find_instrument('QInfinity_MC')
 
 
-config_fn ='D:\\GitHubRepos\\PycQED_py3\\pycqed\\measurement\\openql_experiments\\output\\cfg_CCL_QI.json'
 
-worker = CCLightWorker(name='Starmon', nr_qubits=2,
+config_fn ='D:\\GitHubRepos\\PycQED_py3\\pycqed\\measurement\\openql_experiments\\output\\cfg_CCL_QI.json'
+gcfg_qi.generate_config(filename=config_fn,
+                mw_pulse_duration=20,
+                ro_duration=5000,
+                flux_pulse_duration=40,
+                init_duration=300000)
+
+
+# worker = CCLightWorker(name='Starmon', nr_qubits=2,
+#                        is_simulator=False, openql_config_path=config_fn)
+
+worker = CCLightWorker(name='Octoqubit', nr_qubits=2,
                        is_simulator=False, openql_config_path=config_fn)
