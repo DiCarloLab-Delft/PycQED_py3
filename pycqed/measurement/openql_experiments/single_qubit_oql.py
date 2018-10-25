775c135979a1ba8ae937cfe6b80ffcdc89e961c8
AdriaanRol <adriaan.rol@gmail.com>
2018-10-25 10:53:06 +0200

    preventing open ql crash

--- pycqed/measurement/openql_experiments/single_qubit_oql.py
+++ pycqed/measurement/openql_experiments/single_qubit_oql.py
@@ -192,7 +192,7 @@ def AllXY(qubit_idx: int, platf_cfg: str, double_points: bool=True):
         else:
             js = 1
         for j in range(js):
-            k = oqh.create_kernel("AllXY_{}".format(i+j/2), p)
+            k = oqh.create_kernel("AllXY_{}".format(i+j//2), p)
             k.prepz(qubit_idx)
             k.gate(xy[0], [qubit_idx])
             k.gate(xy[1], [qubit_idx])
@@ -834,7 +834,7 @@ def ef_rabi_seq(q0: int,
         # cw_idx corresponds to special hardcoded pulses in the lutman
         cw_idx = i + 9
 
-        k = oqh.create_kernel("ef_A{}".format(amp), p)
+        k = oqh.create_kernel("ef_A{}".format(int(abs(1000*amp))), p)
         k.prepz(q0)
         k.gate('rx180', [q0])
         k.gate('cw_{:02}'.format(cw_idx), [q0])
