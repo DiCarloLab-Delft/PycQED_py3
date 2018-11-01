e7f57197d0e26f9f78af82854e04b51324d409a0
FKMalina <filipkmalinowski@gmail.com>
2018-11-01 10:42:26 +0100

    Added parametric plotting in VNA_analysis

--- pycqed/analysis_v2/spectroscopy_analysis.py
+++ pycqed/analysis_v2/spectroscopy_analysis.py
@@ -262,6 +262,18 @@ class complex_spectroscopy(Spectroscopy):
                                        'plotsize': plotsize,
                                        'do_legend':True
                                        }
+            self.plot_dicts['plane'] = {'plotfn': plot_fn,
+                                       'xvals': proc_data_dict['plot_real'],
+                                       'yvals': proc_data_dict['plot_imag'],
+                                       'title': 'S21 phase: %s' % (self.timestamps[0]),
+                                       'xlabel': proc_data_dict['real_label'],
+                                       'ylabel': proc_data_dict['imag_label'],
+                                       'yrange': proc_data_dict['amp_range'],
+                                       'xrange': proc_data_dict['amp_range'],
+                                       'setlabel':'plane',
+                                       'plotsize': plotsize,
+                                       'do_legend':True
+                                       }
             pdict_names = ['amp', 'phase', 'real', 'imag']
 
             self.figs['combined'], axs = plt.subplots(
@@ -340,6 +352,17 @@ class VNA_analysis(complex_spectroscopy):
                 'plotfn': self.plot_fit,
                 'fit_res': self.fit_dicts['reso_fit']['fit_res'],
                 'output_mod_fn':lambda a: np.unwrap(np.angle(a)),
+                'plot_init': self.options_dict['plot_init'],
+                'setlabel': 'hanger',
+                'line_kws': {'color': 'r'},
+                'do_legend': True}
+
+            self.plot_dicts['reso_fit_phase'] = {
+                'ax_id': 'plane',
+                'plotfn': self.plot_fit,
+                'fit_res': self.fit_dicts['reso_fit']['fit_res'],
+                'output_mod_fn': np.imag,
+                'output_mod_fn_x': np.real,
                 'plot_init': self.options_dict['plot_init'],
                 'setlabel': 'hanger',
                 'line_kws': {'color': 'r'},
