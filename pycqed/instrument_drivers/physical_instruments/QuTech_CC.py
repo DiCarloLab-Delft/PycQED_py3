3dc486557c0fe2b5ff719c945d802b9b1d207e62
Wouter Vlothuizen <wouter.vlothuizen@tno.nl>
2019-01-15 23:51:32 +0100

    first round of cleanup

--- pycqed/instrument_drivers/physical_instruments/QuTech_CC.py
+++ pycqed/instrument_drivers/physical_instruments/QuTech_CC.py
@@ -2,30 +2,23 @@
     File:               QuTech_CC.py
     Author:             Wouter Vlothuizen, QuTech
     Purpose:            Python control of Qutech Central Controller
-    Prerequisites:      QCodes, SCPI
     Notes:              based on QuTech_CC.py (KKL, XFu)
     Usage:
     Bugs:
-    Tabs: 4
-Revision history:
- *    0.1.0   :            : KKL  : * Created this file.
- *    0.2.0   :            : XFu  : * Refined the intialization process.
- *    0.2.1   : 13-01-2018 : XFu  : * Change the parameters into integer.
 """
-
 
 from .SCPI import SCPI
 from qcodes.instrument.base import Instrument
-from qcodes import Parameter
+#from qcodes import Parameter
 from qcodes.instrument.parameter import ManualParameter
 from qcodes import validators as vals
 import os
 import logging
 import json
-import sys
-import traceback
+#import sys
+#import traceback
 import array
-import re
+#import re
 
 log = logging.getLogger(__name__)
 
@@ -50,6 +43,10 @@ class Qutech_CC(SCPI):
     """
     exceptionLevel = logging.CRITICAL
 
+    ##########################################################################
+    # 'public' functions for the end user
+    ##########################################################################
+
     def __init__(self, name, address, port, log_level=False, **kwargs):
         self.model = name
         self._dummy_instr = False
@@ -58,7 +55,7 @@ class Qutech_CC(SCPI):
             super().__init__(name, address, port, **kwargs)
         except Exception as e:
             # Setting up the SCPI sometimes fails the first time.  If this
-            # happens a second effort to initialize and settup the connection
+            # happens a second effort to initialize and setup the connection
             # is made
             print("Failed to connect (" + str(e) + "). The system will retry" +
                   " to connect")
@@ -67,95 +64,103 @@ class Qutech_CC(SCPI):
         self.get_idn()
         self.add_standard_parameters()
         self.add_additional_parameters()
-        self._init_submodules()
         self.connect_message()
 
-    def _init_submodules(self):
-        """
-        The parser helper objects are initialized in this function.
-        """
-        self.microcode = CCLightMicrocode()
-        self.QISA = QISA_Driver()
-        self.QISA.enableScannerTracing(False)
-        self.QISA.enableParserTracing(False)
-        self.QISA.setVerbose(False)
-
-        curdir = os.path.dirname(__file__)
-        qmap_fn = os.path.join(curdir, '_CCL', 'qisa_opcode.qmap')
-
-        self.qisa_opcode(qmap_fn)
+    def start(self, getOperationComplete=True):
+        self.write('awgcontrol:run:immediate')
+        # FIXME: Introduced to work around AWG8 triggering issue
+        if getOperationComplete:
+            self.getOperationComplete()
 
     def stop(self, getOperationComplete=True):
-        self.run(0),
-        self.enable(0)
-        # Introduced to work around AWG8 triggering issue
+        self.write('awgcontrol:stop:immediate')
+        # FIXME: Introduced to work around AWG8 triggering issue
         if getOperationComplete:
             self.getOperationComplete()
 
-    def start(self, getOperationComplete=True):
-        self.enable(1)
-        self.run(1)
-        # Introduced to work around AWG8 triggering issue
-        if getOperationComplete:
-            self.getOperationComplete()
+    def get_idn(self):
+        self.version_info = {}
+        try:
+            id_string = ""
+            id_string = self.ask('*IDN?')
+            id_string = id_string.replace("'", "\"")
+            self.version_info = json.loads(id_string)
+        except Exception as e:
+            logging.warn('Error: failed to retrieve IDN from CC', str(e))
 
-    def add_standard_parameters(self):
-        """
-        Function to automatically generate the CC-Light specific functions
-        from the qcodes parameters. The function uses the add_parameter
-        function internally.
-        """
-        self.parameter_list = self._read_parameters()
+        self.version_info["Driver Version"] = self.driver_version
 
-        for parameter in self.parameter_list:
-            name = parameter["name"]
-            del parameter["name"]
+        return self.version_info
 
-            if ("vals" in parameter):
-                validator = parameter["vals"]
-                try:
-                    val_type = validator["type"]
+    def print_readable_idn(self):
+        for key, value in self.version_info.items():
+            print("{0: >30s} :  {1:}".format(key, value))
 
-                    if (val_type == "Bool"):
-                        parameter["vals"] = vals.Ints(0, 1)
-                        parameter['get_parser'] = int
+    def print_qisa_opcodes(self):
+        if self.QISA is None:
+            log.info("The assembler of CCLight has not been initialized yet.")
+            return
 
-                    elif (val_type == "Non_Neg_Number"):
-                        if ("range" in validator):
-                            val_min = validator["range"][0]
-                            val_max = validator["range"][1]
-                        else:
-                            val_min = 0
-                            val_max = INT32_MAX
+        print(self.QISA.dumpInstructionsSpecification())
 
-                        parameter["vals"] = vals.PermissiveInts(val_min,
-                                                                val_max)
-                        parameter['get_parser'] = int
-                        parameter['set_parser'] = int
+    def print_control_store(self):
+        if self.microcode is None:
+            log.info("The microcode unit of CCLight has not been"
+                " initialized yet.")
+            return
 
-                    else:
-                        log.warning("Failed to set the validator for the" +
-                                    " parameter " + name + ", because of a" +
-                                    " unknown validator type: '" + val_type +
-                                    "'")
+        self.microcode.dump_microcode()
 
-                except Exception as e:
-                    log.warning(
-                        "Failed to set the validator for the parameter " +
-                        name + ".(%s)", str(e))
+    def print_qisa_with_control_store(self):
+        if self.microcode is None:
+            log.info("The microcode unit of CCLight has not been"
+                " initialized yet.")
+            return
 
-            try:
-                log.info("Adding parameter:")
-                for key, value in parameter.items():
-                    log.info("\t", key, value)
-                log.info("\n")
+        if self.QISA is None:
+            log.info("The assembler of CCLight has not been initialized yet.")
+            return
 
-                self.add_parameter(name, **parameter)
+        q_arg = OrderedDict()
 
-            except Exception as e:
-                log.warning("Failed to create the parameter " + name +
-                            ", because of a unknown keyword in this" +
-                            " parameter.(%s)", str(e))
+        insn_opcodes_str = self.QISA.dumpInstructionsSpecification()
+        lines = insn_opcodes_str.split('\n')
+        trimed_lines = [line.strip() for line in lines \
+                        if line.startswith('def_q')]
+
+        # put every instruction with its opcode into a dict
+        for line in trimed_lines:
+            name, opcode = line.split('=')
+            name = name.strip().lower()
+            opcode = opcode.strip().lower()
+
+            # convert the opcode into an integer
+            if opcode.startswith("0x"):
+                base = 16
+            elif opcode.startswith("0o"):
+                base = 8
+            elif opcode.startswith("0b"):
+                base = 2
+            else:
+                base = 10
+            opcode = int(opcode, base)
+
+            if name.startswith("def_q_arg_none"):
+                q_arg[name[16:-2]] = opcode
+            if name.startswith("def_q_arg_tt"):
+                q_arg[name[14:-2]] = opcode
+            if name.startswith("def_q_arg_st"):
+                q_arg[name[14:-2]] = opcode
+
+        print("Instruction      Codewords")
+        for key, value in q_arg.items():
+            print('  {:<10s}:  '.format(key), end = '')
+            self.microcode.print_cs_line_no_header(value)
+            print("")
+
+    ##########################################################################
+    # 'private' functions, internal to the driver
+    ##########################################################################
 
     def add_additional_parameters(self):
         """
@@ -281,93 +286,69 @@ class Qutech_CC(SCPI):
 
         return results
 
-    def get_idn(self):
-        self.version_info = {}
-        try:
-            id_string = ""
-            id_string = self.ask('*IDN?')
-            id_string = id_string.replace("'", "\"")
-            self.version_info = json.loads(id_string)
-        except Exception as e:
-            logging.warn('Error: failed to retrive IDN from CC-Light.', str(e))
+    def add_standard_parameters(self):
+        """
+        Function to automatically generate the CC-Light specific functions
+        from the qcodes parameters. The function uses the add_parameter
+        function internally.
+        """
+        self.parameter_list = self._read_parameters()
 
-        self.version_info["Driver Version"] = self.driver_version
+        for parameter in self.parameter_list:
+            name = parameter["name"]
+            del parameter["name"]
 
-        return self.version_info
+            if ("vals" in parameter):
+                validator = parameter["vals"]
+                try:
+                    val_type = validator["type"]
 
-    def print_readable_idn(self):
-        for key, value in self.version_info.items():
-            print("{0: >30s} :  {1:}".format(key, value))
+                    if (val_type == "Bool"):
+                        parameter["vals"] = vals.Ints(0, 1)
+                        parameter['get_parser'] = int
 
-    def print_qisa_opcodes(self):
-        if self.QISA is None:
-            log.info("The assembler of CCLight has not been initialized yet.")
-            return
+                    elif (val_type == "Non_Neg_Number"):
+                        if ("range" in validator):
+                            val_min = validator["range"][0]
+                            val_max = validator["range"][1]
+                        else:
+                            val_min = 0
+                            val_max = INT32_MAX
 
-        print(self.QISA.dumpInstructionsSpecification())
+                        parameter["vals"] = vals.PermissiveInts(val_min,
+                                                                val_max)
+                        parameter['get_parser'] = int
+                        parameter['set_parser'] = int
 
-    def print_control_store(self):
-        if self.microcode is None:
-            log.info("The microcode unit of CCLight has not been"
-                " initialized yet.")
-            return
+                    else:
+                        log.warning("Failed to set the validator for the" +
+                                    " parameter " + name + ", because of a" +
+                                    " unknown validator type: '" + val_type +
+                                    "'")
 
-        self.microcode.dump_microcode()
+                except Exception as e:
+                    log.warning(
+                        "Failed to set the validator for the parameter " +
+                        name + ".(%s)", str(e))
 
-    def print_qisa_with_control_store(self):
-        if self.microcode is None:
-            log.info("The microcode unit of CCLight has not been"
-                " initialized yet.")
-            return
+            try:
+                log.info("Adding parameter:")
+                for key, value in parameter.items():
+                    log.info("\t", key, value)
+                log.info("\n")
 
-        if self.QISA is None:
-            log.info("The assembler of CCLight has not been initialized yet.")
-            return
+                self.add_parameter(name, **parameter)
 
-        q_arg = OrderedDict()
+            except Exception as e:
+                log.warning("Failed to create the parameter " + name +
+                            ", because of a unknown keyword in this" +
+                            " parameter.(%s)", str(e))
 
-        insn_opcodes_str = self.QISA.dumpInstructionsSpecification()
-        lines = insn_opcodes_str.split('\n')
-        trimed_lines = [line.strip() for line in lines \
-                        if line.startswith('def_q')]
-
-        # put every instruction with its opcode into a dict
-        for line in trimed_lines:
-            name, opcode = line.split('=')
-            name = name.strip().lower()
-            opcode = opcode.strip().lower()
-
-            # convert the opcode into an integer
-            if opcode.startswith("0x"):
-                base = 16
-            elif opcode.startswith("0o"):
-                base = 8
-            elif opcode.startswith("0b"):
-                base = 2
-            else:
-                base = 10
-            opcode = int(opcode, base)
-
-            if name.startswith("def_q_arg_none"):
-                q_arg[name[16:-2]] = opcode
-            if name.startswith("def_q_arg_tt"):
-                q_arg[name[14:-2]] = opcode
-            if name.startswith("def_q_arg_st"):
-                q_arg[name[14:-2]] = opcode
-
-        print("Instruction      Codewords")
-        for key, value in q_arg.items():
-            print('  {:<10s}:  '.format(key), end = '')
-            self.microcode.print_cs_line_no_header(value)
-            print("")
-
-###############################################################################
-
-#  These are functions which cannot be cast into the standard
-#  form or not that I know of.
-#  They will be added manually using add_parameter explicitly
-
-###############################################################################
+    ###########################################################################
+    #  These are functions which cannot be cast into the standard
+    #  form or not that I know of.
+    #  They will be added manually using add_parameter explicitly
+    ###########################################################################
 
     def _upload_instructions(self, filename):
         """
@@ -470,9 +451,10 @@ class Qutech_CC(SCPI):
         fn = os.path.join(pathname, base_name + ext)
         return fn
 
-class dummy_CCL(CCL):
+
+class dummy_CC(Qutech_CC):
     """
-    Dummy CCL all paramaters are manual and all other methods include pass
+    Dummy CC: all parameters are manual and all other methods include pass
     statements
     """
 
