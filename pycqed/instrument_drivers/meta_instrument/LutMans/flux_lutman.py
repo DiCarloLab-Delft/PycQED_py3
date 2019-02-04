1c2e4777f560a30d0bbcb1a382e6048cc1823dcd
fbattistel <39129759+fbattistel@users.noreply.github.com>
2019-02-04 12:36:31 +0100

    Revert "cz length in long filename with 1 decimal"
    This reverts commit c8438733975c79511f58a5345717e13decf948d3.

--- pycqed/instrument_drivers/meta_instrument/LutMans/flux_lutman.py
+++ pycqed/instrument_drivers/meta_instrument/LutMans/flux_lutman.py
@@ -7,7 +7,7 @@ from qcodes.utils import validators as vals
 from pycqed.instrument_drivers.pq_parameters import NP_NANs
 from pycqed.measurement.waveform_control_CC import waveform as wf
 from pycqed.measurement.waveform_control_CC import waveforms_flux as wfl
-#from pycqed.measurement.openql_experiments.openql_helpers import clocks_to_s
+from pycqed.measurement.openql_experiments.openql_helpers import clocks_to_s
 from qcodes.plots.pyqtgraph import QtPlot
 import matplotlib.pyplot as plt
 from pycqed.analysis.tools.plotting import set_xlabel, set_ylabel
