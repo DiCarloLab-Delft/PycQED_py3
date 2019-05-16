12443b6090c036b3481b7931b120e07d5541f0d3
Timo1104 <timovanabswoude@gmail.com>
2019-05-16 11:10:11 +0200

    Added mock qubit rabi measurement and mock qubit test

--- pycqed/instrument_drivers/meta_instrument/qubit_objects/mock_CCL_Transmon.py
+++ pycqed/instrument_drivers/meta_instrument/qubit_objects/mock_CCL_Transmon.py
@@ -1,19 +1,19 @@
-from .qubit_object import CCLight_Transmon
+from .CCL_Transmon import CCLight_Transmon
 
-
+import numpy as np
 from pycqed.measurement import sweep_functions as swf
 from pycqed.measurement import detector_functions as det
-
+from pycqed.analysis import measurement_analysis as ma
 from pycqed.instrument_drivers.meta_instrument.qubit_objects.qubit_object import Qubit
 from qcodes.instrument.parameter import ManualParameter
 from qcodes.utils import validators as vals
+
 
 class Mock_CCLight_Transmon(CCLight_Transmon):
 
     def __init__(self, name, **kw):
         super().__init__(name, **kw)
         self.add_mock_params()
-
 
     def add_mock_params(self):
         """
@@ -24,7 +24,6 @@ class Mock_CCLight_Transmon(CCLight_Transmon):
             - describe "hidden" parameters to mock real experiments.
         """
 
-
         self.add_parameter(
             'mock_freq_qubit', label='qubit frequency', unit='Hz',
             docstring='A fixed value, can be made fancier by making it depend on Flux through E_c, E_j and flux',
@@ -33,12 +32,27 @@ class Mock_CCLight_Transmon(CCLight_Transmon):
         self.add_parameter('mock_freq_res', label='qubit frequency', unit='Hz',
                            parameter_class=ManualParameter)
 
+        self.add_parameter('mock_mw_amp180', label='Pi-pulse amplitude', unit='V',
+                           parameter_class=ManualParameter)
 
     def measure_rabi(self, MC=None, amps=None,
                      analyze=True, close_fig=True, real_imag=True,
                      prepare_for_timedomain=True, all_modules=False):
         if MC is None:
             MC = self.instr_MC.get_instr()
-        s = swf.None_sweep()
-        d = det.Mock_detector() # Does not exist yet
+        if amps is None:
+            amps = np.linspace(0.1, 1, 31)
+        s = swf.None_Sweep()
+        f_rabi = 1
+        mocked_values = np.cos(2*np.pi*f_rabi*amps)
+        # Add (0,0.5) normal distributed noise
+        mocked_values += np.random.normal(0, 0.5, np.size(mocked_values))
+        d = det.Mock_Detector(value_names=['Rabi Amplitude'], value_units=['-'],
+                              detector_control='soft', mock_values=mocked_values)  # Does not exist yet
 
+        MC.set_sweep_function(s)
+        MC.set_sweep_points(amps)
+        MC.set_detector_function(d)
+        MC.run('mock_rabi_')
+        ma.Rabi_Analysis(label='mock_')
+        return True
