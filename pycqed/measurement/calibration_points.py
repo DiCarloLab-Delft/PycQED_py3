fc991c3d4ff4e1f6b1507350763ec5ce78000385
Nathan Lacroix <lna@ethz.ch>
2019-06-24 16:40:20 +0200

    Add rotation and indices analysis helper functions to CalibrationPoints

--- pycqed/measurement/calibration_points.py
+++ pycqed/measurement/calibration_points.py
@@ -66,6 +66,98 @@ class CalibrationPoints:
     #
     #     return sweep_points
 
+    def get_states(self, qb_names=None):
+        """
+        Get calibrations states given a (subset) of qubits of self.qb_names.
+        This function is a helper for the analysis which works with information
+        per qubit.
+        Args:
+            qb_names: list of qubit names
+
+        Returns: dict where keys are qubit names and values are the calibration
+            states for this particular qubit
+        """
+
+        qb_names = self._check_qb_names(qb_names)
+
+        return {qbn: [s[self.qb_names.index(qbn)] for s in self.states]
+                for qbn in qb_names}
+
+    def get_indices(self, qb_names=None):
+        """
+        Get calibration indices
+        Args:
+            qb_names: qubit name or list of qubit names to retrieve
+                the indices of. Defaults to all.
+
+        Returns: dict where keys are qb_names and values dict of {state: ind}
+
+        """
+        qb_names = self._check_qb_names(qb_names)
+        indices = dict()
+        states = self.get_states(qb_names)
+
+        for qbn in qb_names:
+            unique, idx, inv = np.unique(states[qbn], return_inverse=True,
+                                        return_index=True)
+            indices[qbn] = {s: [-len(states[qbn]) + j
+                                for j in range(len(inv)) if i == inv[j]]
+                            for i, s in enumerate(unique)}
+
+        log.info(f"Calibration Points Indices: {indices}")
+        return indices
+
+    def _check_qb_names(self, qb_names):
+        if qb_names is None:
+            qb_names = self.qb_names
+        elif np.ndim(qb_names) == 0:
+            qb_names = [qb_names]
+        for qbn in qb_names:
+            assert qbn in self.qb_names, f"{qbn} not in Calibrated Qubits: " \
+                f"{self.qb_names}"
+
+        return qb_names
+
+    def get_rotations(self, last_ge_pulses, qb_names=None):
+        """
+        Get rotation dictionaries for each qubit in qb_names,
+        as used by the analysis for plotting.
+        Args:
+            qb_names (list or string): qubit names. Defaults to all.
+            last_ge_pulses (list or bool): one for each qb in the same order as
+                specified in qb_names
+
+        Returns:
+             dict where keys are qb_names and values are dict specifying
+             rotations.
+
+        """
+        qb_names = self._check_qb_names(qb_names)
+        rotations = dict()
+
+        if len(qb_names) == 0:
+            return rotations
+        if len(qb_names) == 1:
+            last_ge_pulses = [last_ge_pulses] if \
+                isinstance(last_ge_pulses, bool) else last_ge_pulses
+        else:
+            i, j = len(qb_names), \
+                   1 if isinstance(last_ge_pulses, bool) else len(last_ge_pulses)
+            assert i == j, f"Size of qb_names and last_ge_pulses don't " \
+                f"match: {i} vs {j}"
+
+        states = self.get_states(qb_names)
+        for i, qbn in enumerate(qb_names):
+            # get unique states in reversed alphabetical order: g, [e, f]
+            order = {"g":0, "e": 1, "f": 2}
+            unique = list(np.unique(states[qbn]))
+            unique.sort(key=lambda s: order[s])
+            if len(unique) == 3:
+                states = np.delete(unique, 1 if last_ge_pulses[i] else 0)
+            rotations[qbn] = {states[i]: i for i in range(len(states))}
+        log.info(f"Calibration Points Rotation: {rotations}")
+        return rotations
+    
     @staticmethod
     def single_qubit(qubit_name, states, n_per_state=2):
         return CalibrationPoints.multi_qubit([qubit_name], states, n_per_state)
