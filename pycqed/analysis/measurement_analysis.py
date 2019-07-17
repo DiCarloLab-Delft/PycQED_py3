780fa81fb7ade440876c003f4a3b2e419e2fedd8
Adriaan Rol <adriaan.rol@gmail.com>
2019-07-17 17:18:39 +0200

    changed rcond, should work with numpy<1.14

--- pycqed/analysis/measurement_analysis.py
+++ pycqed/analysis/measurement_analysis.py
@@ -8656,7 +8656,8 @@ class DoubleFrequency(TD_Analysis):
             for ii in range(len(measured_values)):
                 for jj in range(3):
                     E[ii,:]=np.exp(expvals*sweep_values[ii])
-            coeff = np.linalg.lstsq(E,measured_values,rcond=None)[0]
+            # FIXME: when using numpy >1.14 we should change to rcond=None
+            coeff = np.linalg.lstsq(E, measured_values, rcond=-1)[0]
             amp_guess = 2*np.abs(coeff[[0]])
             phi_guess = np.angle(coeff[[0]])
         else:
