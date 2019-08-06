29b1b9a03a4d61203a3af4d972ea8cbf99977a00
AdriaanRol <adriaan.rol@gmail.com>
2019-08-06 11:19:48 +0200

    updated mw lutman for QWG

--- pycqed/instrument_drivers/meta_instrument/LutMans/mw_lutman.py
+++ pycqed/instrument_drivers/meta_instrument/LutMans/mw_lutman.py
@@ -402,6 +402,53 @@ class QWG_MW_LutMan(Base_MW_LutMan):
         assert val_Q == val_I
         return val_I
 
+    def load_waveform_onto_AWG_lookuptable(self, waveform_name: str,
+                                           regenerate_waveforms: bool=False):
+        if regenerate_waveforms:
+            self.generate_standard_waveforms()
+        # waveform_name is pulse string (i.e. 'X180')
+        # waveforms is the tuple (G,D) with the actual pulseshape array
+        # codewords is the tuple (A,B) with the strings wave_chY_cwXXX
+        # codeword_int is the number XXX (see line above)
+        codeword_int = waveform_name
+        wf_name = self._get_wf_name_from_cw(codeword_int)
+        waveforms = self._wave_dict[codeword_int]
+        # get redundant codewords as a list
+        redundant_cw_list = get_redundant_codewords(codeword_int,
+                                                    bit_width=self.bit_width(),
+                                                    bit_shift=self.bit_shift())
+        # update all of them
+        for redundant_cw_idx in redundant_cw_list:
+            redundant_cw_I = 'wave_ch{}_cw{:03}'.format(self.channel_I(),
+                                                        redundant_cw_idx)
+            self.AWG.get_instr().set(redundant_cw_I, waveforms[0])
+            redundant_cw_Q = 'wave_ch{}_cw{:03}'.format(self.channel_Q(),
+                                                        redundant_cw_idx)
+            self.AWG.get_instr().set(redundant_cw_Q, waveforms[1])
+
+    def _get_wf_name_from_cw(self, codeword: int):
+        for idx, waveform in self.LutMap().items():
+            if int(idx) == codeword:
+                return waveform['name']
+        raise ValueError("Codeword {} not specified"
+                         " in LutMap".format(codeword))
+
+    def _get_cw_from_wf_name(self, wf_name: str):
+        for idx, waveform in self.LutMap().items():
+            if wf_name == waveform['name']:
+                return int(idx)
+        raise ValueError("Waveform {} not specified"
+                         " in LutMap".format(wf_name))
+
+    def apply_mixer_predistortion_corrections(self, wave_dict):
+        M = wf.mixer_predistortion_matrix(self.mixer_alpha(),
+                                          self.mixer_phi())
+
+        self.AWG.get_instr()._setMatrix(chPair=self.channel_I(), mat=M)
+
+        return wave_dict
+
+
 
 class AWG8_MW_LutMan(Base_MW_LutMan):
 
