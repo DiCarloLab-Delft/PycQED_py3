ec49b792a65c8fcb9588e0181055bf3f8ea443ba
Niels Haandbaek <niels@zhinst.com>
2019-08-14 10:36:39 +0200

    Removed a redundant test as suggested by AdriaanRol.

--- pycqed/tests/test_mw_lutman.py
+++ pycqed/tests/test_mw_lutman.py
@@ -198,9 +198,6 @@ class Test_MW_LutMan(unittest.TestCase):
         # Does not check the full lutmap
         dict_contained_in(expected_dict, self.AWG8_VSM_MW_LutMan.LutMap())
 
-    def test_realtime_loading_square_wf_AWG8_VSM(self):
-        self.AWG8_VSM_MW_LutMan.load_waveform_onto_AWG_lookuptable('square')
-
     def test_uploading_standard_pulses_AWG8_VSM(self):
         # Tests that all waveforms are present and no error is raised.
         self.AWG8_VSM_MW_LutMan.load_waveforms_onto_AWG_lookuptable()
