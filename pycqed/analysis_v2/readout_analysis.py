Fixes bug in MultiQubit_TimeDomain_Analysis related to cal_points, and adds normalization of    data if one RO channe and no cal points. Updates the T1 measurements to new wfm generation. Adds measure_ramsey_with_flux_pulse to multi_qubit_module.py. Adds Ramsey_with_flux_pulse_meas_seq. Updates ro_dynamic_phase_seq to new wfm generation framework. Renames T1_seq to t1_active_reset and updates it to new wfm generation.

pycqed/analysis_v2/readout_analysis.py
@@ -1278,6 +1278,7 @@ class MultiQubit_SingleShot_Analysis(ba.BaseDataAnalysis):
                          extract_only=extract_only, do_fitting=do_fitting)
 
         self.n_readouts = options_dict['n_readouts']
+        self.kept_shots = 0
         self.thresholds = options_dict['thresholds']
         self.channel_map = options_dict['channel_map']
         self.use_preselection = options_dict.get('use_preselection', False)
@@ -1381,7 +1382,7 @@ class MultiQubit_SingleShot_Analysis(ba.BaseDataAnalysis):
 
         table = np.zeros((n_readouts, len(observables)))
 
-        print(observables)
+        # print(observables)
 
         for qubit, results in shots_of_qubits.items():
             res_e[qubit] = np.array(results).reshape((n_readouts, -1),

pycqed/analysis_v2/timedomain_analysis.py
@@ -298,39 +298,51 @@ class MultiQubit_TimeDomain_Analysis(ba.BaseDataAnalysis):
         cal_points = self.get_param_value('cal_points')
         last_ge_pulses = self.get_param_value('last_ge_pulses',
                                               default_value=False)
-        self.cp = eval(cal_points)
+        try:
+            self.cp = eval(cal_points)
 
-        # for now assuming the same for all qubits.
-        self.cal_states_dict = self.cp.get_indices()[self.qb_names[0]]
-
-        if rotate:
-            cal_states_rots = self.cp.get_rotations(last_ge_pulses,
-                    self.qb_names[0])[self.qb_names[0]]
-            self.cal_states_rotations = self.get_param_value(
-                'cal_states_rotations', default_value=cal_states_rots)
-        else:
-            self.cal_states_rotations = None
-
-        sweep_points_w_calpts = \
-            {qbn: {'sweep_points': self.cp.extend_sweep_points(
-                self.proc_data_dict['sweep_points_dict'][qbn][
-                    'sweep_points'], qbn)} for qbn in self.qb_names}
-        self.proc_data_dict['sweep_points_dict'] = sweep_points_w_calpts
+            # for now assuming the same for all qubits.
+            self.cal_states_dict = self.cp.get_indices()[self.qb_names[0]]
+            if rotate:
+                cal_states_rots = self.cp.get_rotations(last_ge_pulses,
+                        self.qb_names[0])[self.qb_names[0]] if rotate else None
+                self.cal_states_rotations = self.get_param_value(
+                    'cal_states_rotations', default_value=cal_states_rots)
+            else:
+                self.cal_states_rotations = None
+            sweep_points_w_calpts = \
+                {qbn: {'sweep_points': self.cp.extend_sweep_points(
+                    self.proc_data_dict['sweep_points_dict'][qbn][
+                        'sweep_points'], qbn)} for qbn in self.qb_names}
+            self.proc_data_dict['sweep_points_dict'] = sweep_points_w_calpts
+        except TypeError as e:
+            log.error(e)
+            log.warning("Failed retrieving cal point objects or states. "
+                        "Please update measurement to provide cal point object "
+                        "in metadata. Trying to get them using the old way ...")
+            if rotate:
+                self.cal_states_rotations = self.get_param_value(
+                    'cal_states_rotations', default_value=None)
+            else:
+                self.cal_states_rotations = None
+            self.cal_states_dict = self.get_param_value('cal_states_dict',
+                                                         default_value={})
 
         # create projected_data_dict
         self.data_to_fit = self.get_param_value('data_to_fit')
         if self.cal_states_rotations is not None:
-            print('here')
             self.cal_states_analysis()
         else:
+            # this assumes data obetained with classifier detector!
+            # ie pg, pe, pf are expected to be in the value_names
             self.proc_data_dict['projected_data_dict'] = OrderedDict()
             for qbn, data_dict in self.proc_data_dict[
                     'meas_results_per_qb'].items():
                 self.proc_data_dict['projected_data_dict'][qbn] = OrderedDict()
                 for state_prob in ['pg', 'pe', 'pf']:
                     self.proc_data_dict['projected_data_dict'][qbn].update(
-                        {state_prob: data for key, data in data_dict.items() if
-                         state_prob in key})
+                        {state_prob: data for key, data in data_dict.items()
+                         if state_prob in key})
             if self.cal_states_dict is None:
                 self.cal_states_dict = {}
             self.num_cal_points = np.array(list(
@@ -465,11 +477,16 @@ class MultiQubit_TimeDomain_Analysis(ba.BaseDataAnalysis):
             rotated_data_dict[qb_name] = OrderedDict()
             if len(meas_res_dict) == 1:
                 # one RO channel per qubit
-                rotated_data_dict[qb_name][data_to_fit[qb_name]] = \
-                    a_tools.normalize_data_v3(
-                        data=meas_res_dict[list(meas_res_dict)[0]],
-                        cal_zero_points=cal_zero_points,
-                        cal_one_points=cal_one_points)
+                if cal_zero_points is None and cal_one_points is None:
+                    data = meas_res_dict[list(meas_res_dict)[0]]
+                    rotated_data_dict[qb_name][data_to_fit[qb_name]] = \
+                        (data - np.min(data))/(np.max(data) - np.min(data))
+                else:
+                    rotated_data_dict[qb_name][data_to_fit[qb_name]] = \
+                        a_tools.normalize_data_v3(
+                            data=meas_res_dict[list(meas_res_dict)[0]],
+                            cal_zero_points=cal_zero_points,
+                            cal_one_points=cal_one_points)
             elif list(meas_res_dict) == channel_map[qb_name]:
                 # two RO channels per qubit
                 rotated_data_dict[qb_name][data_to_fit[qb_name]], _, _ = \

pycqed/instrument_drivers/meta_instrument/qubit_objects/QuDev_transmon.py
@@ -746,51 +746,59 @@ class QuDev_transmon(Qubit):
         MC.run('Rabi_amp90_scales_n{}'.format(n)+ self.msmt_suffix)
 
     def measure_T1(self, times=None, analyze=True, upload=True,
-                   close_fig=True, cal_points=True, label=None,
+                   last_ge_pulse=False, n_cal_points_per_state=2,
+                   cal_states='auto', for_ef=False, classified_ro=False,
+                   prep_params=None, label=None,
                    exp_metadata=None):
 
         if times is None:
             raise ValueError("Unspecified times for measure_T1")
-        if np.any(times>1e-3):
+        if np.any(times > 1e-3):
             log.warning('The values in the times array might be too large.'
-                            'The units should be seconds.')
+                        'The units should be seconds.')
 
         self.prepare(drive='timedomain')
 
         MC = self.instr_mc.get_instr()
 
+        if prep_params is None:
+            prep_params = self.preparation_params()
+
         # Define the measurement label
         if label is None:
-            label = 'T1' + self.msmt_suffix
+            label = f'T1{"_ef" if for_ef else ""}'
+            if classified_ro:
+                label += '_classified'
+            if 'active' in prep_params['preparation_type']:
+                label += '_reset'
+            label += self.msmt_suffix
 
-        if cal_points:
-            step = np.abs(times[-1]-times[-2])
-            sweep_points = np.concatenate(
-                [times, [times[-1]+step,  times[-1]+2*step,
-                    times[-1]+3*step, times[-1]+4*step]])
-            cal_states_dict = {'g': [-4, -3], 'e': [-2, -1]}
-            cal_states_rotations = {'g': 0, 'e': 1}
-        else:
-            sweep_points = times
-            cal_states_dict = None
-            cal_states_rotations = {}
+        cal_states = CalibrationPoints.guess_cal_states(cal_states, for_ef)
+        cp = CalibrationPoints.single_qubit(self.name, cal_states,
+                                            n_per_state=n_cal_points_per_state)
 
-        MC.set_sweep_function(awg_swf.T1(
-            pulse_pars=self.get_ge_pars(), RO_pars=self.get_ro_pars(),
-            upload=upload, cal_points=cal_points))
+        seq, sweep_points = sq.t1_active_reset(
+            times=times, qb_name=self.name, cal_points=cp, for_ef=for_ef,
+            operation_dict=self.get_operation_dict(), upload=False,
+            last_ge_pulse=last_ge_pulse, prep_params=prep_params)
+
+        MC.set_sweep_function(awg_swf.SegmentHardSweep(
+            sequence=seq, upload=upload, parameter_name='Time', unit='s'))
         MC.set_sweep_points(sweep_points)
-        MC.set_detector_function(self.int_avg_classif_det if
-                                 self.acq_weights_type() == 'optimal_qutrit'
-                                 else self.int_avg_det)
+        MC.set_detector_function(self.int_avg_classif_det if classified_ro else
+                                 self.int_avg_det)
+
         if exp_metadata is None:
             exp_metadata = {}
-        exp_metadata.update({'sweep_points_dict': {self.name: sweep_points},
-                             'cal_states_dict': cal_states_dict,
-                             'cal_states_rotations': cal_states_rotations if
-                                self.acq_weights_type() != 'optimal_qutrit'
-                                else None,
-                             'data_to_fit': {self.name: 'pe'},
-                             'use_cal_points': cal_points})
+        exp_metadata.update({'sweep_points_dict': {self.name: times},
+                             'preparation_params': prep_params,
+                             'cal_points': repr(cp),
+                             'rotate': False if classified_ro else True,
+                             'last_ge_pulses': [last_ge_pulse],
+                             'data_to_fit': {self.name: 'pf' if for_ef else 'pe'},
+                             "sweep_name": "Time",
+                             "sweep_unit": "s"})
+
         MC.run(label, exp_metadata=exp_metadata)
 
         if analyze:
@@ -2346,10 +2354,10 @@ class QuDev_transmon(Qubit):
 
         return
 
-
-    def find_T1(self, times, label=None, for_ef=False, update=False,
-                cal_points=True, no_cal_points=None, close_fig=True,
-                last_ge_pulse=True, upload=True, **kw):
+    def find_T1(self, times, n_cal_points_per_state=2, cal_states='auto',
+                upload=True, last_ge_pulse=False, classified_ro=False,
+                prep_params=None, analyze=True, update=False, label=None,
+                for_ef=False, exp_metadata=None, **kw):
 
         """
         Finds the relaxation time T1 from the fit to an exponential
@@ -2406,29 +2414,10 @@ class QuDev_transmon(Qubit):
 
         if not update:
             log.warning("Does not automatically update the qubit "
-                            "T1 parameter. Set update=True if you want this!")
-        if np.any(times>1e-3):
+                        "T1 parameter. Set update=True if you want this!")
+        if np.any(times > 1e-3):
             raise ValueError('Some of the values in the times array might be too '
-                            'large. The units should be seconds.')
-
-        if cal_points and no_cal_points is None:
-            log.warning('no_cal_points is None. Defaults to 4 if '
-                            'for_ef==False, or to 6 if for_ef==True.')
-            if for_ef:
-                no_cal_points = 6
-            else:
-                no_cal_points = 4
-
-        if not cal_points:
-            no_cal_points = 0
-
-        MC = self.instr_mc.get_instr()
-
-        if label is None:
-            if for_ef:
-                label = 'T1_ef' + self.msmt_suffix
-            else:
-                label = 'T1' + self.msmt_suffix
+                             'large. The units should be seconds.')
 
         if times is None:
             times_span = kw.get('times_span', 10e-6)
@@ -2436,30 +2425,24 @@ class QuDev_transmon(Qubit):
             nr_points = kw.get('nr_points', 50)
             if times_mean == 0:
                 log.warning("find_T1 does not know how long to wait before"
-                                "doing the read out. Please specify the "
-                                "times_mean or the times function parameter.")
+                            "doing the read out. Please specify the "
+                            "times_mean or the times function parameter.")
                 return 0
             else:
-                times = np.linspace(times_mean - times_span/2, times_mean +
-                                    times_span/2, nr_points)
+                times = np.linspace(times_mean - times_span / 2, times_mean +
+                                    times_span / 2, nr_points)
 
-        #Perform measurement
-        if for_ef:
-            self.measure_T1_2nd_exc(times=times,
-                                    close_fig=close_fig,
-                                    cal_points=cal_points,
-                                    no_cal_points=no_cal_points,
-                                    last_ge_pulse=last_ge_pulse,
-                                    upload=upload, label=label)
+        # Perform measurement
+        self.measure_T1(times=times,
+                        analyze=False, upload=upload,
+                        last_ge_pulse=last_ge_pulse, for_ef=for_ef,
+                        n_cal_points_per_state=n_cal_points_per_state,
+                        cal_states=cal_states, classified_ro=classified_ro,
+                        prep_params=prep_params, label=label,
+                        exp_metadata=exp_metadata)
 
-        else:
-            self.measure_T1(times=times,
-                            close_fig=close_fig,
-                            cal_points=cal_points,
-                            upload=upload, label=label)
-
-        #Extract T1 and T1_stddev from ma.T1_Analysis
-        if kw.pop('analyze', True):
+        # Extract T1 and T1_stddev from ma.T1_Analysis
+        if analyze:
             T1_ana = tda.T1Analysis(qb_names=[self.name])
             if update:
                 T1 = T1_ana.proc_data_dict['analysis_params_dict'][
@@ -2468,8 +2451,7 @@ class QuDev_transmon(Qubit):
                     self.T1_ef(T1)
                 else:
                     self.T1(T1)
-
-        return
+        return T1
 
     def find_RB_gate_fidelity(self, cliffords, nr_seeds, label=None,
                               gate_decomposition='HZ', interleaved_gate=None,

pycqed/measurement/multi_qubit_module.py
@@ -28,6 +28,7 @@ import pycqed.analysis.randomized_benchmarking_analysis as rbma
 import pycqed.analysis_v2.readout_analysis as ra
 import pycqed.analysis_v2.timedomain_analysis as tda
 import pycqed.measurement.waveform_control.sequence as sequence
+from pycqed.utilities.general import temporary_value
 
 try:
     import \
@@ -2247,6 +2248,86 @@ def measure_dynamic_phases(qbc, qbt, cz_pulse_name, hard_sweep_params=None,
     return dyn_phases
 
 
+def measure_ramsey_with_flux_pulse(qb, cz_pulse_name, hard_sweep_params=None,
+                           qb_freq=None,
+                           artificial_detunings=20e6,
+                           cal_points=True,
+                           analyze=True, upload=True, n_cal_points_per_state=2,
+                           cal_states='auto', prep_params=None,
+                           exp_metadata=None, classified=False):
+
+    if hard_sweep_params is None:
+        hard_sweep_params = {
+            'Delay': {
+                'values': np.linspace(0, 200e-9, 31),
+                'unit': 's'}}
+    label = f'Ramsey_flux_pulse-{qb.name}'
+
+
+    if qb_freq is None:
+        qb_freq = qb.ge_freq()
+
+    with temporary_value(qb.ge_freq, qb_freq):
+        qb.prepare(drive='timedomain')
+        MC = qb.instr_mc.get_instr()
+
+        if cal_points:
+            cal_states = CalibrationPoints.guess_cal_states(cal_states)
+            cp = CalibrationPoints.single_qubit(
+                qb.name, cal_states, n_per_state=n_cal_points_per_state)
+        else:
+            cp = None
+
+        if prep_params is None:
+            prep_params = qb.preparation_params()
+
+        operation_dict = get_operation_dict([qb])
+
+        seq, hard_sweep_points = fsqs.Ramsey_time_with_flux_seq(qb.name,
+                                                                hard_sweep_params,
+                                                                operation_dict,
+                                                                cz_pulse_name,
+                                                                artificial_detunings=artificial_detunings,
+                                                                cal_points=cp,
+                                                                upload=False,
+                                                                prep_params=prep_params)
+
+        MC.set_sweep_function(awg_swf.SegmentHardSweep(
+            sequence=seq, upload=upload,
+            parameter_name=list(hard_sweep_params)[0],
+            unit=list(hard_sweep_params.values())[0]['unit']))
+        MC.set_sweep_points(hard_sweep_points)
+        MC.set_detector_function(qb.int_avg_classif_det if classified
+                                 else qb.int_avg_det)
+        if exp_metadata is None:
+            exp_metadata = {}
+        exp_metadata.update({'sweep_points_dict': {qb.name: hard_sweep_params[
+            'Delay']['values']},
+                             'sweep_name': 'Delay',
+                             'sweep_unit': 's',
+                             'artificial_detuning': artificial_detunings,
+                             'preparation_params': prep_params,
+                             'cal_points': repr(cp),
+                             'rotate': True,
+                             'cal_states_rotations': {qb.name: []},
+                             'data_to_fit': {qb.name: 'pe'},
+                            })
+        MC.run(label, exp_metadata=exp_metadata)
+
+    ramsey_ana = None
+    if analyze:
+        ramsey_ana = tda.RamseyAnalysis(
+            qb_names=[qb.name], options_dict=dict(
+                fit_gaussian_decay=True))
+        new_qubit_freq = ramsey_ana.proc_data_dict[
+            'analysis_params_dict'][qb.name]['exp_decay_' + qb.name][
+            'new_qb_freq']
+        T2_star = ramsey_ana.proc_data_dict[
+            'analysis_params_dict'][qb.name]['exp_decay_' + qb.name][
+            'T2_star']
+    return new_qubit_freq, T2_star
+
+
 def measure_cz_bleed_through(qb, CZ_separation_times, phases, CZ_pulse_name,
                              label=None, upload=True, cal_points=True,
                              oneCZ_msmt=False, soft_avgs=1, nr_cz_gates=1,
@@ -2646,10 +2727,12 @@ def measure_pygsti(qubits, f_LO, pygsti_gateset=None,
     return MC
 
 
-def measure_ro_dynamic_phases(pulsed_qubit, measured_qubits, hard_sweep_params,
+def measure_ro_dynamic_phases(pulsed_qubit, measured_qubits,
+                              hard_sweep_params=None, exp_metadata=None,
                               pulse_separation=None, init_state='g',
                               upload=True, n_cal_points_per_state=1,
-                              cal_states='auto', classified=False):
+                              cal_states='auto', classified=False,
+                              prep_params=None):
     if not hasattr(measured_qubits, '__iter__'):
         measured_qubits = [measured_qubits]
     qubits = measured_qubits + [pulsed_qubit]
@@ -2661,9 +2744,15 @@ def measure_ro_dynamic_phases(pulsed_qubit, measured_qubits, hard_sweep_params,
         MC = qb.instr_mc.get_instr()
         qb.prepare(drive='timedomain')
 
+    if hard_sweep_params is None:
+        hard_sweep_params = {
+            'phase': {'values': np.tile(np.linspace(0, 2*np.pi, 6)*180/np.pi, 2),
+                      'unit': 'deg'}
+        }
+
     cal_states = CalibrationPoints.guess_cal_states(cal_states,
-                                                    for_ef=for_ef)
-    cp = CalibrationPoints.multi_qubit([qb for qb in measured_qubits], cal_states,
+                                                    for_ef=False)
+    cp = CalibrationPoints.multi_qubit([qb.name for qb in qubits], cal_states,
                                        n_per_state=n_cal_points_per_state)
 
     if prep_params is None:
@@ -2676,12 +2765,11 @@ def measure_ro_dynamic_phases(pulsed_qubit, measured_qubits, hard_sweep_params,
                                                    measured_qubits],
             operation_dict=operation_dict,
             pulse_separation=pulse_separation,
-            init_state = init_state,
-            cal_points=cp, upload=False,
-            prep_params=prep_params)
+            init_state = init_state, prep_params=prep_params,
+            cal_points=cp, upload=False)
 
     hard_sweep_func = awg_swf.SegmentHardSweep(
-        sequence=sequences[0], upload=upload,
+        sequence=sequences, upload=upload,
         parameter_name=list(hard_sweep_params)[0],
         unit=list(hard_sweep_params.values())[0]['unit'])
     MC.set_sweep_function(hard_sweep_func)
@@ -2695,16 +2783,23 @@ def measure_ro_dynamic_phases(pulsed_qubit, measured_qubits, hard_sweep_params,
 
     if exp_metadata is None:
         exp_metadata = {}
+
+    hard_sweep_params = {
+        'phase': {'values': np.repeat(np.tile(np.linspace(0, 2 * np.pi, 6) * 180 /
+                                         np.pi,
+                                     2),2),
+                  'unit': 'deg'}
+    }
     exp_metadata.update({'qbnames': [qb.name for qb in qubits],
                          'preparation_params': prep_params,
                          'cal_points': repr(cp),
                          'rotate': len(cal_states) != 0,
                          'cal_states_rotations':
-                             {qb.name: {'g': 0, 'f': 1} for qb in qubits} if
+                             {qb.name: {'g': 0, 'e': 1} for qb in qubits} if
                              len(cal_states) != 0 else None,
                          'data_to_fit': {qb.name: 'pe' for qb in qubits},
                          'hard_sweep_params': hard_sweep_params})
     MC.run('RO_DynamicPhase_{}{}'.format(
-        pulsed_qubit.name, ''.join(qbr_names)),
+        pulsed_qubit.name, ''.join([qb.name for qb in qubits])),
         exp_metadata=exp_metadata)
-    ma.MeasurementAnalysis()
+    tda.MultiQubit_TimeDomain_Analysis(qb_names=[qb.name for qb in measured_qubits])

pycqed/measurement/pulse_sequences/multi_qubit_tek_seq_elts.py
@@ -10,7 +10,7 @@ from pycqed.measurement.pulse_sequences.standard_elements import \
 import pycqed.measurement.randomized_benchmarking.randomized_benchmarking as rb
 import pycqed.measurement.randomized_benchmarking.two_qubit_clifford_group as tqc
 from pycqed.measurement.pulse_sequences.single_qubit_tek_seq_elts import \
-    get_pulse_dict_from_pars, add_preparation_pulses, pulse_list_list_seq
+    get_pulse_dict_from_pars, add_preparation_pulses, pulse_list_list_seq, sweep_pulse_params
 from pycqed.measurement.gate_set_tomography.gate_set_tomography import \
     create_experiment_list_pyGSTi_qudev as get_exp_list
 from pycqed.measurement.waveform_control import pulsar as ps
@@ -1779,8 +1779,8 @@ def pygsti_seq(qb_names, pygsti_listOfExperiments, operation_dict,
 def ro_dynamic_phase_seq(hard_sweep_dict, qbp_name, qbr_names,
                          operation_dict,
                          pulse_separation, init_state,
-                         verbose=False, cal_points=True,
-                         upload=True, return_seq=False):
+                         cal_points=True, prep_params=dict(),
+                         upload=True):
 
     """
     RO cross-dephasing measurement sequence. Measures the dynamic phase induced
@@ -1798,81 +1798,102 @@ def ro_dynamic_phase_seq(hard_sweep_dict, qbp_name, qbr_names,
     """
 
     seq_name = 'ro_dynamic_phase_sequence'
-    seq = sequence.Sequence(seq_name)
-    el_list = []
+    dummy_ro_1 = {'pulse_type': 'GaussFilteredCosIQPulse',
+                  'I_channel': 'UHF1_ch1',
+                  'Q_channel': 'UHF1_ch2',
+                  'amplitude': 0.00001,
+                  'pulse_length': 50e-09,
+                  'pulse_delay': 0,
+                  'mod_frequency': 900.0e6,
+                  'phase': 0,
+                  'phi_skew': 0,
+                  'alpha': 1,
+                  'gaussian_filter_sigma': 1e-09,
+                  'nr_sigma': 2,
+                  'phase_lock': True,
+                  'basis_rotation': {},
+                  'operation_type': 'RO'}
+    dummy_ro_2 = {'pulse_type': 'GaussFilteredCosIQPulse',
+                  'I_channel': 'UHF2_ch1',
+                  'Q_channel': 'UHF2_ch2',
+                  'amplitude': 0.00001,
+                  'pulse_length': 50e-09,
+                  'pulse_delay': 0,
+                  'mod_frequency': 900.0e6,
+                  'phase': 0,
+                  'phi_skew': 0,
+                  'alpha': 1,
+                  'gaussian_filter_sigma': 1e-09,
+                  'nr_sigma': 2,
+                  'phase_lock': True,
+                  'basis_rotation': {},
+                  'operation_type': 'RO'}
+
+
+
+
 
     # put together n-qubit calibration point pulse lists
     # put together n-qubit ramsey pulse lists
-    cal_I_pulses = []
-    cal_X_pulses = []
-    x90_1_pulses = []  # first ramsey pulse
-    x90_2a_pulses = []  # with readout pulse
-    x90_2b_pulses = []  # without readout pulse
+    pulse_list = []
     for j, qbr_name in enumerate(qbr_names):
-        if j == 0:
-            cal_I_pulses.append(deepcopy(operation_dict['I ' + qbr_name]))
-            cal_X_pulses.append(deepcopy(operation_dict['X180 ' + qbr_name]))
-            x90_1_pulses.append(deepcopy(operation_dict['X90 ' + qbr_name]))
-            x90_2a_pulses.append(deepcopy(operation_dict['X90 ' + qbr_name]))
-            x90_2b_pulses.append(deepcopy(operation_dict['X90 ' + qbr_name]))
-            x90_2a_pulses[-1]['pulse_delay'] = pulse_separation
-            x90_2a_pulses[-1]['refpoint'] = 'start'
-            x90_2b_pulses[-1]['pulse_delay'] = pulse_separation
-        else:
-            cal_I_pulses.append(deepcopy(operation_dict['Is ' + qbr_name]))
-            cal_X_pulses.append(deepcopy(operation_dict['X180s ' + qbr_name]))
-            x90_1_pulses.append(deepcopy(operation_dict['X90s ' + qbr_name]))
-            x90_2a_pulses.append(deepcopy(operation_dict['X90s ' + qbr_name]))
-            x90_2b_pulses.append(deepcopy(operation_dict['X90s ' + qbr_name]))
+        pulse_list.append(deepcopy(operation_dict['X90 ' + qbr_name]))
+        pulse_list[-1]['name'] = f'x1_{qbr_name}'
+        pulse_list[-1]['ref_pulse'] = 'segment_start'
+        pulse_list[-1]['refpoint'] = 'start'
+
+    ro_probe = deepcopy(operation_dict['RO ' + qbp_name])
+    pulse_list.append(ro_probe)
+    pulse_list[-1]['name'] = f'ro_probe'
+    pulse_list[-1]['element_name'] = f'ro_probe'
+    pulse_list.append(dummy_ro_1)
+    pulse_list[-1]['refpoint'] = 'start'
+    pulse_list[-1]['element_name'] = f'ro_probe'
+    pulse_list.append(dummy_ro_2)
+    pulse_list[-1]['refpoint'] = 'start'
+    pulse_list[-1]['element_name'] = f'ro_probe'
+
+    for j, qbr_name in enumerate(qbr_names):
+        pulse_list.append(deepcopy(operation_dict['X90 ' + qbr_name]))
+        pulse_list[-1]['name'] = f'x2_{qbr_name}'
+        pulse_list[-1]['ref_pulse'] = 'segment_start'
+        pulse_list[-1]['refpoint'] = 'start'
+        pulse_list[-1]['pulse_delay'] = pulse_separation
+
+    ro_list = generate_mux_ro_pulse_list(qbr_names+[qbp_name], operation_dict)
+    pulse_list += ro_list
+
     if init_state == 'e':
-        x90_1_pulses.append(deepcopy(operation_dict['X180 ' + qbp_name]))
-    cal_I_pulses.append(operation_dict['RO mux'])
-    cal_X_pulses.append(operation_dict['RO mux'])
+        pulse_list.append(deepcopy(operation_dict['X180 ' + qbp_name]))
+        pulse_list[-1]['ref_pulse'] = 'segment_start'
+        pulse_list[-1]['refpoint'] = 'start'
+        pulse_list[-1]['pulse_delay'] = -100e-9
 
-    for i, theta in enumerate(phases):
-        if cal_points and (i == (len(phases)-4) or i == (len(phases)-3)):
-            pulse_list = [operation_dict['I ' + qbp_name],
-                          operation_dict['RO ' + qbp_name]]
-            el = multi_pulse_elt(3*i, station, pulse_list)
-            el_list.append(el)
-            seq.append_element(el, trigger_wait=True)
-            for j in range(1, 3):
-                el = multi_pulse_elt(3*i + j, station, cal_I_pulses)
-                el_list.append(el)
-                seq.append_element(el, trigger_wait=True)
-        elif cal_points and (i == (len(phases)-2) or i == (len(phases)-1)):
-            pulse_list = [operation_dict['X180 ' + qbp_name],
-                          operation_dict['RO ' + qbp_name]]
-            el = multi_pulse_elt(3*i, station, pulse_list)
-            el_list.append(el)
-            seq.append_element(el, trigger_wait=True)
-            for j in range(1, 3):
-                el = multi_pulse_elt(3*i + j, station, cal_X_pulses)
-                el_list.append(el)
-                seq.append_element(el, trigger_wait=True)
-        else:
-            for qbr_X90_pulse in x90_2a_pulses + x90_2b_pulses:
-                qbr_X90_pulse['phase'] = theta*180/np.pi
+    params = {f'x2_{qbr_name}.{k}': v['values']
+                   for k, v in hard_sweep_dict.items() for qbr_name in qbr_names}
+    hsl = len(list(hard_sweep_dict.values())[0]['values'])
+    params.update({f'ro_probe.amplitude': np.concatenate(
+        [ro_probe['amplitude'] * np.ones(hsl // 2), np.zeros(hsl // 2)])})
 
-            pulse_list = x90_1_pulses + [operation_dict['RO ' + qbp_name]] + \
-                         x90_2a_pulses + [operation_dict['RO mux']]
-            el = multi_pulse_elt(3*i, station, pulse_list)
-            el_list.append(el)
-            seq.append_element(el, trigger_wait=True)
+    print(params)
+    swept_pulses = sweep_pulse_params(pulse_list, params)
 
-            pulse_list = x90_1_pulses + x90_2b_pulses + \
-                         [operation_dict['RO mux']]
-            el = multi_pulse_elt(3*i + 1, station, pulse_list)
-            el_list.append(el)
-            seq.append_element(el, trigger_wait=True)
+    swept_pulses_with_prep = \
+        [add_preparation_pulses(p, operation_dict, [qbp_name], **prep_params)
+         for p in swept_pulses]
+    seq = pulse_list_list_seq(swept_pulses_with_prep, seq_name, upload=False)
 
+    if cal_points is not None:
+        # add calibration segments
+        seq.extend(cal_points.create_segments(operation_dict, **prep_params))
+
+    log.debug(seq)
     if upload:
-        station.pulsar.program_awgs(seq, *el_list, verbose=verbose)
+        ps.Pulsar.get_instance().program_awgs(seq)
 
-    if return_seq:
-        return seq, el_list
-    else:
-        return seq_name
+    print(seq.n_acq_elements())
+
+    return seq, np.arange(seq.n_acq_elements())
 
 
 def generate_mux_ro_pulse_list(qubit_names, operation_dict, element_name='RO',

pycqed/measurement/pulse_sequences/single_qubit_tek_seq_elts.py
@@ -112,49 +112,62 @@ def rabi_seq_active_reset(amps, qb_name, operation_dict, cal_points,
     return seq, np.arange(seq.n_acq_elements())
 
 
-def T1_seq(times, pulse_pars, RO_pars,
-           cal_points=True, upload=True, return_seq=False):
+def t1_active_reset(times, qb_name, operation_dict, cal_points,
+                    upload=True, for_ef=False, last_ge_pulse=False,
+                    prep_params=dict()):
     '''
-    Rabi sequence for a single qubit using the tektronix.
+    T1 sequence for a single qubit using the tektronix.
     SSB_Drag pulse is used for driving, simple modulation used for RO
     Input pars:
         times:       array of times to wait after the initial pi-pulse
         pulse_pars:  dict containing the pulse parameters
         RO_pars:     dict containing the RO parameters
     '''
+
     if np.any(times>1e-3):
         logging.warning('The values in the times array might be too large.'
                         'The units should be seconds.')
     seq_name = 'T1_sequence'
-    seq = sequence.Sequence(seq_name)
-    seg_list = []
-    RO_pulse_delay = RO_pars['pulse_delay']
-    RO_pars = deepcopy(RO_pars)  # Prevents overwriting of the dict
-    pulses = get_pulse_dict_from_pars(pulse_pars)
 
-    for i, tau in enumerate(times):  # seq has to have at least 2 elts
-        RO_pars['pulse_delay'] = RO_pulse_delay + tau
-        if cal_points and (i == (len(times)-4) or i == (len(times)-3)):
-            RO_pars['pulse_delay'] = RO_pulse_delay
-            seg = segment.Segment('segment_{}'.format(i),
-                                  [pulses['I'], RO_pars])
-        elif cal_points and (i == (len(times)-2) or i == (len(times)-1)):
-            RO_pars['pulse_delay'] = RO_pulse_delay
-            seg = segment.Segment('segment_{}'.format(i),
-                                  [pulses['X180'], RO_pars])
-        else:
-            seg = segment.Segment('segment_{}'.format(i),
-                                  [pulses['X180'], RO_pars])
-        seg_list.append(seg)
-        seq.add(seg)
+    #Operations
+    if for_ef:
+        ops = ["X180", "X180_ef"]
+        if last_ge_pulse:
+            ops += ["X180"]
+    else:
+        ops = ["X180"]
+    ops += ["RO"]
+    ops = add_suffix(ops, " " + qb_name)
+    pulses = [deepcopy(operation_dict[op]) for op in ops]
+
+    # name delayed pulse: last ge pulse if for_ef and last_ge_pulse
+    # otherwise readout pulse
+    if for_ef and last_ge_pulse:
+        delayed_pulse = -2 # last_ge_pulse
+        delays = np.array(times)
+    else:
+        delayed_pulse = -1 # readout pulse
+        delays = np.array(times) + pulses[-1]["pulse_delay"]
+
+    pulses[delayed_pulse]['name'] = "Delayed_pulse"
+
+    # vary delay of readout pulse or last ge pulse
+    swept_pulses = sweep_pulse_params(pulses, {'Delayed_pulse.pulse_delay': delays})
+
+    # add preparation pulses
+    swept_pulses_with_prep = \
+        [add_preparation_pulses(p, operation_dict, [qb_name], **prep_params)
+         for p in swept_pulses]
+    seq = pulse_list_list_seq(swept_pulses_with_prep, seq_name, upload=False)
+
+    # add calibration segments
+    seq.extend(cal_points.create_segments(operation_dict, **prep_params))
 
     if upload:
         ps.Pulsar.get_instance().program_awgs(seq)
 
-    if return_seq:
-        return seq, seg_list
-    else:
-        return seq_name
+    return seq, np.arange(seq.n_acq_elements())
+
 
 
 def ramsey_seq_Echo(times, pulse_pars, RO_pars, nr_echo_pulses=4,