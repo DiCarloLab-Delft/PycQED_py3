fd208416cdc61fde4fb06bd3aa2dcfe6aa3dd69d
Nathan Lacroix <lna@student.ethz.ch>
2019-10-10 17:24:32 +0200

    Fix string recognition of ro channel map because of representation saving of strings

--- pycqed/analysis/analysis_toolbox.py
+++ pycqed/analysis/analysis_toolbox.py
@@ -345,7 +345,8 @@ def get_qb_channel_map_from_hdf(qb_names, file_path, value_names, h5mode='r+'):
 
     for qbn in qb_names:
         qbchs = [str(instr_settings[qbn].attrs['acq_I_channel'])]
-        ro_acq_weight_type = instr_settings[qbn].attrs['acq_weights_type']
+        # eval because strings are saved as representations
+        ro_acq_weight_type = eval(instr_settings[qbn].attrs['acq_weights_type'])
         if ro_acq_weight_type in ['SSB', 'DSB', 'optimal_qutrit']:
             qbchs += [str(instr_settings[qbn].attrs['acq_Q_channel'])]
         channel_map[qbn] = [ch for ch in value_names for nr in qbchs
