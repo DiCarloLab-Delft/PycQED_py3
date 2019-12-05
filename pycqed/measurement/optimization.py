61329761e3af0c286e00d17b10b7c5eaedf4867c
caenrigen <31376402+caenrigen@users.noreply.github.com>
2019-12-05 18:01:58 +0100

    mirrored the SKOptLearner with the pull request in the adaptive

--- pycqed/measurement/optimization.py
+++ pycqed/measurement/optimization.py
@@ -1,8 +1,10 @@
 import copy
 import numpy as np
 import logging
+from collections import OrderedDict
 from skopt import Optimizer
 from adaptive.utils import cache_latest
+from adaptive.notebook_integration import ensure_holoviews
 from adaptive.learner.base_learner import BaseLearner
 
 log = logging.getLogger(__name__)
@@ -276,18 +278,24 @@ class SKOptLearnerND(Optimizer, BaseLearner):
 
     def __init__(self, function, **kwargs):
         self.function = function
-        self.pending_points = list()
+        self.pending_points = set()
+        self.data = OrderedDict()
         super().__init__(**kwargs)
 
     def tell(self, x, y, fit=True):
-        x = list(x)
-        super().tell(x, y, fit)
+        if hasattr(x, "__iter__"):
+            self.pending_points.discard(tuple(x))
+            self.data[tuple(x)] = y
+            super().tell(x, y, fit)
+        else:
+            self.pending_points.discard(x)
+            self.data[x] = y
+            super().tell([x], y, fit)
 
     def tell_pending(self, x):
         # 'skopt.Optimizer' takes care of points we
         # have not got results for.
-        x = list(x)
-        self.pending_points.append(x)
+        self.pending_points.add(tuple(x))
 
     def remove_unfinished(self):
         pass
@@ -320,6 +328,39 @@ class SKOptLearnerND(Optimizer, BaseLearner):
     def npoints(self):
         """Number of evaluated points."""
         return len(self.Xi)
+
+    def plot(self, nsamples=200):
+        hv = ensure_holoviews()
+        if self.space.n_dims > 1:
+            raise ValueError("Can only plot 1D functions")
+        bounds = self.space.bounds[0]
+        if not self.Xi:
+            p = hv.Scatter([]) * hv.Curve([]) * hv.Area([])
+        else:
+            scatter = hv.Scatter(([p[0] for p in self.Xi], self.yi))
+            if self.models:
+                model = self.models[-1]
+                xs = np.linspace(*bounds, nsamples)
+                xsp = self.space.transform(xs.reshape(-1, 1).tolist())
+                y_pred, sigma = model.predict(xsp, return_std=True)
+                # Plot model prediction for function
+                curve = hv.Curve((xs, y_pred)).opts(style=dict(line_dash="dashed"))
+                # Plot 95% confidence interval as colored area around points
+                area = hv.Area(
+                    (xs, y_pred - 1.96 * sigma, y_pred + 1.96 * sigma),
+                    vdims=["y", "y2"],
+                ).opts(style=dict(alpha=0.5, line_alpha=0))
+
+            else:
+                area = hv.Area([])
+                curve = hv.Curve([])
+            p = scatter * curve * area
+
+        # Plot with 5% empty margins such that the boundary points are visible
+        margin = 0.05 * (bounds[1] - bounds[0])
+        plot_bounds = (bounds[0] - margin, bounds[1] + margin)
+
+        return p.redim(x=dict(range=plot_bounds))
 
     def _get_data(self):
         return [x[0] for x in self.Xi], self.yi
