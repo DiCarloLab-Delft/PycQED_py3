75488971bc2459c3324f83133a1c6cbe017380a1
caenrigen <31376402+caenrigen@users.noreply.github.com>
2020-02-16 03:51:23 +0100

    fixed bug for zero area edge case

--- pycqed/analysis/tools/plot_interpolation.py
+++ pycqed/analysis/tools/plot_interpolation.py
@@ -2,6 +2,7 @@ import numpy as np
 import logging
 from scipy import interpolate
 
+
 def areas(ip):
     p = ip.tri.points[ip.tri.vertices]
     q = p[:, :-1, :] - p[:, -1, None, :]
@@ -18,7 +19,8 @@ def unscale(points, xy_mean, xy_scale):
     points = np.asarray(points, dtype=float)
     return points * xy_scale + xy_mean
 
-def interpolate_heatmap(x, y, z, n: int=None, interp_method:str='linear'):
+
+def interpolate_heatmap(x, y, z, n: int = None, interp_method: str = "linear"):
     """
     Args:
         x   (array): x data points
@@ -38,10 +40,10 @@ def interpolate_heatmap(x, y, z, n: int=None, interp_method:str='linear'):
     The output of this method can directly be used for
         plt.imshow(z_grid, extent=extent, aspect='auto')
         where the extent is determined by the min and max of the x_grid and
-        y_grid. 
+        y_grid.
 
-    The output can also be used as input for 
-        ax.pcolormesh(x, y, Z,**kw) 
+    The output can also be used as input for
+        ax.pcolormesh(x, y, Z,**kw)
 
     """
 
@@ -57,46 +59,51 @@ def interpolate_heatmap(x, y, z, n: int=None, interp_method:str='linear'):
     # N.B. even if interp_method == "nearest" the linear interpolation is used
     # to determine the amount of grid points. Could be improved.
     ip = interpolate.LinearNDInterpolator(
-        scale(points, xy_mean=xy_mean, xy_scale=xy_scale), z)
+        scale(points, xy_mean=xy_mean, xy_scale=xy_scale), z
+    )
 
     if n is None:
         # Calculate how many grid points are needed.
         # factor from A=√3/4 * a² (equilateral triangle)
-        # N.B. a factor 4 was added as there were to few points for uniform 
-        # grid otherwise. 
-        n = int(0.658 / np.sqrt(areas(ip).min()))*4
+        # N.B. a factor 4 was added as there were to few points for uniform
+        # grid otherwise.
+        all_areas = areas(ip)
+        area_min = all_areas[all_areas > 0.].min()
+        n = int(0.658 / np.sqrt(area_min)) * 4
         n = max(n, 10)
         if n > 500:
-            logging.warning('n: {} larger than 500'.format(n))
-            n=500
+            logging.debug("n: {} larger than 500".format(n))
+            n = 500
 
     x_lin = y_lin = np.linspace(-0.5, 0.5, n)
 
-    if interp_method == 'linear':
+    if interp_method == "linear":
         z_grid = ip(x_lin[:, None], y_lin[None, :]).squeeze()
     elif interp_method == "nearest":
         ip = interpolate.NearestNDInterpolator(
-            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), z)
+            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), z
+        )
         z_grid = ip(x_lin[:, None], y_lin[None, :]).squeeze()
     elif interp_method == "deg":
         # Circular interpolation in deg units
-        phases=np.deg2rad(z)
-        newdata_cos=np.cos(phases)
-        newdata_sin=np.sin(phases)
+        phases = np.deg2rad(z)
+        newdata_cos = np.cos(phases)
+        newdata_sin = np.sin(phases)
 
         ip_cos = interpolate.LinearNDInterpolator(
-            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), newdata_cos)
+            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), newdata_cos
+        )
         newdata_cos = ip_cos(x_lin[:, None], y_lin[None, :]).squeeze()
 
         ip_sin = interpolate.LinearNDInterpolator(
-            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), newdata_sin)
+            scale(points, xy_mean=xy_mean, xy_scale=xy_scale), newdata_sin
+        )
         newdata_sin = ip_sin(x_lin[:, None], y_lin[None, :]).squeeze()
 
         z_grid = (np.rad2deg(np.arctan2(newdata_sin, newdata_cos)) % 360).squeeze()
 
     # x and y grid points need to be rescaled from the linearly chosen points
-    points_grid = unscale(list(zip(x_lin, y_lin)),
-                          xy_mean=xy_mean, xy_scale=xy_scale)
+    points_grid = unscale(list(zip(x_lin, y_lin)), xy_mean=xy_mean, xy_scale=xy_scale)
     x_grid = points_grid[:, 0]
     y_grid = points_grid[:, 1]
 
