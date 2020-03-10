ad153feead2da638215d49496b6ba25fe6c33cee
caenrigen <31376402+caenrigen@users.noreply.github>
2020-03-10 13:39:01 +0100

    bug fixing: proper delays handling for UHFQC and HDAWG

--- pycqed/instrument_drivers/meta_instrument/device_object_CCL.py
+++ pycqed/instrument_drivers/meta_instrument/device_object_CCL.py
@@ -299,32 +299,35 @@ class DeviceCCL(Instrument):
                                      ('mw_3', self.tim_mw_latency_3()),
                                      ('mw_4', self.tim_mw_latency_4())]
                                     )
-
         # Substract lowest value to ensure minimal latency is used.
         # note that this also supports negative delays (which is useful for
         # calibrating)
-
         lowest_value = min(latencies.values())
         for key, val in latencies.items():
-            latencies[key] = val - lowest_value
+            # Align to minimum and change to ns to avoid number precision problems
+            # The individual multiplications are on purpose
+            latencies[key] = val * 1e9 - lowest_value * 1e9
 
         # ensuring that RO latency is a multiple of 20 ns as the UHFQC does
         # not have a fine timing control.
-        ro_latency_modulo_20 = latencies['ro_0'] % 20e-9
-        for key, val in latencies.items():
-            latencies[key] = val + (20e-9 - ro_latency_modulo_20) % 20e-9
+        ro_latency_modulo_20 = latencies['ro_0'] % 20
+        # `% 20` is for the case ro_latency_modulo_20 == 20ns
+        correction_for_multiple = (20 - ro_latency_modulo_20) % 20
+        if correction_for_multiple >= 1e-3:  # at least one 1 ps
+            # Only apply corrections if they are significant
+            for key, val in latencies.items():
+                latencies[key] = val + correction_for_multiple
 
         # Setting the latencies in the CCL
-        CC = self.instr_CC.get_instr()
-        dio_map = self.dio_map()
-
         # Iterate over keys in dio_map as this ensures only relevant
         # timing setting are set.
-        for lat_key, dio_ch in dio_map.items():
+        for lat_key, dio_ch in self.dio_map().items():
             lat = latencies[lat_key]
-            lat_coarse = int(lat*1e9 // 20)  # Convert to CC dio value
-            lat_fine = int(lat*1e9 % 20)*1e-9
-            CC.set('dio{}_out_delay'.format(dio_ch), lat_coarse)
+            lat_coarse = int(np.round(lat) // 20)  # Convert to CC dio value
+            lat_fine = (lat % 20) * 1e-9
+            log.debug('Setting `dio{}_out_delay` for `{}` to `{}`'.format(
+                dio_ch, lat_key, lat_coarse))
+            cc.set('dio{}_out_delay'.format(dio_ch), lat_coarse)
 
             # RO devices do not support fine delay setting.
             if 'mw' in lat_key or 'flux' in lat_key:
@@ -337,6 +340,8 @@ class DeviceCCL(Instrument):
                         # All channels are set globally from the device object.
                         AWG.stop()
                         for i in range(8):  # assumes the AWG is an HDAWG
+                            log.debug("Setting `sigouts_{}_delay` to {:4g}"
+                                " in {}".format(i, lat_fine, AWG.name))
                             AWG.set('sigouts_{}_delay'.format(i), lat_fine)
                         AWG.start()
                         ch_not_ready = 8
