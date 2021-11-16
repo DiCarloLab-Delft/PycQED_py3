26504fe309d92acb242f4b85c409dadacf0baebb
Wouter Vlothuizen <wouter.vlothuizen@tno.nl>
2021-11-16 22:11:02 +0100

    cleanup

--- examples/QWG_examples/QWG_example.py
+++ examples/QWG_examples/QWG_example.py
@@ -3,37 +3,38 @@
 import pycqed as pq # FIXME: must be before qcodes
 
 import time
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
 
 from pycqed.instrument_drivers.library.Transport import IPTransport
-import pycqed.instrument_drivers.library.DIO as DIO
+#import pycqed.instrument_drivers.library.DIO as DIO
 from pycqed.instrument_drivers.physical_instruments.QuTech.QWG import QWG,QWGMultiDevices
-from .waveform import Waveform
+
+from waveform import Waveform
 
 
 # create waveforms
-sampleCnt = 96
+sample_cnt = 96
 fs = 1e9  # sampling frequency
 f = fs/32
 # mu=30, sigma=10, dirAmpl=1.0 fits 64 samples nicely
 mu = 30e-9
 sigma = 10e-9
-dirAmpl = 1.0
+dir_ampl = 1.0
 # mu2=15, sigma2=5, dirAmpl2=1.0 fits 64 samples nicely
 mu2 = 15e-9
 sigma2 = 5e-9
-dirAmpl2 = 1.0
+dir_ampl2 = 1.0
 
-wvCos = Waveform.cos(fs, sampleCnt, f)
-wvSin = Waveform.sin(fs, sampleCnt, f)
-wvZero = Waveform.DC(fs, sampleCnt)
-wvHi = Waveform.DC(fs, sampleCnt, 1.0)
-wvLo = Waveform.DC(fs, sampleCnt, -1.0)
-wvGauss = Waveform.gauss(fs, sampleCnt, mu, sigma)
-wvDerivGauss = Waveform.derivGauss(fs, sampleCnt, mu, sigma, dirAmpl)
-wvGauss2 = Waveform.gauss(fs, sampleCnt, mu2, sigma2)
-wvDerivGauss2 = Waveform.derivGauss(fs, sampleCnt, mu2, sigma2, dirAmpl2)
+wv_cos = Waveform.cos(fs, sample_cnt, f)
+wv_sin = Waveform.sin(fs, sample_cnt, f)
+wv_zero = Waveform.DC(fs, sample_cnt)
+wv_hi = Waveform.DC(fs, sample_cnt, 1.0)
+wv_lo = Waveform.DC(fs, sample_cnt, -1.0)
+wv_gauss = Waveform.gauss(fs, sample_cnt, mu, sigma)
+wv_deriv_gauss = Waveform.derivGauss(fs, sample_cnt, mu, sigma, dir_ampl)
+wv_gauss2 = Waveform.gauss(fs, sample_cnt, mu2, sigma2)
+wv_deriv_gauss2 = Waveform.derivGauss(fs, sample_cnt, mu2, sigma2, dir_ampl2)
 
 
 qwg1 = QWG('qwg_21', IPTransport('192.168.0.179'))
@@ -42,13 +43,13 @@ qwg1.init()
 def run(continuous=True):
 
     if continuous:
-        qwg1.create_waveform_real('cos', wvCos)
-        qwg1.create_waveform_real('sin', wvSin)
-        qwg1.create_waveform_real('zero', wvZero)
-        qwg1.create_waveform_real('hi', wvHi)
-        qwg1.create_waveform_real('lo', wvLo)
-        qwg1.create_waveform_real('gauss', wvGauss)
-        qwg1.create_waveform_real('derivGauss', wvDerivGauss)
+        qwg1.create_waveform_real('cos', wv_cos)
+        qwg1.create_waveform_real('sin', wv_sin)
+        qwg1.create_waveform_real('zero', wv_zero)
+        qwg1.create_waveform_real('hi', wv_hi)
+        qwg1.create_waveform_real('lo', wv_lo)
+        qwg1.create_waveform_real('gauss', wv_gauss)
+        qwg1.create_waveform_real('derivGauss', wv_deriv_gauss)
 
         # qwg1.set('ch1_default_waveform', 'hi')
         # qwg1.set('ch2_default_waveform', 'zero')
@@ -58,14 +59,14 @@ def run(continuous=True):
         qwg1.run_mode('CONt')
 
     else:  # codeword based
-        qwg1.create_waveform_real('zero', wvZero)
-        qwg1.create_waveform_real('hi', wvHi)
-        qwg1.create_waveform_real('lo', wvLo)
-        qwg1.create_waveform_real('gauss', wvGauss)
-        qwg1.create_waveform_real('derivGauss', wvDerivGauss)
-        qwg1.create_waveform_real('gauss2', wvGauss2)
-        qwg1.create_waveform_real('derivGauss2', wvDerivGauss2)
-        qwg1.create_waveform_real('gaussNeg', -wvGauss)
+        qwg1.create_waveform_real('zero', wv_zero)
+        qwg1.create_waveform_real('hi', wv_hi)
+        qwg1.create_waveform_real('lo', wv_lo)
+        qwg1.create_waveform_real('gauss', wv_gauss)
+        qwg1.create_waveform_real('derivGauss', wv_deriv_gauss)
+        qwg1.create_waveform_real('gauss2', wv_gauss2)
+        qwg1.create_waveform_real('derivGauss2', wv_deriv_gauss2)
+        qwg1.create_waveform_real('gaussNeg', -wv_gauss)
 
         # segment 0: idle
         qwg1.set('ch1_default_waveform', 'zero')
@@ -75,10 +76,10 @@ def run(continuous=True):
 
         # set some standard waveform to all codewords
         for seg in range(8):
-            qwg1.set('wave_ch{}_cw{:03}'.format(1, seg), wvGauss)
-            qwg1.set('wave_ch{}_cw{:03}'.format(2, seg), wvDerivGauss)
-            qwg1.set('wave_ch{}_cw{:03}'.format(3, seg), wvGauss2)
-            qwg1.set('wave_ch{}_cw{:03}'.format(4, seg), wvDerivGauss2)
+            qwg1.set('wave_ch{}_cw{:03}'.format(1, seg), wv_gauss)
+            qwg1.set('wave_ch{}_cw{:03}'.format(2, seg), wv_deriv_gauss)
+            qwg1.set('wave_ch{}_cw{:03}'.format(3, seg), wv_gauss2)
+            qwg1.set('wave_ch{}_cw{:03}'.format(4, seg), wv_deriv_gauss2)
 
         qwg1.run_mode('CODeword')
 
@@ -98,30 +99,30 @@ def run(continuous=True):
 
 
     if 1:
-        wvCosReadBack = qwg1.get_waveform_data_float('cos')
-        plt.plot(wvCosReadBack)
+        wv_cos_read_back = qwg1.get_waveform_data_float('cos')
+        plt.plot(wv_cos_read_back)
         plt.ylabel('cos')
         plt.show()
 
         # waveform upload performance
         sizes = [100, 500, 1000, 1500, 2000, 2500]
-        nrIter = 50
+        nr_iter = 50
         durations = []
-        megaBytesPerSecond = []
+        mega_bytes_per_second = []
         for size in sizes:
-            wvTest = Waveform.sin(fs, size, f)
+            wv_test = Waveform.sin(fs, size, f)
             qwg1.get_operation_complete()
-            markStart = time.perf_counter()
-            for i in range(nrIter):
-                qwg1.create_waveform_real(f'testSize{size}Nr{i}', wvTest)
+            mark_start = time.perf_counter()
+            for i in range(nr_iter):
+                qwg1.create_waveform_real(f'testSize{size}Nr{i}', wv_test)
             qwg1.getOperationComplete()
             markEnd = time.perf_counter()
-            duration = (markEnd-markStart)/nrIter
+            duration = (markEnd-mark_start)/nr_iter
             durations.append(duration*1e3)
-            megaBytesPerSecond.append(size*4/duration/1e6)
+            mega_bytes_per_second.append(size*4/duration/1e6)
         print(sizes)
         print(durations)
-        print(megaBytesPerSecond)
+        print(mega_bytes_per_second)
         plt.figure(1)
         plt.subplot(211)
         plt.plot(sizes, durations, 'bs')
@@ -129,7 +130,7 @@ def run(continuous=True):
         plt.ylabel('duration per upload [ms]')
         plt.axis([0, 2600, 0, 5])
         plt.subplot(212)
-        plt.plot(sizes, megaBytesPerSecond, 'g^')
+        plt.plot(sizes, mega_bytes_per_second, 'g^')
         plt.xlabel('upload size [samples]')
         plt.ylabel('performance [MB/s]')
         plt.axis([0, 2600, 0, 20])
