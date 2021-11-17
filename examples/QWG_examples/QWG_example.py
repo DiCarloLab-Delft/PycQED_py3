8cd13d875a05e140042fa78c3deb55d2653df8c8
Wouter Vlothuizen <wouter.vlothuizen@tno.nl>
2021-11-17 14:34:51 +0100

    fixed example, performance plot works again

--- examples/QWG_examples/QWG_example.py
+++ examples/QWG_examples/QWG_example.py
@@ -37,72 +37,73 @@ wv_gauss2 = Waveform.gauss(fs, sample_cnt, mu2, sigma2)
 wv_deriv_gauss2 = Waveform.derivGauss(fs, sample_cnt, mu2, sigma2, dir_ampl2)
 
 
-qwg1 = QWG('qwg_21', IPTransport('192.168.0.179'))
-qwg1.init()
+qwg = QWG('qwg_9', IPTransport('192.168.0.191'))
+qwg.init()
 
 def run(continuous=True):
 
     if continuous:
-        qwg1.create_waveform_real('cos', wv_cos)
-        qwg1.create_waveform_real('sin', wv_sin)
-        qwg1.create_waveform_real('zero', wv_zero)
-        qwg1.create_waveform_real('hi', wv_hi)
-        qwg1.create_waveform_real('lo', wv_lo)
-        qwg1.create_waveform_real('gauss', wv_gauss)
-        qwg1.create_waveform_real('derivGauss', wv_deriv_gauss)
+        qwg.create_waveform_real('cos', wv_cos)
+        qwg.create_waveform_real('sin', wv_sin)
+        qwg.create_waveform_real('zero', wv_zero)
+        qwg.create_waveform_real('hi', wv_hi)
+        qwg.create_waveform_real('lo', wv_lo)
+        qwg.create_waveform_real('gauss', wv_gauss)
+        qwg.create_waveform_real('derivGauss', wv_deriv_gauss)
 
         # qwg1.set('ch1_default_waveform', 'hi')
         # qwg1.set('ch2_default_waveform', 'zero')
-        qwg1.set('ch3_default_waveform', 'hi')
+        qwg.set('ch3_default_waveform', 'hi')
         # qwg1.set('ch4_default_waveform', 'zero')
 
-        qwg1.run_mode('CONt')
+        qwg.run_mode('CONt')
 
     else:  # codeword based
-        qwg1.create_waveform_real('zero', wv_zero)
-        qwg1.create_waveform_real('hi', wv_hi)
-        qwg1.create_waveform_real('lo', wv_lo)
-        qwg1.create_waveform_real('gauss', wv_gauss)
-        qwg1.create_waveform_real('derivGauss', wv_deriv_gauss)
-        qwg1.create_waveform_real('gauss2', wv_gauss2)
-        qwg1.create_waveform_real('derivGauss2', wv_deriv_gauss2)
-        qwg1.create_waveform_real('gaussNeg', -wv_gauss)
+        qwg.create_waveform_real('zero', wv_zero)
+        qwg.create_waveform_real('hi', wv_hi)
+        qwg.create_waveform_real('lo', wv_lo)
+        qwg.create_waveform_real('gauss', wv_gauss)
+        qwg.create_waveform_real('derivGauss', wv_deriv_gauss)
+        qwg.create_waveform_real('gauss2', wv_gauss2)
+        qwg.create_waveform_real('derivGauss2', wv_deriv_gauss2)
+        qwg.create_waveform_real('gaussNeg', -wv_gauss)
 
         # segment 0: idle
-        qwg1.set('ch1_default_waveform', 'zero')
-        qwg1.set('ch2_default_waveform', 'zero')
-        qwg1.set('ch3_default_waveform', 'zero')
-        qwg1.set('ch4_default_waveform', 'zero')
+        qwg.set('ch1_default_waveform', 'zero')
+        qwg.set('ch2_default_waveform', 'zero')
+        qwg.set('ch3_default_waveform', 'zero')
+        qwg.set('ch4_default_waveform', 'zero')
 
         # set some standard waveform to all codewords
         for seg in range(8):
-            qwg1.set('wave_ch{}_cw{:03}'.format(1, seg), wv_gauss)
-            qwg1.set('wave_ch{}_cw{:03}'.format(2, seg), wv_deriv_gauss)
-            qwg1.set('wave_ch{}_cw{:03}'.format(3, seg), wv_gauss2)
-            qwg1.set('wave_ch{}_cw{:03}'.format(4, seg), wv_deriv_gauss2)
+            qwg.set('wave_ch{}_cw{:03}'.format(1, seg), wv_gauss)
+            qwg.set('wave_ch{}_cw{:03}'.format(2, seg), wv_deriv_gauss)
+            qwg.set('wave_ch{}_cw{:03}'.format(3, seg), wv_gauss2)
+            qwg.set('wave_ch{}_cw{:03}'.format(4, seg), wv_deriv_gauss2)
 
-        qwg1.run_mode('CODeword')
+        qwg.run_mode('CODeword')
 
-    qwg1.ch_pair1_sideband_frequency.set(100e6)
-    qwg1.ch_pair3_sideband_frequency.set(100e6)
-    qwg1.sync_sideband_generators()
+    qwg.ch_pair1_sideband_frequency.set(100e6)
+    qwg.ch_pair3_sideband_frequency.set(100e6)
+    qwg.sync_sideband_generators()
 
-    qwg1.ch1_state.set(True)
-    qwg1.ch2_state.set(True)
-    qwg1.ch3_state.set(True)
-    qwg1.ch4_state.set(True)
+    qwg.ch1_state.set(True)
+    qwg.ch2_state.set(True)
+    qwg.ch3_state.set(True)
+    qwg.ch4_state.set(True)
 
-    qwg1.start()
+    qwg.start()
 
     # read back
-    qwg1.get_operation_complete()
+    qwg.get_operation_complete()
 
 
     if 1:
-        wv_cos_read_back = qwg1.get_waveform_data_float('cos')
-        plt.plot(wv_cos_read_back)
-        plt.ylabel('cos')
-        plt.show()
+        if 0: # FIXME: fails
+            wv_cos_read_back = qwg.get_waveform_data_float('cos')
+            plt.plot(wv_cos_read_back)
+            plt.ylabel('cos')
+            plt.show()
 
         # waveform upload performance
         sizes = [100, 500, 1000, 1500, 2000, 2500]
@@ -111,13 +112,13 @@ def run(continuous=True):
         mega_bytes_per_second = []
         for size in sizes:
             wv_test = Waveform.sin(fs, size, f)
-            qwg1.get_operation_complete()
+            qwg.get_operation_complete()
             mark_start = time.perf_counter()
             for i in range(nr_iter):
-                qwg1.create_waveform_real(f'testSize{size}Nr{i}', wv_test)
-            qwg1.getOperationComplete()
-            markEnd = time.perf_counter()
-            duration = (markEnd-mark_start)/nr_iter
+                qwg.create_waveform_real(f'testSize{size}Nr{i}', wv_test)
+            qwg.get_operation_complete()
+            mark_end = time.perf_counter()
+            duration = (mark_end-mark_start)/nr_iter
             durations.append(duration*1e3)
             mega_bytes_per_second.append(size*4/duration/1e6)
         print(sizes)
@@ -128,20 +129,20 @@ def run(continuous=True):
         plt.plot(sizes, durations, 'bs')
         plt.xlabel('upload size [samples]')
         plt.ylabel('duration per upload [ms]')
-        plt.axis([0, 2600, 0, 5])
+#        plt.axis([0, 2600, 0, 5])
         plt.subplot(212)
         plt.plot(sizes, mega_bytes_per_second, 'g^')
         plt.xlabel('upload size [samples]')
         plt.ylabel('performance [MB/s]')
-        plt.axis([0, 2600, 0, 20])
+#        plt.axis([0, 2600, 0, 20])
         plt.show()
 
         # list waveforms
-        print('WLIST size: ', qwg1.get_wlist_size())
-        print('WLIST: ', qwg1.get_wlist())
+        print('WLIST size: ', qwg.get_wlist_size())
+        print('WLIST: ', qwg.get_wlist())
 
-    print('Identity: ', qwg1.get_identity())
-    qwg1.check_errors()
+    print('Identity: ', qwg.get_identity())
+    qwg.check_errors()
 
 
 if __name__ == '__main__':
