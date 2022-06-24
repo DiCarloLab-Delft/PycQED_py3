ee9612315e08cf8744e5dfb119e5dc260c133dee
TUD277774 <TUD277774@tudelft.net>
2022-06-24 11:23:13 +0200

    calibration function added

--- pycqed/tests/instrument_drivers/physical_instruments/test_DIO_CC_HDAWG.py
+++ pycqed/tests/instrument_drivers/physical_instruments/test_DIO_CC_HDAWG.py
@@ -1,11 +1,9 @@
-from RsInstrument import RsInstrument
 from pycqed.instrument_drivers.physical_instruments.QuTech.CC import CC
 from pycqed.instrument_drivers.library.Transport import IPTransport
 from pycqed.instrument_drivers.physical_instruments.ZurichInstruments.ZI_HDAWG8 import ZI_HDAWG8
-
+from pycqed.instrument_drivers.library.DIO import calibrate
 import time
 import qcodes as qc
-import pycqed.tests.instrument_drivers.physical_instruments.utils as utils
 from scipy.signal import butter, lfilter
 import matplotlib.pyplot as plt
 
@@ -32,3 +30,9 @@ HDAWG = ZI_HDAWG8(
     port=8004
 )
 
+ccio = 3
+central_controller.calibrate_dio_protocol()
+# central_controller.assemble_and_start("""mainLoop:
+#                     seq_out         0x10000,1 #0b10000000000000000
+#                     seq_out         0x00000000,1
+#                     stop""")
